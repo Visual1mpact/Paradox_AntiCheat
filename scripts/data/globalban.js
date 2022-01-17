fd0a47301a4bde8ed3b1d8daf8282ed36e9c7539
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-01-17 14:08:51 -0500

    Relocate playername to the proper global ban list
    Since we have a global ban list this can be rectified and is not
    needed in onJoin.son.

--- scripts/data/globalban.js
+++ scripts/data/globalban.js
@@ -213,5 +213,6 @@ export const banplayer = [
     {name: 'RusherB0T'},
     {name: 'BLADE FEAR6'},
     {name: 'UniformKap8104'},
-    {name: 'NevadaKlient71'}
+    {name: 'NevadaKlient71'},
+    {name: 'PointNetwork341'}
 ];
