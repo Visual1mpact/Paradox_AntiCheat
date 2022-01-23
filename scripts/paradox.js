2c841db8c662683e6537d10167adc8beafbe5799
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-01-22 21:39:11 -0500

    Make sure Jesus/B is disabled if staff disables Jesus/A

--- scripts/paradox.js
+++ scripts/paradox.js
@@ -187,10 +187,14 @@ setTickInterval(() => {
         // jesus/b = motion check
         try {
             if (config.modules.jesusB.enabled && Math.abs(player.velocity.y).toFixed(4) <= config.modules.jesusB.maxMotion && Math.abs(player.velocity.y).toFixed(4) >= config.modules.jesusB.minMotion && !player.getEffect(Minecraft.MinecraftEffectTypes.slowFalling)) {
-                if (!playerTags.includes('flying') && !playerTags.includes('jump') && !playerTags.includes('ground') && !playerTags.includes('gliding') && !playerTags.includes('levitating') && !playerTags.includes('vanish') && !playerTags.includes('swimming') ) {
-                    Commands.run(`execute @a[name="${player.nameTag}",tag=!flying,m=!c,tag=!jump,tag=!ground,tag=!gliding,tag=!levitating,tag=!vanish,tag=!swimming] ~~~ detect ~~-1~ water 0 list`, World.getDimension("overworld"));
-                    flag(player, "Jesus", "B", "Movement", "yMotion", Math.abs(player.velocity.y).toFixed(4), true, false);
-                }
+                try {
+	                // Make sure Anti Jesus is turned on
+	                Commands.run(`testfor @a[name="${player.nameTag}",scores={jesus=1..}]`, World.getDimension("overworld")); 
+	                if (!playerTags.includes('flying') && !playerTags.includes('jump') && !playerTags.includes('ground') && !playerTags.includes('gliding') && !playerTags.includes('levitating') && !playerTags.includes('vanish') && !playerTags.includes('swimming') ) {
+	                    Commands.run(`execute @a[name="${player.nameTag}",tag=!flying,m=!c,tag=!jump,tag=!ground,tag=!gliding,tag=!levitating,tag=!vanish,tag=!swimming] ~~~ detect ~~-1~ water 0 list`, World.getDimension("overworld"));
+	                    flag(player, "Jesus", "B", "Movement", "yMotion", Math.abs(player.velocity.y).toFixed(4), true, false);
+				    }
+                } catch (error2) {}
             }
         } catch (error) {}
 
