b96b045f0e9ae9cc3c8e9b74c252befea22b8b7d
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-01-23 11:05:24 -0500

    Make sure player has correct tags for global ban
    There were some instances where global ban would give you the "By"
    information but if that player was missing the "Reason" information
    then the screen message would say "N/A". This is because we were
    not checking this particular tag. Now we do.

--- scripts/paradox.js
+++ scripts/paradox.js
@@ -104,8 +104,13 @@ setTickInterval(() => {
                 // if they are not tagged then we do that here before we ban
                 Commands.run(`testfor @a[name="${player.nameTag}",tag=!"By:Paradox Anticheat"]`, World.getDimension("overworld"));
                 Commands.run(`tag "${player.nameTag}" add "By:Paradox Anticheat"`, World.getDimension("overworld"));
-                Commands.run(`tag "${player.nameTag}" add "Reason:You are Paradox Anticheat global banned!"`, World.getDimension("overworld"));
             } catch (error) {}
+            try {
+                 // test if they have the tag first or global ban will fail if we attempt to tag with an existing tag
+                // if they are not tagged then we do that here before we ban
+                Commands.run(`testfor @a[name="${player.nameTag}",tag=!"Reason:You are Paradox Anticheat global banned!"]`, World.getDimension("overworld"));
+                Commands.run(`tag "${player.nameTag}" add "Reason:You are Paradox Anticheat global banned!"`, World.getDimension("overworld"));
+            } catch (error2) {}
             banMessage(player);
         }
 
