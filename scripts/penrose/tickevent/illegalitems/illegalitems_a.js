d72b13cc826474f3feb48191c713a9bd056871a7
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-08 13:04:32 -0500

    Don't kick staff if they have illegal items
    This is a bug fix. Naturally we do not want to ban and kick staff members if they have illegal items.

--- scripts/penrose/tickevent/illegalitems/illegalitems_a.js
+++ scripts/penrose/tickevent/illegalitems/illegalitems_a.js
@@ -1,5 +1,6 @@
 import * as Minecraft from "mojang-minecraft";
 import { illegalitems } from "../../../data/itemban.js"
+import { getTags } from "../../../util.js";
 import config from "../../../data/config.js";
 
 const World = Minecraft.World;
@@ -13,6 +14,9 @@ const IllegalItemsA = () => {
             player.nameTag = player.nameTag.replace("\"", "");
             player.nameTag = player.nameTag.replace("\\", "");
 
+            // get all tags of the player
+            let playerTags = getTags(player);
+
             let inventory = player.getComponent('minecraft:inventory').container
             let inventory_items = []
             for (let i = 0; i < inventory.size; i++) {
@@ -24,7 +28,7 @@ const IllegalItemsA = () => {
             // If player has an illegal item we clear their entire inventory then kick them
             // If we cannot kick them then we despawn them (no mercy)
             inventory_items.forEach(item => {
-                if (illegalitems.includes(item) || item > config.modules.illegalitemsA.maxStack) {
+                if (illegalitems.includes(item) && !playerTags.includes('op') || item > config.modules.illegalitemsA.maxStack && !playerTags.includes('op')) {
                     try {
                         Commands.run(`clear "${player.nameTag}"`, World.getDimension('overworld'))
                         Commands.run(`tag "${player.nameTag}" add "isBanned"`, World.getDimension('overworld'))
