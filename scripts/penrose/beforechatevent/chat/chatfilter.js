b591843568092d239d8b92974ffe2cd887bd9b0a
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-10 15:41:06 -0500

    Changes to chatfilter
    With Gametest we don't need to use tellraw to filter chat messages. So instead of using tellraw we can use the property message: string. Unfortunately I could not find a property to modify the players name without using tellraw. Tests showed that player.nameTag returns the same as msg.sender.nameTag but there doesn't seem to be a specific property of some kind that allows msg.sender to be changed. So in cases where players change player.nameTag we use tellraw instead.

--- scripts/penrose/beforechatevent/chat/chatfilter.js
+++ scripts/penrose/beforechatevent/chat/chatfilter.js
@@ -7,14 +7,18 @@ const ChatFilter = () => {
 
         const player = msg.sender;
 
-        // add's user custom tags to their messages if it exists or we fall back
-        // also filter for non ASCII characters and remove them in messages
+        // Modify the message before broadcasting
+        // Add custom tags to their messages if it exists or we fall back
+        // Filter for non ASCII characters and remove them in messages
         if (player.name && player.name !== player.nameTag && !msg.cancel) {
+            // We can't modify the name of the messenger. Might be hardcoded. So we use tellraw.
+            // This will be used if player.nameTag is modified
             player.dimension.runCommand(`tellraw @a {"rawtext":[{"text":"${player.nameTag} ${msg.message.replace(/[^\x00-\xFF]/g, "")}"}]}`);
             msg.cancel = true;
-        } else if (player.name && player.name === player.nameTag && !msg.cancel) {
-            player.dimension.runCommand(`tellraw @a {"rawtext":[{"text":"<${player.nameTag}> ${msg.message.replace(/[^\x00-\xFF]/g, "")}"}]}`);
-            msg.cancel = true;
+        } else if (player.name && player.name === player.nameTag) {
+            // We can modify the message without using tellraw
+            // This will be used if player.nameTag is not modified
+            msg.message = `${msg.message.replace(/[^\x00-\xFF]/g, "")}`
         }
     })
 }
