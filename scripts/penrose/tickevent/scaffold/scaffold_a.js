ae7f5ed56de91cbd904923195d7ccb900b81a741
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2022-02-10 20:13:52 -0700

    Update scaffold_a.js
    Fixed a missing ! which I removed during testing (replace tag=!op with tag=op line 21 in case you want to test without getting banned).

--- scripts/penrose/tickevent/scaffold/scaffold_a.js
+++ scripts/penrose/tickevent/scaffold/scaffold_a.js
@@ -18,7 +18,7 @@ const ScaffoldA = () => {
     World.events.blockPlace.subscribe(block => {
         block.player.runCommand(`scoreboard players add @s bpps 1`);  
         try {
-            block.player.runCommand(`execute @s[tag=op,scores={bpps=13..}] ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(${block.block.id}) §4Scaffold. VL= "},{"score":{"name":"@s","objective":"cbevl"}}]}`);
+            block.player.runCommand(`execute @s[tag=!op,scores={bpps=13..}] ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(${block.block.id}) §4Scaffold. VL= "},{"score":{"name":"@s","objective":"cbevl"}}]}`);
             block.player.runCommand(`tag @a[tag=!op,scores={bpps=13..}] add isBanned`);  
             block.player.runCommand(`tellraw @a[tag=packetlogger] {"rawtext":[{"text":"§߈§r§4[§6Paradox§4]§r §bRecieved §6BLOCK PLACE§r packet (${block.block.x} ${block.block.y} ${block.block.z}) from: "},{"selector":"@s"},{" §7(type=${block.block.id})"}]}`); 
         } catch (error) {}
