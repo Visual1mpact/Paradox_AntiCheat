024bda96f05e257fd5bb1f4bad50335fbf6e8790
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2022-02-10 20:12:32 -0700

    Update placeflags_a.js
    I forgot to exclude staff members from the check.

--- scripts/penrose/tickevent/placeflags/placeflags_a.js
+++ scripts/penrose/tickevent/placeflags/placeflags_a.js
@@ -6,8 +6,8 @@ const World = Minecraft.world;
 const PlaceflagsA = () => {
     World.events.blockPlace.subscribe(block => {
         if(illegalitems.includes(block.block.id)) {
-            block.player.runCommand(`scoreboard players add @s cbevl 1`);
-            block.player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(${block.block.id}) §4CBE. VL= "},{"score":{"name":"@s","objective":"cbevl"}}]}`);
+            block.player.runCommand(`scoreboard players add @s[tag=!op] cbevl 1`);
+            block.player.runCommand(`execute @s[tag=!op] ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(${block.block.id}) §4CBE. VL= "},{"score":{"name":"@s","objective":"cbevl"}}]}`);
             block.player.runCommand(`setblock ${block.block.x} ${block.block.y} ${block.block.z} air`);
         }
     })
