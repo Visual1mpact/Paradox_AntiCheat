14ac656ccc16f702518f75d2adb109826c95d9e4
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-11 23:17:10 -0500

    Fix scoreboard creation when logging on
    For unknown reasons to me the list needed to get split up. Not all entries would complete in the original compiled list. However, this fixes it. I plan to revisit this and maybe find another solution that I feel is cleaner. Main thing is getting Paradox up and running again for more development.

---
+++ scripts/data/onjoindata3.js
@@ -0,0 +1,19 @@
+// Don't exceed beyond 40 entries on the list
+export const onJoinData3 = [
+    'scoreboard players operation @a frostwalker = paradox:config frostwalker',
+    'scoreboard players operation @a worldborder = paradox:config worldborder',
+    'scoreboard players operation @a gma = paradox:config gma',
+    'scoreboard players operation @a gmc = paradox:config gmc',
+    'scoreboard players operation @a gms = paradox:config gms',
+    'scoreboard players operation @a cmds = paradox:config cmds',
+    'scoreboard players operation @a bedrock = paradox:config bedrock',
+    'scoreboard players operation @a autoclicker = paradox:config autoclicker',
+    'scoreboard players operation @a jesus = paradox:config jesus',
+    'scoreboard players operation @a phase = paradox:config phase',
+    'scoreboard players operation @a gametestapi = paradox:config gametestapi',
+    'scoreboard players operation @a encharmor = paradox:config encharmor',
+    'scoreboard players operation @a autoaura = paradox:config autoaura',
+    'scoreboard players operation @a antikb = paradox:config antikb',
+    'event entity @s[tag=vanish] vanish',
+    'ability @s[tag=flying] mayfly true'
+]
--- scripts/penrose/playerjoinevent/onjoin/onjoin2.js
+++ scripts/penrose/playerjoinevent/onjoin/onjoin2.js
@@ -1,5 +1,5 @@
 import * as Minecraft from "mojang-minecraft";
-import { onJoinData } from "../../../data/onjoindata.js";
+import { onJoinData2 } from "../../../data/onjoindata2.js";
 
 const World = Minecraft.world;
 
@@ -8,7 +8,7 @@ const tickEventCallback = World.events.tick;
 // This is to allow passing between functions
 let player;
 
-// This function will be called when tick event is triggered from the onJoin function
+// This function will be called when tick event is triggered from the onJoin2 function
 function time() {
     try {
         // We loop testfor until it returns true so we know the
@@ -17,7 +17,7 @@ function time() {
         player.runCommand(`testfor @a[name="${player.name}"]`);
         try {
             // We execute each command in the list
-            onJoinData.forEach(list => {
+            onJoinData2.forEach(list => {
                 player.runCommand(`${list}`);
             })
             tickEventCallback.unsubscribe(time)
@@ -27,11 +27,11 @@ function time() {
 }
 
 // This function will be called when playerJoin event is triggered
-function onJoin(loaded) {
+function onJoin2(loaded) {
     // Get the name of the player who is joining
     player = loaded.player;
     // Subscribe tick event to the time function
     tickEventCallback.subscribe(time)
 }
 
-export { onJoin }
+export { onJoin2 }
