6c2241e0d39b76ea8ab6557ed134856769de2ea3
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-13 20:50:41 -0500

    Fix typo for Fly/B flag
    Some additional changes too with missing semi-colons and spaces.

--- scripts/penrose/tickevent/fly/fly_b.js
+++ scripts/penrose/tickevent/fly/fly_b.js
@@ -1,4 +1,3 @@
-//bridge-file-version: #23
 import * as Minecraft from "mojang-minecraft";
 import { getTags } from "../../../util.js";
 import { setTickInterval } from "../../../timer/scheduling.js";
@@ -7,7 +6,7 @@ const World = Minecraft.world;
 
 function time() {
     try {
-        player.dimension.runCommand(`execute @a[name="${player.nameTag}",scores={fly_timer=6..}] ~~~ detect ~~-1~ air 0 tp @s ~ ~-1 ~ true`)
+        player.dimension.runCommand(`execute @a[name="${player.nameTag}",scores={fly_timer=6..}] ~~~ detect ~~-1~ air 0 tp @s ~ ~-1 ~ true`);
     } catch (error) {}
 }
 
@@ -22,15 +21,15 @@ const FlyB = () => {
             // get all tags of the player
             let playerTags = getTags(player);
 
-            //Fun trick here so that we don't false flag /ability @s mayfly true users
-            //It works because hacks add y vel to the player to stay in the air, and it stays between 1-3 whereas mayfly will have a steady score of 0
-            //Will still false flag sometimes, but that's why we have !fly
+            // Fun trick here so that we don't false flag /ability @s mayfly true users
+            // It works because hacks add y vel to the player to stay in the air, and it stays between 1-3 whereas mayfly will have a steady score of 0
+            // Will still false flag sometimes, but that's why we have !fly
            if(Math.abs(player.velocity.y).toFixed(4) != 0.0000) { 
                 if(!playerTags.includes('riding') && !playerTags.includes('ground') && !playerTags.includes('gliding') && !playerTags.includes('levitating') && !playerTags.includes('flying')) {
                     try {
                         player.dimension.runCommand(`execute @a[name="${player.nameTag}"] ~~~ detect ~~-1~ air 0 execute @s ~~~ detect ~~-2~ air 0 execute @s ~~~ detect ~~-3 ~ air 0 execute @s ~~~ detect ~~-4~ air 0 execute @s ~~~ detect ~~-5~ air 0 scoreboard players add @s fly_timer 1`);
                         player.dimension.runCommand(`execute @a[name="${player.nameTag}",scores={fly_timer=6..}] ~~~ detect ~~-1~ air 0 execute @s ~~~ detect ~~-2~ air 0 execute @s ~~~ detect ~~-3 ~ air 0 execute @s ~~~ detect ~~-4~ air 0 execute @s ~~~ detect ~~-5~ air 0 scoreboard players add @s flyvl 1`);
-                        player.dimension.runCommand(`execute @a[name="${player.nameTag}",scores={fly_timer=6..}] ~~~ detect ~~-1~ air 0 execute @s ~~~ detect ~~-2~ air 0 execute @s ~~~ detect ~~-3 ~ air 0 execute @s ~~~ detect ~~-4~ air 0 execute @s ~~~ detect ~~-5~ air 0 tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(Movement) §4Fly/A. VL= "},{"score":{"name":"@s","objective":"flyvl"}}]}`);
+                        player.dimension.runCommand(`execute @a[name="${player.nameTag}",scores={fly_timer=6..}] ~~~ detect ~~-1~ air 0 execute @s ~~~ detect ~~-2~ air 0 execute @s ~~~ detect ~~-3 ~ air 0 execute @s ~~~ detect ~~-4~ air 0 execute @s ~~~ detect ~~-5~ air 0 tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(Movement) §4Fly/B. VL= "},{"score":{"name":"@s","objective":"flyvl"}}]}`);
                     } catch(error) {}
                 } else {
                     try {
@@ -39,7 +38,7 @@ const FlyB = () => {
                 }
            }
         }
-    }, 40) //Executes every 2 seconds
-}
+    }, 40); // Executes every 2 seconds
+};
 
-export { FlyB, time }
+export { FlyB, time };
