7ba3ca0bdda6cb1302460eabd475ad7861ca4018
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-15 22:56:11 -0500

    Fix error "No target selector available"
    Additional change from 8be1ebc74b3d4d59645eb14a27f8a39ddc36d3a7

--- scripts/penrose/blockplaceevent/placeflags/placeflags_a.js
+++ scripts/penrose/blockplaceevent/placeflags/placeflags_a.js
@@ -6,7 +6,9 @@ const World = Minecraft.world;
 const PlaceflagsA = () => {
     World.events.blockPlace.subscribe(block => {
         if(illegalitems.includes(block.block.id)) {
-            block.player.runCommand(`scoreboard players add @s[tag=!op] cbevl 1`);
+            try {
+                block.player.runCommand(`scoreboard players add @s[tag=!op] cbevl 1`);
+            } catch (error) {}
             try {
                 block.player.runCommand(`execute @s[tag=!op] ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" §1has failed §7(${block.block.id}) §4CBE. VL= "},{"score":{"name":"@s","objective":"cbevl"}}]}`);
             } catch(error) {}
