1341492f1fb82d54f86e50f2ec03a5b5ef80228c
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-02-27 07:42:49 -0500

    Ignore Anti Spam if player has permission for Paradox
    Just target players with no authority.

--- scripts/penrose/beforechatevent/chat/antispam.js
+++ scripts/penrose/beforechatevent/chat/antispam.js
@@ -21,62 +21,66 @@ const AntiSpam = () => {
         let player = msg.sender;
         let message = msg.message;
 
-        // Increment
-        _player.count++;
+        let tag = player.hasTag('paradoxOpped');
 
-        // Specific to Horion
-        if (message.includes("the best minecraft bedrock utility mod")) {
-            _player.check++;
-        }
+        if (tag) {
+            // Increment
+            _player.count++;
 
-        if (!spamCheck.get(player.nameTag)) {
-            spamCheck.set(player.nameTag, message);
-        } else {
-            let oldChat = spamCheck.get(player.nameTag);
-            if (oldChat === message) {
-                _player.spam++;
-                try {
-                    player.runCommand(`tellraw "${player.nameTag}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"Do not spam chat!"}]}`);
-                } catch (error) {}
-                msg.cancel = true;
-            } else if (_player.check >= 2) {
-                msg.cancel = true;
-                _player.spam = 10;
-                _player.check = 0;
+            // Specific to Horion
+            if (message.includes("the best minecraft bedrock utility mod")) {
+                _player.check++;
+            }
+
+            if (!spamCheck.get(player.nameTag)) {
+                spamCheck.set(player.nameTag, message);
             } else {
-                _player.spam = 0;
-            }
-            if (_player.spam >= 10) {
-                let tags = player.getTags();
-                // This removes old ban tags
-                tags.forEach(t => {
-                    if(t.startsWith("Reason:")) {
-                        player.removeTag(t.slice(1));
-                    }
-                    if(t.startsWith("By:")) {
-                        player.removeTag(t.slice(1));
-                    }
-                });
-                try {
-                    player.runCommand(`clear "${player.nameTag}"`);
-                } catch (error) {}
-                try {
-                    player.runCommand(`tag "${player.nameTag}" add "Reason:Spamming"`);
-                    player.runCommand(`tag "${player.nameTag}" add "By:Paradox"`);
-                    player.addTag('isBanned');
-                } catch (error) {
-                    player.triggerEvent('paradox:kick');
+                let oldChat = spamCheck.get(player.nameTag);
+                if (oldChat === message) {
+                    _player.spam++;
+                    try {
+                        player.runCommand(`tellraw "${player.nameTag}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"Do not spam chat!"}]}`);
+                    } catch (error) {}
+                    msg.cancel = true;
+                } else if (_player.check >= 2) {
+                    msg.cancel = true;
+                    _player.spam = 10;
+                    _player.check = 0;
+                } else {
+                    _player.spam = 0;
                 }
+                if (_player.spam >= 10) {
+                    let tags = player.getTags();
+                    // This removes old ban tags
+                    tags.forEach(t => {
+                        if(t.startsWith("Reason:")) {
+                            player.removeTag(t.slice(1));
+                        }
+                        if(t.startsWith("By:")) {
+                            player.removeTag(t.slice(1));
+                        }
+                    });
+                    try {
+                        player.runCommand(`clear "${player.nameTag}"`);
+                    } catch (error) {}
+                    try {
+                        player.runCommand(`tag "${player.nameTag}" add "Reason:Spamming"`);
+                        player.runCommand(`tag "${player.nameTag}" add "By:Paradox"`);
+                        player.addTag('isBanned');
+                    } catch (error) {
+                        player.triggerEvent('paradox:kick');
+                    }
+                }
+                spamCheck.set(player.nameTag, message);
             }
-            spamCheck.set(player.nameTag, message);
-        }
 
-        if (_player.count >= 2) {
-            msg.cancel = true;
-            try {
-                player.runCommand(`tellraw "${player.nameTag}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"You are sending too many messages! Please slow down!"}]}`);
-            } catch (error) {}
-            return;
+            if (_player.count >= 2) {
+                msg.cancel = true;
+                try {
+                    player.runCommand(`tellraw "${player.nameTag}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"You are sending too many messages! Please slow down!"}]}`);
+                } catch (error) {}
+                return;
+            }
         }
     });
 };
