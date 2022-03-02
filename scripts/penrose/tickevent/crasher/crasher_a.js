e033ee7c46aaca1707f1614e02a3e50d3d0e7c82
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-03-01 20:35:05 -0500

    Update crasher/A to instantly ban

--- scripts/penrose/tickevent/crasher/crasher_a.js
+++ scripts/penrose/tickevent/crasher/crasher_a.js
@@ -13,6 +13,26 @@ const CrasherA = () => {
             // Crasher/A = invalid pos check
             if (Math.abs(player.location.x) > 30000000 || Math.abs(player.location.y) > 30000000 || Math.abs(player.location.z) > 30000000) {
                 flag(player, "Crasher", "A", "Exploit", false, false, true, false);
+                let tags = player.getTags();
+                // This removes old ban tags
+                tags.forEach(t => {
+                    if(t.startsWith("Reason:")) {
+                        player.removeTag(t.slice(1));
+                    }
+                    if(t.startsWith("By:")) {
+                        player.removeTag(t.slice(1));
+                    }
+                });
+                try {
+                    player.runCommand(`clear "${player.nameTag}"`);
+                } catch (error) {}
+                try {
+                    player.runCommand(`tag "${player.nameTag}" add "Reason:Crasher"`);
+                    player.runCommand(`tag "${player.nameTag}" add "By:Paradox"`);
+                    player.addTag('isBanned');
+                } catch (error) {
+                    player.triggerEvent('paradox:kick');
+                }
             }
         }
     });
