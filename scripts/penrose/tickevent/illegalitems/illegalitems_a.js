d9b8ddb7cacf2cfd2f26d70a3d677cc00754fa76
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-03-19 22:49:49 -0400

    Update illegalitems_a.js
    It should be checking the stack size and not the id type when verifying the amount of items found in the container.

--- scripts/penrose/tickevent/illegalitems/illegalitems_a.js
+++ scripts/penrose/tickevent/illegalitems/illegalitems_a.js
@@ -21,7 +21,7 @@ function illegalitemsa() {
             }
             // If player has an illegal item or stacks over 64 then we clear the item and kick them
             // If we cannot kick them then we despawn them (no mercy)
-            if (illegalitems.includes(inventory_item.id) && !player.hasTag('paradoxOpped') || inventory_item.id > config.modules.illegalitemsA.maxStack && !player.hasTag('paradoxOpped')) {
+            if (illegalitems.includes(inventory_item.id) && !player.hasTag('paradoxOpped') || inventory_item.amount > config.modules.illegalitemsA.maxStack && !player.hasTag('paradoxOpped')) {
                 flag(player, "IllegalItems", "A", "Exploit", false, false, false, false);
                 try {
                     inventory.setItem(i, new ItemStack(MinecraftItemTypes.air));
