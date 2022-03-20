b92cd43cea923f20248464a9809a66c36e133109
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-03-20 12:44:18 -0400

    Update illegalitems_c.js
    Better handling for buckets.

--- scripts/penrose/blockplaceevent/illegalitems/illegalitems_c.js
+++ scripts/penrose/blockplaceevent/illegalitems/illegalitems_c.js
@@ -47,7 +47,10 @@ function illegalitemsc(object) {
         rip(player);
     }
     // Check if placed item has a inventory container
-    let inventory = block.getComponent('inventory').container;
+    let inventory;
+    try {
+        inventory = block.getComponent('inventory').container;
+    } catch (error) {}
     if (inventory) {
         for (let i = 0; i < inventory.size; i++) {
             let inventory_item = inventory.getItem(i);
@@ -63,41 +66,9 @@ function illegalitemsc(object) {
             // We don't need to ban these items
             // We replace them instead to delete the NBT so we can still use them safely
             } else if (fishbuckets.includes(inventory_item.id) && !player.hasTag('paradoxOpped')) {
-                if (inventory_item.id === 'minecraft:axolotl_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.axolotlBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:cod_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.codBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:salmon_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.salmonBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:tropical_fish_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.tropicalFishBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:powder_snow_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.powderSnowBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:pufferfish_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.pufferfishBucket));
-                    } catch {}
-                }
-                if (inventory_item.id === 'minecraft:tadpole_bucket') {
-                    try {
-                        inventory.setItem(i, new ItemStack(MinecraftItemTypes.tadpoleBucket));
-                    } catch {}
-                }
+                try {
+                    inventory.setItem(i, new ItemStack(inventory_item.id));
+                } catch {}
             }
         }
     }
