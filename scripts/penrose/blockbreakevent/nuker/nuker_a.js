b5ef5126e28f91bdc2af3f3d425665f99e8c8936
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-03-23 16:27:25 -0400

    Ignore vegetation with nuker/A

--- scripts/penrose/blockbreakevent/nuker/nuker_a.js
+++ scripts/penrose/blockbreakevent/nuker/nuker_a.js
@@ -35,8 +35,66 @@ function nukera(object) {
     // Block dimension and location for permutation
     let blockLoc = dimension.getBlock(new BlockLocation(x, y, z));
 
+    // Ignore vegetation
+    let vegetation = [
+        "minecraft:yellow_flower",
+        "minecraft:red_flower",
+        "minecraft:double_plant",
+        "minecraft:wither_rose",
+        "minecraft:tallgrass",
+        "minecraft:hanging_roots",
+        "minecraft:leaves",
+        "minecraft:leaves2",
+        "minecraft:azalea_leaves",
+        "minecraft:azalea_leaves_flowered",
+        "minecraft:deadbush",
+        "minecraft:cocoa",
+        "minecraft:chorus_plant",
+        "minecraft:chorus_flower",
+        "minecraft:cave_vines",
+        "minecraft:cave_vines_body_with_berries",
+        "minecraft:cave_vines_head_with_berries",
+        "minecraft:glow_berries",
+        "minecraft:carrots",
+        "minecraft:cactus",
+        "minecraft:big_dripleaf",
+        "minecraft:beetroot",
+        "minecraft:bamboo",
+        "minecraft:bamboo_sapling",
+        "minecraft:azalea",
+        "minecraft:flowering_azalea",
+        "minecraft:waterlily",
+        "minecraft:melon_block",
+        "minecraft:melon_stem",
+        "minecraft:potatoes",
+        "minecraft:pumpkin",
+        "minecraft:carved_pumpkin",
+        "minecraft:pumpkin_stem",
+        "minecraft:sapling",
+        "minecraft:seagrass",
+        "minecraft:small_dripleaf_block",
+        "minecraft:spore_blossom",
+        "minecraft:reeds",
+        "minecraft:sweet_berry_bush",
+        "minecraft:sweet_berries",
+        "minecraft:vine",
+        "minecraft:wheat",
+        "minecraft:kelp",
+        "minecraft:crimson_fungus",
+        "minecraft:warped_fungus",
+        "minecraft:glow_lichen",
+        "minecraft:brown_mushroom",
+        "minecraft:red_mushroom",
+        "minecraft:nether_wart",
+        "minecraft:nether_sprouts",
+        "minecraft:crimson_roots",
+        "minecraft:warped_roots",
+        "minecraft:twisting_vines",
+        "minecraft:weeping_vines"
+    ]
+
     // Flag and salvage broken blocks to their original forms
-    if (tiktok.length >= config.modules.antinukerA.max && !player.hasTag('paradoxOpped')) {
+    if (tiktok.length >= config.modules.antinukerA.max && vegetation.indexOf(brokenBlockPermutation.type.id) === -1 && !player.hasTag('paradoxOpped')) {
         flag(player, "Nuker", "A", "Break", false, false, false, false);
         blockLoc.setPermutation(blockID);
         try {
