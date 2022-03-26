bb8040909ea8b8a878f4308b3edb336b3a14390f
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-03-25 20:51:05 -0400

    Add property, nametag, to salvageable
    This will be used when they decide to release 1.18.20 or greater.

--- scripts/data/salvageable.js
+++ scripts/data/salvageable.js
@@ -2,158 +2,202 @@ export default
 {
     'minecraft:axolotl_bucket': {
         name: 'minecraft:axolotl_bucket',
-        data: 0
+        data: 0,
+        nametag: 'axolotl_bucket'
     },
     'minecraft:cod_bucket': {
         name: 'minecraft:cod_bucket',
-        data: 0
+        data: 0,
+        nametag: 'cod_bucket'
     },
     'minecraft:salmon_bucket': {
         name: 'minecraft:salmon_bucket',
-        data: 0
+        data: 0,
+        nametag: 'salmon_bucket'
     },
     'minecraft:tropical_fish_bucket': {
         name: 'minecraft:tropical_fish_bucket',
-        data: 0
+        data: 0,
+        nametag: 'tropical_fish_bucket'
     },
     'minecraft:powder_snow_bucket': {
         name: 'minecraft:powder_snow_bucket',
-        data: 0
+        data: 0,
+        nametag: 'powder_snow_bucket'
     },
     'minecraft:pufferfish_bucket': {
         name: 'minecraft:pufferfish_bucket',
-        data: 0
+        data: 0,
+        nametag: 'pufferfish_bucket'
     },
     'minecraft:tadpole_bucket': {
         name: 'minecraft:tadpole_bucket',
-        data: 0
+        data: 0,
+        nametag: 'tadpole_bucket'
     },
     'minecraft:beehive': {
         name: 'minecraft:beehive',
-        data: 0
+        data: 0,
+        nametag: 'beehive'
     },
     'minecraft:bee_nest': {
         name: 'minecraft:bee_nest',
-        data: 0
+        data: 0,
+        nametag: 'bee_nest'
     },
     'minecraft:oak_sign': {
         name: 'minecraft:oak_sign',
-        data: 0
+        data: 0,
+        nametag: 'oak_sign'
     },
     'minecraft:spruce_sign': {
         name: 'minecraft:spruce_sign',
-        data: 0
+        data: 0,
+        nametag: 'spruce_sign'
     },
     'minecraft:birch_sign': {
         name: 'minecraft:birch_sign',
-        data: 0
+        data: 0,
+        nametag: 'birch_sign'
     },
     'minecraft:jungle_sign': {
         name: 'minecraft:jungle_sign',
-        data: 0
+        data: 0,
+        nametag: 'jungle_sign'
     },
     'minecraft:acacia_sign': {
         name: 'minecraft:acacia_sign',
-        data: 0
+        data: 0,
+        nametag: 'acacia_sign'
     },
     'minecraft:dark_oak_sign': {
         name: 'minecraft:dark_oak_sign',
-        data: 0
+        data: 0,
+        nametag: 'dark_oak_sign'
     },
     'minecraft:crimson_sign': {
         name: 'minecraft:crimson_sign',
-        data: 0
+        data: 0,
+        nametag: 'crimson_sign'
     },
     'minecraft:warped_sign': {
         name: 'minecraft:warped_sign',
-        data: 0
+        data: 0,
+        nametag: 'warped_sign'
     },
     'minecraft:white_banner': {
         name: 'minecraft:white_banner',
-        data: 15
+        data: 15,
+        nametag: 'white_banner'
     },
     'minecraft:orange_banner': {
         name: 'minecraft:orange_banner',
-        data: 14
+        data: 14,
+        nametag: 'orange_banner'
     },
     'minecraft:magenta_banner': {
         name: 'minecraft:magenta_banner',
-        data: 13
+        data: 13,
+        nametag: 'magenta_banner'
     },
     'minecraft:light_blue_banner': {
         name: 'minecraft:light_blue_banner',
-        data: 12
+        data: 12,
+        nametag: 'light_blue_banner'
     },
     'minecraft:yellow_banner': {
         name: 'minecraft:yellow_banner',
-        data: 11
+        data: 11,
+        nametag: 'yellow_banner'
     },
     'minecraft:lime_banner': {
         name: 'minecraft:lime_banner',
-        data: 10
+        data: 10,
+        nametag: 'lime_banner'
     },
     'minecraft:pink_banner': {
         name: 'minecraft:pink_banner',
-        data: 9
+        data: 9,
+        nametag: 'pink_banner'
     },
     'minecraft:gray_banner': {
         name: 'minecraft:gray_banner',
-        data: 8
+        data: 8,
+        nametag: 'gray_banner'
     },
     'minecraft:light_gray_banner': {
         name: 'minecraft:light_gray_banner',
-        data: 7
+        data: 7,
+        nametag: 'light_gray_banner'
     },
     'minecraft:cyan_banner': {
         name: 'minecraft:cyan_banner',
-        data: 6
+        data: 6,
+        nametag: 'cyan_banner'
     },
     'minecraft:purple_banner': {
         name: 'minecraft:purple_banner',
-        data: 5
+        data: 5,
+        nametag: 'purple_banner'
     },
     'minecraft:blue_banner': {
         name: 'minecraft:blue_banner',
-        data: 4
+        data: 4,
+        nametag: 'blue_banner'
     },
     'minecraft:brown_banner': {
         name: 'minecraft:brown_banner',
-        data: 3
+        data: 3,
+        nametag: 'brown_banner'
     },
     'minecraft:green_banner': {
         name: 'minecraft:green_banner',
-        data: 2
+        data: 2,
+        nametag: 'green_banner'
     },
     'minecraft:red_banner': {
         name: 'minecraft:red_banner',
-        data: 1
+        data: 1,
+        nametag: 'red_banner'
     },
     'minecraft:black_banner': {
         name: 'minecraft:black_banner',
-        data: 0
+        data: 0,
+        nametag: 'black_banner'
     },
     'minecraft:armor_stand': {
         name: 'minecraft:armor_stand',
-        data: 0
+        data: 0,
+        nametag: 'armor_stand'
     },
     'minecraft:splash_potion': {
         name: 'minecraft:splash_potion',
-        data: 42
+        data: 42,
+        nametag: 'splash_potion'
     },
     'minecraft:potion': {
         name: 'minecraft:potion',
-        data: 42
+        data: 42,
+        nametag: 'potion'
     },
     'minecraft:lingering_potion': {
         name: 'minecraft:lingering_potion',
-        data: 42
+        data: 42,
+        nametag: 'lingering_potion'
     },
     'minecraft:writable_book': {
         name: 'minecraft:writable_book',
-        data: 0
+        data: 0,
+        nametag: 'writable_book'
     },
     'minecraft:furnace': {
         name: 'minecraft:furnace',
-        data: 0
-    }
+        data: 0,
+        nametag: 'furnace'
+    }/*,
+    'minecraft:crossbow': {
+        name: 'minecraft:crossbow',
+        data: 0,
+        nametag: 'crossbow'
+    }*/
 };
