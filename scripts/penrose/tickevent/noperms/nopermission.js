6b7b3425cf4b469e1f4ec012e18a1bac4af25aff
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-04-03 17:33:37 -0400

    Potential workaround for a bug in minecraft
    There seems to be a bug in Minecraft that causes a player entity to briefly return as an entity but not of a player type during the tick. It appears to happen when the angle of the player returns incorrectly. When this happens the script would not recognize the entity as a player and subsequently remove the tag from staff. This patch now attempts to work around the issue by gathering a list of players online filtered out by the tag then verifies if the entity nameTag matches the player as a fail safe to see if the entity is in fact a player or not to prevent accidentally removing the tag due to this unusual bug. [Still testing]

--- scripts/penrose/tickevent/noperms/nopermission.js
+++ scripts/penrose/tickevent/noperms/nopermission.js
@@ -5,11 +5,19 @@ const World = world;
 function noperms() {
     let filter = new EntityQueryOptions();
     filter.tags = ['paradoxOpped'];
+    // We need a list of players for checking behind a bug in Minecraft
+    let playerArray = [...World.getPlayers(filter)];
     // Let's check the entities for illegal permissions
     // Apparently all dimensions are checked even though we target overworld
     for (let entity of World.getDimension('overworld').getEntities(filter)) {
         // If it's a player then ignore
         if (entity instanceof Player) {
+            continue;
+        }
+        // This covers a bug that exists in Minecraft where for a brief tick the player will not return as a player entity
+        // This bug would essentially cause this script to remove permissions from staff unintentionally
+        if (playerArray.includes(entity.nameTag)) {
+            // Skip to the next entity since this is a bug in Minecraft
             continue;
         }
         entity.removeTag('paradoxOpped');
