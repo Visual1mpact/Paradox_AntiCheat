229df274565a1844a83a67443efeca01fdd66356
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-04-09 12:37:38 -0400

    Update anti jesus
    Modified logic for detection. Minecraft allows, whether intentional or not, for players to bunny hop across water. So this feature will naturally flag such players as well. Instead of giving the player a violation we just silently teleport them under water to force them to swim or use another object such as a boat to get across the water because ain't nobody bunny hopping across water in real life anyways.

--- scripts/penrose/tickevent/jesus/jesus_a.js
+++ scripts/penrose/tickevent/jesus/jesus_a.js
@@ -1,13 +1,11 @@
 import { world, Location, BlockLocation } from "mojang-minecraft";
-import { flag } from "../../../util.js";
 import { setTickInterval } from "../../../timer/scheduling.js";
 import config from "../../../data/config.js";
 
 const World = world;
 
+let BlockAtPlayer0
 let BlockAtPlayer1;
-let BlockAtPlayer2;
-let BlockAtPlayer3;
 
 const _player = {
     count: 0
@@ -15,7 +13,6 @@ const _player = {
 
 function timer(player, dimension, x, y, z) {
     player.teleport(new Location(x, y - 2, z), dimension, 0, player.bodyRotation);
-    flag(player, "Jesus", "A", "Movement", false, false, false, false, false ,false);
     _player.count = 0;
 }
 
@@ -32,18 +29,16 @@ function jesusa(){
         const z = Math.floor(player.location.z);
         const dimension = player.dimension;
         try {
+            // Below Below player
+            BlockAtPlayer0 = player.dimension.getBlock(new BlockLocation(x, y - 1, z));
             // Below player
             BlockAtPlayer1 = player.dimension.getBlock(new BlockLocation(x, y, z));
-            // Mid player
-            BlockAtPlayer2 = player.dimension.getBlock(new BlockLocation(x, y + 1, z));
-            // Above player
-            BlockAtPlayer3 = player.dimension.getBlock(new BlockLocation(x, y + 2, z));
         } catch (error) {}
 
-        if (!player.hasTag('paradoxOpped') && !player.hasTag('vanish') && !player.hasTag('swimming') && !player.hasTag('riding') && !player.hasTag('flying') && BlockAtPlayer1.type.id === "minecraft:water" && BlockAtPlayer3.type.id != "minecraft:water" && BlockAtPlayer2.type.id != "minecraft:water" || !player.hasTag('paradoxOpped') && !player.hasTag('vanish') && !player.hasTag('swimming') && !player.hasTag('riding') && !player.hasTag('flying') && BlockAtPlayer1.type.id === "minecraft:lava" && BlockAtPlayer3.type.id != "minecraft:lava" && BlockAtPlayer2.type.id != "minecraft:lava") {
+        if (!player.hasTag('paradoxOpped') && !player.hasTag('vanish') && !player.hasTag('swimming') && !player.hasTag('riding') && !player.hasTag('flying') && BlockAtPlayer1.type.id === "minecraft:water" && BlockAtPlayer0.type.id === "minecraft:water" || !player.hasTag('paradoxOpped') && !player.hasTag('vanish') && !player.hasTag('swimming') && !player.hasTag('riding') && !player.hasTag('flying') && BlockAtPlayer1.type.id === "minecraft:lava" && BlockAtPlayer0.type.id === "minecraft:lava") {
             _player.count++;
-            // Flag them after 4 seconds of activity
-            if (_player.count === 2) {
+            // Flag them after 2 seconds of activity
+            if (_player.count === 1) {
                 timer(player, dimension, x, y, z);
             }
         }
