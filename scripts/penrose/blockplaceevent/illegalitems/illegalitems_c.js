1dae50f6c1619b35b5510076ee15746fefa03715
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-04-19 21:54:48 -0400

    Update illegalitems_c for illegal enchantments
    While checking the items in containers we quickly verify if each item is enchanted, then we verify what type of enchantment it has and what level that enchantment type is. If the level of that enchantment exceeds or breaks vanilla then we remove the stack from the container immediately, warn them that this is not allowed, then pass this information over to staff so they are aware about the player who had the illegally enchanted item.

--- scripts/penrose/blockplaceevent/illegalitems/illegalitems_c.js
+++ scripts/penrose/blockplaceevent/illegalitems/illegalitems_c.js
@@ -1,4 +1,4 @@
-import { world, BlockLocation, MinecraftBlockTypes, MinecraftItemTypes, ItemStack, Items } from "mojang-minecraft";
+import { world, BlockLocation, MinecraftBlockTypes, MinecraftItemTypes, ItemStack, Items, MinecraftEnchantmentTypes } from "mojang-minecraft";
 import { illegalitems } from "../../../data/itemban.js";
 import salvageable from "../../../data/salvageable.js";
 import config from "../../../data/config.js";
@@ -74,6 +74,34 @@ function illegalitemsc(object) {
                 inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 1));
                 pl.verify = 1;
                 continue;
+            }
+            // We get a list of enchantments on this item
+            let item_enchants = inventory_item.getComponent("minecraft:enchantments").enchantments;
+            // Check if enchantment is illegal on item
+            if (item_enchants) {
+                for (let enchants in MinecraftEnchantmentTypes) {
+                    // If no enchantment then move to next loop
+                    let enchanted = MinecraftEnchantmentTypes[enchants];
+                    if (!item_enchants.hasEnchantment(enchanted)) {
+                        continue;
+                    }
+                    // Get properties of this enchantment
+                    let enchant_data = item_enchants.getEnchantment(MinecraftEnchantmentTypes[enchants]);
+                    // Does the enchantment type exceed or break vanilla levels
+                    if (enchant_data && enchant_data.level > MinecraftEnchantmentTypes[enchants].maxLevel || enchant_data && enchant_data.level < 0) {
+                        // Remove this item immediately
+                        try {
+                            inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 0));
+                        } catch {}
+                        // Use try/catch in case nobody has tag 'notify' as this will report 'no target selector'
+                        try {
+                            player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"\n§r§4[§6Paradox§4]§r §4[§f${disabler(player.nameTag)}§4]§r §6=>§r §4[§f${block.id.replace("minecraft:", "")}§4]§r §6=>§r §4[§fSlot§4]§r ${i}§r §6=>§r §4[§f${inventory_item.id.replace("minecraft:", "")}§4]§r §6Enchanted: §4${enchant_data.type.id}=${enchant_data.level}§r"}]}`);
+                            player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r Removed §4[§f${inventory_item.id.replace("minecraft:", "")}§4]§r from ${disabler(player.nameTag)}."}]}`);
+                        } catch (error) {}
+                        player.runCommand(`tellraw "${disabler(player.nameTag)}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r Illegal enchantments are not allowed!"}]}`);
+                        continue;
+                    }
+                }
             }
             // Check if item found inside the container is illegal
             if (illegalitems.includes(inventory_item.id) && !player.hasTag('paradoxOpped')) {
