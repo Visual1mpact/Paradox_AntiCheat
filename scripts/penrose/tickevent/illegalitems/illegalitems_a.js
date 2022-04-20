f5c4407febce565afdb21850a8cb258ad7952c21
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-04-19 20:42:35 -0400

    Update illegalitems_a for illegal enchantments
    While checking the player's inventory we quickly verify if each item is enchanted, then we verify what type of enchantment it has and what level that enchantment type is. If the level of that enchantment exceeds or breaks vanilla then we remove the stack from them immediately, warn them that this is not allowed, then pass this information over to staff so they are aware about the player who had the illegally enchanted item.

--- scripts/penrose/tickevent/illegalitems/illegalitems_a.js
+++ scripts/penrose/tickevent/illegalitems/illegalitems_a.js
@@ -1,4 +1,4 @@
-import { world, ItemStack, MinecraftItemTypes, Items } from "mojang-minecraft";
+import { world, ItemStack, MinecraftItemTypes, Items, MinecraftEnchantmentTypes } from "mojang-minecraft";
 import { illegalitems } from "../../../data/itemban.js";
 import salvageable from "../../../data/salvageable.js";
 import config from "../../../data/config.js";
@@ -49,11 +49,39 @@ function illegalitemsa() {
             if (!inventory_item) {
                 continue;
             }
+            // We get a list of enchantments on this item
+            let item_enchants = inventory_item.getComponent("minecraft:enchantments").enchantments;
+            // Check if enchantment is illegal on item
+            if (item_enchants) {
+                for (let enchants in MinecraftEnchantmentTypes) {
+                    // If no enchantment then move to next loop
+                    let enchanted = MinecraftEnchantmentTypes[enchants];
+					if (!item_enchants.hasEnchantment(enchanted)) {
+                        continue;
+                    }
+                    // Get properties of this enchantment
+                    let enchant_data = item_enchants.getEnchantment(MinecraftEnchantmentTypes[enchants]);
+                    // Does the enchantment type exceed or break vanilla levels
+                    if (enchant_data && enchant_data.level > MinecraftEnchantmentTypes[enchants].maxLevel || enchant_data && enchant_data.level < 0) {
+                        // Remove this item immediately
+                        try {
+                            inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 0));
+                        } catch {}
+                        // Use try/catch in case nobody has tag 'notify' as this will report 'no target selector'
+                        try {
+                            player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"\n§r§4[§6Paradox§4]§r §4[§f${disabler(player.nameTag)}§4]§r §6=>§r §4[§fSlot§4]§r ${i}§r §6=>§r §4[§f${inventory_item.id.replace("minecraft:", "")}§4]§r §6Enchanted: §4${enchant_data.type.id}=${enchant_data.level}§r"}]}`);
+                            player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r Removed §4[§f${inventory_item.id.replace("minecraft:", "")}§4]§r from ${disabler(player.nameTag)}."}]}`);
+                        } catch (error) {}
+                        player.runCommand(`tellraw "${disabler(player.nameTag)}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r Illegal enchantments are not allowed!"}]}`);
+                        continue;
+                    }
+                }
+            }
             // If shulker boxes are not allowed in the server then we handle this here
             // No need to ban when we can just remove it entirely and it's not officially listed as an illegal item at this moment
             if (config.modules.antishulker.enabled && inventory_item.id === "minecraft:shulker_box" && !player.hasTag('paradoxOpped') || config.modules.antishulker.enabled && inventory_item.id === "minecraft:undyed_shulker_box" && !player.hasTag('paradoxOpped')) {
                 try {
-                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 1));
+                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 0));
                 } catch {}
                 continue;
             }
@@ -62,7 +90,7 @@ function illegalitemsa() {
             if (illegalitems.includes(inventory_item.id) && !player.hasTag('paradoxOpped')) {
                 flag(player, "IllegalItems", "A", "Exploit", inventory_item.id, inventory_item.amount, false, false, false, false);
                 try {
-                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 1));
+                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 0));
                 } catch {}
                 // Ban
                 return rip(player, inventory_item);
@@ -88,7 +116,7 @@ function illegalitemsa() {
             } else if (inventory_item.amount > config.modules.illegalitemsA.maxStack && !player.hasTag('paradoxOpped')) {
                 // Item stacks over 64 we clear them
                 try {
-                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 1));
+                    inventory.setItem(i, new ItemStack(MinecraftItemTypes.air, 0));
                 } catch {}
                 pl.verify = 1;
             }
