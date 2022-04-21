0e6114a9ff07e0ce5eec3fc44ec65eb1f1c659e9
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2022-04-20 21:21:27 -0600

    Added anti-teleport check.
    First, this check is likely going to flag multiple times quickly (fixing that ASAP, figured getting this in now would still be worth it), but is very unlikely to false flag.

--- scripts/data/config.js
+++ scripts/data/config.js
@@ -173,6 +173,10 @@ export default
         },
         "worldBorder": {
             "enabled": true
+        },
+        "antiTeleport": {
+            "enabled": true,
+            "constraint": 50
         }
     }
 };
