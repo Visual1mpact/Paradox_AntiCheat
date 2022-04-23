483c262d9b021bb81b005e6790f3244e31f25082
FrostIce482 <frostice48260@gmail.com>
2022-04-23 15:41:14 -0400

    Add list for enchantments
    This list is a compilation of what items can currently be enchanted from vanilla gameplay, the types of enchantments those items are allowed to have during vanilla gameplay, and their max levels respectively.
    
    This can be customized for realm/server owners who wish to extend, or restrict which of these items can have which enchantments or how high the levels of those enchantments may be. As is, this is set up for a vanilla experience.

---
+++ scripts/data/enchantments.js
@@ -0,0 +1,264 @@
+// https://docs.microsoft.com/en-us/minecraft/creator/scriptapi/mojang-minecraft/enchantmentslot
+export const enchantmentSlot = {
+    // None
+    0: {},
+    // armorHead
+    1: {
+        protection: 4,
+        fireProtection: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        respiration: 3,
+        aquaAffinity: 1,
+        unbreaking: 3,
+        mending: 1,
+        binding: 1,
+        vanishing: 1
+    },
+    // armorTorso
+    2: {
+        protection: 4,
+        fireProtection: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        unbreaking: 3,
+        mending: 1,
+        binding: 1,
+        vanishing: 1
+    },
+    // armorFeet
+    4: {
+        protection: 4,
+        fireProtection: 4,
+        featherFalling: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        depthStrider: 3,
+        unbreaking: 3,
+        frostWalker: 2,
+        mending: 1,
+        binding: 1,
+        vanishing: 1,
+        soulSpeed: 3
+    },
+    // armorLegs
+    8: {
+        protection: 4,
+        fireProtection: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        unbreaking: 3,
+        mending: 1,
+        binding: 1,
+        vanishing: 1
+    },
+    // gArmor
+    15: {
+        protection: 4,
+        fireProtection: 4,
+        featherFalling: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        respiration: 3,
+        depthStrider: 3,
+        aquaAffinity: 1,
+        unbreaking: 3,
+        frostWalker: 2,
+        mending: 1,
+        binding: 1,
+        vanishing: 1,
+        soulSpeed: 3
+    },
+    // sword
+    16: {
+        sharpness: 5,
+        smite: 5,
+        baneOfArthropods: 5,
+        knockback: 2,
+        fireAspect: 2,
+        looting: 3,
+        unbreaking: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // bow
+    32: {
+        unbreaking: 3,
+        power: 5,
+        punch: 2,
+        flame: 1,
+        infinity: 1,
+        mending: 1,
+        vanishing: 1
+    },
+    // hoe
+    64: {
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // shears
+    128: {
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // flintsteel
+    256: {
+        unbreaking: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // axe
+    512: {
+        sharpness: 5,
+        smite: 5,
+        baneOfArthropods: 5,
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // pickaxe
+    1024: {
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // shovel
+    2048: {
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // gDigging
+    3648: {
+        sharpness: 5,
+        smite: 5,
+        baneOfArthropods: 5,
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // fishingRod
+    4096: { 
+        unbreaking: 3,
+        luckOfTheSea: 3,
+        lure: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // carrotStick
+    8192: { 
+        unbreaking: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // elytra
+    16384: { 
+        unbreaking: 3,
+        mending: 1,
+        binding: 1,
+        vanishing: 1
+    },
+    // spear
+    32768: {
+      unbreaking: 3,
+      mending: 1,
+      vanishing: 1,
+      impaling: 5,
+      riptide: 3,
+      loyalty: 3,
+      channeling: 1
+    },
+    // crossbow
+    65536: {
+      unbreaking: 3,
+      mending: 1,
+      vanishing: 1,
+      multishot: 1,
+      piercing: 4,
+      quickCharge: 3
+    },
+    // shield
+    131072: {
+        unbreaking: 3,
+        mending: 1,
+        vanishing: 1
+    },
+    // gTool
+    131520: {
+      efficiency: 5,
+      silkTouch: 1,
+      unbreaking: 3,
+      fortune: 3,
+      mending: 1,
+      vanishing: 1
+    },
+    // cosmeticHead
+    262144: {
+        binding: 1,
+        vanishing: 1
+    },
+    // All
+    '-1': {
+        protection: 4,
+        fireProtection: 4,
+        featherFalling: 4,
+        blastProtection: 4,
+        projectileProtection: 4,
+        thorns: 3,
+        respiration: 3,
+        depthStrider: 3,
+        aquaAffinity: 1,
+        sharpness: 5,
+        smite: 5,
+        baneOfArthropods: 5,
+        knockback: 2,
+        fireAspect: 2,
+        looting: 3,
+        efficiency: 5,
+        silkTouch: 1,
+        unbreaking: 3,
+        fortune: 3,
+        power: 5,
+        punch: 2,
+        flame: 1,
+        infinity: 1,
+        luckOfTheSea: 3,
+        lure: 3,
+        frostWalker: 2,
+        mending: 1,
+        binding: 1,
+        vanishing: 1,
+        impaling: 5,
+        riptide: 3,
+        loyalty: 3,
+        channeling: 1,
+        multishot: 1,
+        piercing: 4,
+        quickCharge: 3,
+        soulSpeed: 3
+    }
+}
