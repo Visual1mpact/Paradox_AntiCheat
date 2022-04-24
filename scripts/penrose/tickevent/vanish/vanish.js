ed3f6e6e877e02d41c0cf7d9c1f4838e77e3b499
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-04-24 09:12:16 -0400

    Update vanish
    Conditional checking

--- scripts/penrose/tickevent/vanish/vanish.js
+++ scripts/penrose/tickevent/vanish/vanish.js
@@ -1,18 +1,33 @@
 import { world, MinecraftEffectTypes, EntityQueryOptions, EffectType} from "mojang-minecraft";
+import { disabler } from "../../../util.js";
 
 const World = world;
 
 function vanish() {
     // Filter for only players who are vanished
     let filter = new EntityQueryOptions();
-    filter.tags = ['vanish', 'paradoxOpped'];
+    filter.tags = ['vanish'];
     // Run as each player
     for (let player of World.getPlayers(filter)) {
         // Grant them invisibility and night vision
         // 1728000 = 24 hours
-        player.addEffect(MinecraftEffectTypes.invisibility, 1728000, 255);
-        player.addEffect(MinecraftEffectTypes.nightVision, 1728000, 255);
-        player.runCommand(`title @s actionbar §6YOU ARE VANISHED!`);
+        if (player.hasTag('paradoxOpped')) {
+            player.addEffect(MinecraftEffectTypes.invisibility, 1728000, 255);
+            player.addEffect(MinecraftEffectTypes.nightVision, 1728000, 255);
+            player.runCommand(`title @s actionbar §6YOU ARE VANISHED!`);
+        }
+        // Make sure they have permission to use Vanish
+        if (!player.hasTag('paradoxOpped')) {
+            // They have been busted!
+            player.removeTag('vanish')
+            if (player.getEffect(MinecraftEffectTypes.invisibility) || player.getEffect(MinecraftEffectTypes.nightVision)) {
+                player.runCommand(`effect "${disabler(player.nameTag)}" clear`);
+            }
+            // Use try/catch in case nobody has tag 'notify' as this will report 'no target selector'
+            try {
+                player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"${disabler(player.nameTag)} had unauthorized permissions for Vanish. Permission removed!"}]}`);
+            } catch (error) {}
+        }
     }
 }
 
