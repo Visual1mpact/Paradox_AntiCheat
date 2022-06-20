11805f96d1afb1ce89f26490764aec88010d3a74
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-06-20 19:26:26 -0400

    Revert "Revert "Add Dynamic Properties for spammera""
    This reverts commit 1516bd235188de03afd3f8f24725e131d4de971a.

--- scripts/commands/settings/spammera.js
+++ scripts/commands/settings/spammera.js
@@ -1,7 +1,10 @@
 import { crypto, disabler, getPrefix } from "../../util.js";
 import config from "../../data/config.js";
+import { world } from "mojang-minecraft";
 
-function spammerAHelp(player, prefix) {
+const World = world;
+
+function spammerAHelp(player, prefix, spammerABoolean) {
     let commandStatus;
     if (!config.customcommands.spammera) {
         commandStatus = "§6[§4DISABLED§6]§r";
@@ -9,7 +12,7 @@ function spammerAHelp(player, prefix) {
         commandStatus = "§6[§aENABLED§6]§r";
     }
     let moduleStatus;
-    if (!config.modules.spammerA.enabled) {
+    if (spammerABoolean === false) {
         moduleStatus = "§6[§4DISABLED§6]§r";
     } else {
         moduleStatus = "§6[§aENABLED§6]§r";
@@ -49,23 +52,29 @@ export function spammerA(message, args) {
         return player.runCommand(`tellraw "${disabler(player.nameTag)}" {"rawtext":[{"text":"§r§4[§6Paradox§4]§r "},{"text":"You need to be Paradox-Opped to use this command."}]}`);
     }
 
+    // Get Dynamic Property Boolean
+    let spammerABoolean = World.getDynamicProperty('spammera_b');
+    if (spammerABoolean === undefined) {
+        spammerABoolean = config.modules.spammerA.enabled;
+    }
+
     // Check for custom prefix
     let prefix = getPrefix(player);
 
     // Was help requested
     let argCheck = args[0];
     if (argCheck && args[0].toLowerCase() === "help" || !config.customcommands.spammera) {
-        return spammerAHelp(player, prefix);
+        return spammerAHelp(player, prefix, spammerABoolean);
     }
 
-    if (config.modules.spammerA.enabled === false) {
+    if (spammerABoolean === false) {
         // Allow
-        config.modules.spammerA.enabled = true;
+        World.setDynamicProperty('spammera_b', true);
         player.runCommand(`tellraw @a[tag=Hash:${crypto}] {"rawtext":[{"text":"\n§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" has enabled §6SpammerA§r!"}]}`);
         return;
-    } else if (config.modules.spammerA.enabled === true) {
+    } else if (spammerABoolean === true) {
         // Deny
-        config.modules.spammerA.enabled = false;
+        World.setDynamicProperty('spammera_b', false);
         player.runCommand(`tellraw @a[tag=Hash:${crypto}] {"rawtext":[{"text":"\n§r§4[§6Paradox§4]§r "},{"selector":"@s"},{"text":" has disabled §4SpammerA§r!"}]}`);
         return;
     }
