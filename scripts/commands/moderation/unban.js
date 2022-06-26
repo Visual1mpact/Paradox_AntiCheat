dbfdd1c4ff02767b596d8a6d00a0c5991c06520d
FrostIce482 <frostice48260@gmail.com>
2022-06-26 11:11:01 +0700

    fix missing ']'

--- scripts/commands/moderation/unban.js
+++ scripts/commands/moderation/unban.js
@@ -71,5 +71,5 @@ export function unban(message, args) {
     // Add player to queue
     let regexp = /["'`]/g;
     queueUnban.add(args.join(" ").replace(regexp, ""));
-    sendMsg('@a[tag=paradoxOpped', `§r§4[§6Paradox§4]§r ${args.join(" ").replace(regexp, "")} is queued to be unbanned!`);
+    sendMsg('@a[tag=paradoxOpped]', `§r§4[§6Paradox§4]§r ${args.join(" ").replace(regexp, "")} is queued to be unbanned!`);
 }
