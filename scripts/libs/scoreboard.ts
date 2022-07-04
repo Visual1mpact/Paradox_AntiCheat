5f9e092aa5209632476b809aa40e5a0547f36883
FrostIce482 <92860250+frostice482@users.noreply.github.com>
2022-07-04 22:26:48 +0700

    Update scoreboard.ts

--- scripts/libs/scoreboard.ts
+++ scripts/libs/scoreboard.ts
@@ -2,8 +2,7 @@
 // transpiled from typescript to javascript
 
 // https://github.com/frostice482/server-expansion/blob/main/dev/src/libcore/mc.ts#L33
-/** @type { (command: string, source?: Dimension, ignoreError?: boolean) => {statusCode: number, statusMessage: string} } */
-const execCmd: (command: string, source?: Dimension, ignoreError?: boolean) => { statusCode: number; statusMessage: string; } = (command: string, source = dim, ignoreError = false): { statusCode: number; statusMessage: string; } => {
+const execCmd = (command: string, source: Dimension = dim, ignoreError = false): { statusCode: number; statusMessage: string; } => {
     try { return source.runCommand(command); }
     catch (e) {
         if (e instanceof Error) throw e;
@@ -17,139 +16,145 @@ const execCmd: (command: string, source?: Dimension, ignoreError?: boolean) => {
     }
 };
 
-import { Dimension, ScoreboardIdentityType, world } from 'mojang-minecraft';
-const dim = world.getDimension('overworld');
-const auth = Symbol();
-export default class scoreboard {
-    static get display() { return display; }
-    static get objective() { return objective; }
-    constructor() { throw new TypeError('Class is not constructable'); }
-}
-const toExecutable = JSON.stringify;
-class objective {
-	public exist: any;
-	public edit: any;
-    static create = (id: any, displayName = id) => new this(id, displayName, true);
-    static edit = (id: any) => new this(id, id, false);
-    static for = (id: any, displayName = id) => new this(id, displayName, !this.exist(id));
-    static exist = (id: any) => {
-        id = toExecutable(id);
-        try {
-            execCmd(`scoreboard objectives add ${id} dummy`);
-            execCmd(`scoreboard objectives remove ${id}`);
-            return false;
-        }
-        catch {
-            return true;
-        }
-    };
-    static delete = (id: any) => !execCmd(`scoreboard objectives remove ${toExecutable(id)}`, dim, true).statusCode;
-    static getList = () => world.scoreboard.getObjectives().map(v => this.edit(v.id));
-    constructor(id: any, displayName = id, create = true) {
-        if (id.length > 16)
-            throw new RangeError(`Objective identifier length cannot go more than 16 characters`);
-        if (displayName.length > 32)
-            throw new RangeError(`Objective display length cannot go more than 32 characters`);
-        const execid = toExecutable(id), execDisplay = toExecutable(displayName);
-        const exist = objective.exist(id);
-        if (create) {
-            if (exist)
-                throw new TypeError(`Objective with ID '${id}' already exists.`);
-            execCmd(`scoreboard objectives add ${execid} dummy ${execDisplay}`);
-        }
-        else if (!exist)
-                throw new ReferenceError(`Objective with ID '${id}' not found.`);
-        this.id = id;
-        this.execId = execid;
-        this.#data = world.scoreboard.getObjective(id);
-        this.dummies = new dummies(auth, this, this.#data);
-        this.players = new players(auth, this, this.#data);
-    }
-    #data: any;
-    id: any;
-    execId: any;
-    get displayName() { return this.#data.displayName; }
-    get data() { return this.#data; }
-    dummies: any;
-    players: any;
-    display = new display(auth, this);
-}
-class players {
-	public participant: any;
-	public score: any;
+import { ScoreboardIdentityType, ScoreboardObjective, world } from 'mojang-minecraft'
+import { Player } from 'mojang-minecraft'
 
-    constructor(key: any, obj: any, data: any) {
-        if (key !== auth)
-            throw new TypeError('Class is not constructable');
-        this.#obj = obj;
-        this.#data = data;
+const auth = Symbol()
+
+export default class scoreboard {
+    static get display() { return display }
+    static get objective() { return objective }
+
+    protected constructor() { throw new TypeError('Class is not constructable') }
+}
+
+type displaySlot = 'sidebar' | 'list' | 'belowname'
+
+const toExecutable = JSON.stringify
+
+class objective {
+    static readonly create = (id: string, displayName = id) => new this(id, displayName, true)
+    static readonly edit = (id: string) => new this(id, id, false)
+    static readonly for = (id: string, displayName = id) => new this(id, displayName, !this.exist(id))
+    static readonly exist = (id: string) => {
+        id = toExecutable(id)
+        try {
+            execCmd(`scoreboard objectives add ${id} dummy`)
+            execCmd(`scoreboard objectives remove ${id}`)
+            return false
+        } catch {
+            return true
+        }
     }
-    #obj: any;
-    #data: any;
-    'set' = (plr: any, score: any) => (void execCmd(`scoreboard players set @s ${this.#obj.execId} ${score}`, plr, true), this);
-    add = (plr: any, score: any) => (void execCmd(`scoreboard players add @s ${this.#obj.execId} ${score}`, plr, true), this);
-    'get' = (plr: any) => {
-        const r = execCmd(`scoreboard players test @s ${this.#obj.execId} * *`, plr, true);
-        if (r.statusCode)
-            return;
-        return +r.statusMessage.match(/-?\d+/)?.[0];
-    };
-    exist = (plr: any) => !execCmd(`scoreboard players test @s ${this.#obj.execId} * *`, plr, true).statusCode;
-    delete = (plr: any) => !execCmd(`scoreboard players reset @s ${this.#obj.execId}`, plr, true).statusCode;
-    getScores = (() => {
-        const t = this;
+    static readonly delete = (id: string) => !execCmd(`scoreboard objectives remove ${toExecutable(id)}`, dim.o, true).statusCode
+    static readonly getList = () => world.scoreboard.getObjectives().map(v => this.edit(v.id))
+
+    constructor(id: string, displayName = id, create = true) {
+        if (id.length > 16) throw new RangeError(`Objective identifier length cannot go more than 16 characters`)
+        if (displayName.length > 32) throw new RangeError(`Objective display length cannot go more than 32 characters`)
+
+        const execid = toExecutable(id),
+            execDisplay = toExecutable(displayName)
+
+        const exist = objective.exist(id)
+        if (create) {
+            if (exist) throw new TypeError(`Objective with ID '${id}' already exists.`)
+            execCmd(`scoreboard objectives add ${execid} dummy ${execDisplay}`)
+        } else {
+            if (!exist) throw new ReferenceError(`Objective with ID '${id}' not found.`)
+        }
+
+        this.id = id
+        this.execId = execid
+        this.#data = world.scoreboard.getObjective(id)
+        this.dummies = new dummies(auth, this, this.#data)
+        this.players = new players(auth, this, this.#data)
+    }
+
+    #data: ScoreboardObjective
+
+    readonly id: string
+    readonly execId: string
+    get displayName() { return this.#data.displayName }
+    get data() { return this.#data }
+
+    readonly dummies: dummies
+    readonly players: players
+    readonly display = new display(auth, this)
+}
+
+class players {
+    constructor(key: typeof auth, obj: objective, data: ScoreboardObjective) {
+        if (key !== auth) throw new TypeError('Class is not constructable')
+        this.#obj = obj
+        this.#data = data
+    }
+
+    #obj: objective
+    #data: ScoreboardObjective
+
+    readonly 'set' = (plr: Player, score: number) => ( void execCmd(`scoreboard players set @s ${this.#obj.execId} ${score}`, plr, true), this )
+    readonly add = (plr: Player, score: number) => ( void execCmd(`scoreboard players add @s ${this.#obj.execId} ${score}`, plr, true), this )
+    readonly 'get' = (plr: Player) => {
+        const r = execCmd(`scoreboard players test @s ${this.#obj.execId} * *`, plr, true)
+        if (r.statusCode) return 
+        return +r.statusMessage.match(/-?\d+/)?.[0]
+    }
+
+    readonly exist = (plr: Player) => !execCmd(`scoreboard players test @s ${this.#obj.execId} * *`, plr, true).statusCode
+    readonly delete = (plr: Player) => !execCmd(`scoreboard players reset @s ${this.#obj.execId}`, plr, true).statusCode
+    readonly getScores = (() => {
+        const t = this
         return function* () {
             for (const { participant, score } of t.#data.getScores())
                 if (participant.type == ScoreboardIdentityType.player)
-                    try {
-                        yield [participant.getEntity(), score, participant.displayName];
-                    }
-                    catch {
-                        yield [null, score, null];
-                    }
-        };
-    })();
+                    try { yield [participant.getEntity(), score, participant.displayName] as [player: Player, score: number, displayName: string] }
+                    catch { yield [null, score, null] as [player: null, score: number, displayName: null] }
+        }
+    })()
 }
-class dummies {
-	public participant: any;
-	public score: any;
 
-    constructor(key: any, obj: any, data: any) {
-        if (key !== auth)
-            throw new TypeError('Class is not constructable');
-        this.#obj = obj;
-        this.#data = data;
+class dummies {
+    constructor(key: typeof auth, obj: objective, data: ScoreboardObjective) {
+        if (key !== auth) throw new TypeError('Class is not constructable')
+        this.#obj = obj
+        this.#data = data
     }
-    #obj: any;
-    #data: any;
-    'set' = (name: any, score: any) => (void execCmd(`scoreboard players set ${toExecutable(name)} ${this.#obj.execId} ${score}`, dim, true), this);
-    add = (name: any, score: any) => (void execCmd(`scoreboard players add ${toExecutable(name)} ${this.#obj.execId} ${score}`, dim, true), this);
-    'get' = (name: any) => {
-        const r = execCmd(`scoreboard players test ${toExecutable(name)} ${this.#obj.execId} * *`, dim, true);
-        if (r.statusCode)
-            return;
-        return +r.statusMessage.match(/-?\d+/)?.[0];
-    };
-    exist = (name: any) => !execCmd(`scoreboard players test ${toExecutable(name)} ${this.#obj.execId} * *`, dim, true).statusCode;
-    delete = (name: any) => !execCmd(`scoreboard players reset ${toExecutable(name)} ${this.#obj.execId}`, dim, true).statusCode;
-    getScores = (() => {
-        const t = this;
+
+    #obj: objective
+    #data: ScoreboardObjective
+
+    readonly 'set' = (name: string, score: number) => ( void execCmd(`scoreboard players set ${toExecutable(name)} ${this.#obj.execId} ${score}`, dim.o, true), this )
+    readonly add = (name: string, score: number) => ( void execCmd(`scoreboard players add ${toExecutable(name)} ${this.#obj.execId} ${score}`, dim.o, true), this )
+    readonly 'get' = (name: string) => {
+        const r = execCmd(`scoreboard players test ${toExecutable(name)} ${this.#obj.execId} * *`, dim.o, true)
+        if (r.statusCode) return 
+        return +r.statusMessage.match(/-?\d+/)?.[0]
+    }
+
+    readonly exist = (name: string) => !execCmd(`scoreboard players test ${toExecutable(name)} ${this.#obj.execId} * *`, dim.o, true).statusCode
+    readonly delete = (name: string) => !execCmd(`scoreboard players reset ${toExecutable(name)} ${this.#obj.execId}`, dim.o, true).statusCode
+    readonly getScores = (() => {
+        const t = this
         return function* () {
             for (const { participant, score } of t.#data.getScores())
                 if (participant.type == ScoreboardIdentityType.fakePlayer)
-                    yield [participant.displayName, score];
-        };
-    })();
+                    yield [participant.displayName, score] as [displayName: string, score: number]
+        }
+    })()
 }
+
 class display {
-    static setDisplay = (displaySlot: any, scoreboard: any) => !execCmd(`scoreboard objectives setdisplay ${displaySlot} ${scoreboard instanceof objective ? scoreboard.execId : toExecutable(scoreboard)}`, dim, true).statusCode;
-    static clearDisplay = (displaySlot: any) => !execCmd(`scoreboard objectives setdisplay ${displaySlot}`, dim, true).statusCode;
-    constructor(key: any, obj: any) {
-        if (key !== auth)
-            throw new TypeError('Class is not constructable');
-        this.#obj = obj;
+    static readonly setDisplay = (displaySlot: displaySlot, scoreboard: objective | string) => !execCmd(`scoreboard objectives setdisplay ${displaySlot} ${scoreboard instanceof objective ? scoreboard.execId : toExecutable(scoreboard)}`, dim.o, true).statusCode
+    static readonly clearDisplay = (displaySlot: displaySlot) => !execCmd(`scoreboard objectives setdisplay ${displaySlot}`, dim.o, true).statusCode
+
+    constructor(key: typeof auth, obj: objective) {
+        if (key !== auth) throw new TypeError('Class is not constructable')
+        this.#obj = obj
     }
-    #obj: any;
-    setDisplay = (displaySlot: any) => !execCmd(`scoreboard objectives setdisplay ${displaySlot} ${this.#obj.execId}`, dim, true).statusCode;
-    clearDisplay = display.clearDisplay;
+
+    #obj: objective
+
+    readonly setDisplay = (displaySlot: displaySlot) => !execCmd(`scoreboard objectives setdisplay ${displaySlot} ${this.#obj.execId}`, dim.o, true).statusCode
+    readonly clearDisplay = display.clearDisplay
 }
