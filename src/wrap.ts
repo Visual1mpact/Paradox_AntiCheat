09705c9012baee2e5bdda9c7f9dff80eeeca5e0d
FrostIce482 <frostice48260@gmail.com>
2022-10-26 00:49:36 +0700

    1.19.40 (3)

--- src/wrap.ts
+++ src/wrap.ts
@@ -1,7 +1,7 @@
 import { SimulatedPlayer } from "@minecraft/server-gametest";
-import { Block, Entity, Player } from "@minecraft/server";
+import { Block, Entity, IEntityComponent, ItemCooldownComponent, ItemDurabilityComponent, ItemEnchantsComponent, ItemFoodComponent, ItemStack, Player } from "@minecraft/server";
 
-for (const obj of [Entity, Player, SimulatedPlayer, Block]) {
+for (const obj of [Entity, Player, SimulatedPlayer, Block, ItemStack, IEntityComponent, Block, ItemFoodComponent, ItemCooldownComponent, ItemEnchantsComponent, ItemDurabilityComponent]) {
     const { get: OIdGet } = Object.getOwnPropertyDescriptor(obj.prototype, "typeId") ?? {};
     Object.defineProperty(obj.prototype, "id", { get: OIdGet });
 }
