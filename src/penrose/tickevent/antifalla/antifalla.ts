96a38b3c4af0886bd1dd4ac780f4b25685da2aef
Pete9xi <peter95rose@msn.com>
2023-03-01 14:25:44 +0000

    Finished AntiFall Module, resolved corner false flags tested and working as it should do

--- src/penrose/tickevent/antifalla/antifalla.ts
+++ src/penrose/tickevent/antifalla/antifalla.ts
@@ -1,4 +1,4 @@
-import { world, Player, Block, EntityQueryOptions, GameMode, system, BlockLocation } from "@minecraft/server";
+import { world, Player, Block, EntityQueryOptions, GameMode, system, BlockLocation, BlockType, MinecraftBlockTypes } from "@minecraft/server";
 import { getScore, flag, crypto, setScore } from "../../../util.js";
 const World = world;
 
@@ -10,19 +10,19 @@ function antifalla(id: number) {
         let vy = player.velocity.y;
         let Block: Block;
 
-        let CenBlockX: Block, NegBlockX: Block, PosBlockX: Block, CenBlockZ: Block, NegBlockZ: Block, PosBlockZ: Block;
-
-        Block = player.dimension.getBlock(new BlockLocation(player.location.x, player.location.y, player.location.z));
+        let CenBlockX: Block, CenBlockXTopLeft: Block, CenBlockXTopRight: Block, CenBlockXBottomLeft: Block, CenBlockXBottomRight: Block, NegBlockX: Block, PosBlockX: Block, CenBlockZ: Block, NegBlockZ: Block, PosBlockZ: Block;
         CenBlockX = player.dimension.getBlock(new BlockLocation(player.location.x, player.location.y - 1, player.location.z));
         PosBlockX = player.dimension.getBlock(new BlockLocation(player.location.x + 1, player.location.y - 1, player.location.z));
         NegBlockX = player.dimension.getBlock(new BlockLocation(player.location.x - 1, player.location.y - 1, player.location.z));
         CenBlockZ = player.dimension.getBlock(new BlockLocation(player.location.x, player.location.y - 1, player.location.z));
         PosBlockZ = player.dimension.getBlock(new BlockLocation(player.location.x, player.location.y - 1, player.location.z + 1));
         NegBlockZ = player.dimension.getBlock(new BlockLocation(player.location.x, player.location.y - 1, player.location.z - 1));
-
-        //we know that the NoFall hack keeps the players Velocity set to 0 at all times we can check for this while the player has air blocks around them.
-        // we check to make sure that all air blocks are present, this current code prevents false checks if the player is at the edge of a block.
-        // Far corners are still an issue im working on that. This will cause false flags as the player is standing within air even though theya re on a block.
+        // if the player is in any corner of the block we move the block back to the center before hand and test for air
+        CenBlockXTopLeft = player.dimension.getBlock(new BlockLocation(player.location.x - 1, player.location.y - 1, player.location.z + 1));
+        CenBlockXTopRight = player.dimension.getBlock(new BlockLocation(player.location.x - 1, player.location.y - 1, player.location.z - 1));
+        CenBlockXBottomLeft = player.dimension.getBlock(new BlockLocation(player.location.x + 1, player.location.y - 1, player.location.z + 1));
+        CenBlockXBottomRight = player.dimension.getBlock(new BlockLocation(player.location.x + 1, player.location.y - 1, player.location.z - 1));
+        //we know that the NoFall hack keeps the players Velocity set to 0 at all times we can check for this while the player has air blocks and are not standing on a block or its edges etc.
         if (
             CenBlockX.typeId == "minecraft:air" &&
             PosBlockX.typeId == "minecraft:air" &&
@@ -30,9 +30,14 @@ function antifalla(id: number) {
             CenBlockZ.typeId == "minecraft:air" &&
             PosBlockZ.typeId == "minecraft:air" &&
             NegBlockZ.typeId == "minecraft:air" &&
+            CenBlockXTopLeft.typeId == "minecraft:air" &&
+            CenBlockXTopRight.typeId == "minecraft:air" &&
+            CenBlockXBottomLeft.typeId == "minecraft:air" &&
+            CenBlockXBottomRight.typeId == "minecraft:air" &&
             vy == 0
         ) {
             //flag it as a hack and not a movement hack due to issues with flya.
+
             flag(player, "AntiFall", "A", "Hack", null, null, null, null, false, null);
         }
     }
