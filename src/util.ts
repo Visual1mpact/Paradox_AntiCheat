835db1a183129a7fbf179f4d80914c1850b048f6
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-03-11 20:48:06 -0500

    Optimize UUID
    1. Converted the function to an arrow function to avoid binding this to self.
    2. Replaced var with let.
    3. Used ES6 arrow function syntax for better readability and conciseness.
    4. Used const instead of let wherever the variable is not reassigned.
    5. Wrapped the code inside an IIFE to avoid polluting the global namespace.
    6. Return an object directly instead of assigning it to self.

--- src/util.ts
+++ src/util.ts
@@ -208,41 +208,42 @@ export function resetTag(player: Player, member: Player) {
  * @license MIT license
  * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
  **/
-export const UUID = (function () {
-    const self = { generate: this };
+export const UUID = (() => {
     const lut = [];
-    for (var i = 0; i < 256; i++) {
+    for (let i = 0; i < 256; i++) {
         lut[i] = (i < 16 ? "0" : "") + i.toString(16);
     }
-    self.generate = function () {
-        const d0 = (Math.random() * 0x100000000) >>> 0;
-        const d1 = (Math.random() * 0x100000000) >>> 0;
-        const d2 = (Math.random() * 0x100000000) >>> 0;
-        const d3 = (Math.random() * 0x100000000) >>> 0;
-        return (
-            lut[d0 & 0xff] +
-            lut[(d0 >> 8) & 0xff] +
-            lut[(d0 >> 16) & 0xff] +
-            lut[(d0 >> 24) & 0xff] +
-            "-" +
-            lut[d1 & 0xff] +
-            lut[(d1 >> 8) & 0xff] +
-            "-" +
-            lut[((d1 >> 16) & 0x0f) | 0x40] +
-            lut[(d1 >> 24) & 0xff] +
-            "-" +
-            lut[(d2 & 0x3f) | 0x80] +
-            lut[(d2 >> 8) & 0xff] +
-            "-" +
-            lut[(d2 >> 16) & 0xff] +
-            lut[(d2 >> 24) & 0xff] +
-            lut[d3 & 0xff] +
-            lut[(d3 >> 8) & 0xff] +
-            lut[(d3 >> 16) & 0xff] +
-            lut[(d3 >> 24) & 0xff]
-        );
+
+    return {
+        generate: () => {
+            const d0 = (Math.random() * 0x100000000) >>> 0;
+            const d1 = (Math.random() * 0x100000000) >>> 0;
+            const d2 = (Math.random() * 0x100000000) >>> 0;
+            const d3 = (Math.random() * 0x100000000) >>> 0;
+            return (
+                lut[d0 & 0xff] +
+                lut[(d0 >> 8) & 0xff] +
+                lut[(d0 >> 16) & 0xff] +
+                lut[(d0 >> 24) & 0xff] +
+                "-" +
+                lut[d1 & 0xff] +
+                lut[(d1 >> 8) & 0xff] +
+                "-" +
+                lut[((d1 >> 16) & 0x0f) | 0x40] +
+                lut[(d1 >> 24) & 0xff] +
+                "-" +
+                lut[(d2 & 0x3f) | 0x80] +
+                lut[(d2 >> 8) & 0xff] +
+                "-" +
+                lut[(d2 >> 16) & 0xff] +
+                lut[(d2 >> 24) & 0xff] +
+                lut[d3 & 0xff] +
+                lut[(d3 >> 8) & 0xff] +
+                lut[(d3 >> 16) & 0xff] +
+                lut[(d3 >> 24) & 0xff]
+            );
+        },
     };
-    return self;
 })();
 
 /**
