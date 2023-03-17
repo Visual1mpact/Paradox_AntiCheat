93a5bad7fd889af24baae40e6caa9eebd6dbd9e0
Pete9xi <peter95rose@msn.com>
2023-03-17 21:28:25 +0000

    ParadoxUI Complete gamemodes and test

--- src/gui/modules/uiGamemodes.ts
+++ src/gui/modules/uiGamemodes.ts
@@ -4,6 +4,8 @@ import { dynamicPropertyRegistry } from "../../penrose/worldinitializeevent/regi
 import { sendMsg, sendMsgToPlayer } from "../../util";
 import { paradoxui } from "../paradoxui.js";
 import { Adventure } from "../../penrose/tickevent/gamemode/adventure.js";
+import { Creative } from "../../penrose/tickevent/gamemode/creative.js";
+import { Survival } from "../../penrose/tickevent/gamemode/survival.js";
 
 export function uiGAMEMODES(gamemodeResult: ModalFormResponse, player: Player) {
     const [AdventureGM, CreativeGM, SurvivalGM] = gamemodeResult.formValues;
@@ -11,7 +13,7 @@ export function uiGAMEMODES(gamemodeResult: ModalFormResponse, player: Player) {
     const uniqueId = dynamicPropertyRegistry.get(player.scoreboard.id);
 
     // Get Dynamic Property Boolean
-    //const adventureGMBoolean = dynamicPropertyRegistry.get("adventuregm_b");
+    const adventureGMBoolean = dynamicPropertyRegistry.get("adventuregm_b");
     const creativeGMBoolean = dynamicPropertyRegistry.get("creativegm_b");
     const survivalGMBoolean = dynamicPropertyRegistry.get("survivalgm_b");
 
@@ -22,7 +24,7 @@ export function uiGAMEMODES(gamemodeResult: ModalFormResponse, player: Player) {
     if (AdventureGM === true && CreativeGM === true && SurvivalGM === true) {
         return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You cant disable all gamemodes!`);
     }
-
+    //Adventure gamemode
     if (AdventureGM === true) {
         // Allow
         dynamicPropertyRegistry.set("adventuregm_b", true);
@@ -44,9 +46,48 @@ export function uiGAMEMODES(gamemodeResult: ModalFormResponse, player: Player) {
         world.setDynamicProperty("adventuregm_b", false);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has allowed §6Gamemode 2 (Adventure)§r to be used!`);
     }
+    //Creative gamemode
     if (CreativeGM === true) {
+        // Allow
+        dynamicPropertyRegistry.set("creativegm_b", true);
+        world.setDynamicProperty("creativegm_b", true);
+        // Make sure at least one is allowed since this could cause serious issues if all were locked down
+        // We will allow Adventure Mode in this case
+        if (adventureGMBoolean === true && survivalGMBoolean === true) {
+            dynamicPropertyRegistry.set("adventuregm_b", false);
+            world.setDynamicProperty("adventuregm_b", false);
+            sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r Since all gamemodes were disallowed, Adventure mode has been enabled.`);
+            Adventure();
+        }
+        sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has disallowed §4Gamemode 1 (Creative)§r to be used!`);
+        Creative();
+    }
+    if (CreativeGM === false) {
+        // Deny
+        dynamicPropertyRegistry.set("creativegm_b", false);
+        world.setDynamicProperty("creativegm_b", false);
+        sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has allowed §6Gamemode 1 (Creative)§r to be used!`);
     }
     if (SurvivalGM === true) {
+        // Allow
+        dynamicPropertyRegistry.set("survivalgm_b", true);
+        world.setDynamicProperty("survivalgm_b", true);
+        // Make sure at least one is allowed since this could cause serious issues if all were locked down
+        // We will allow Adventure Mode in this case
+        if (adventureGMBoolean === true && creativeGMBoolean === true) {
+            dynamicPropertyRegistry.set("adventuregm_b", false);
+            world.setDynamicProperty("adventuregm_b", false);
+            sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r Since all gamemodes were disallowed, Adventure mode has been enabled.`);
+            Adventure();
+        }
+        sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has disallowed §4Gamemode 0 (Survival)§r to be used!`);
+        Survival();
+    }
+    if (SurvivalGM === false) {
+        // Deny
+        dynamicPropertyRegistry.set("survivalgm_b", false);
+        world.setDynamicProperty("survivalgm_b", false);
+        sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has allowed §6Gamemode 0 (Survival)§r to be used!`);
     }
 
     //show the main ui to the player one complete.
