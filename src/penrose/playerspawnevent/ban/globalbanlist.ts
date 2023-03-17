51139b83b6253df2dc388d12e48b17d915cf4376
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-03-16 22:38:48 -0400

    Optimize Global Ban List handler
    1. Use a for loop instead of some() method:
    This is because the some() method will keep looping through the array even after finding a match, whereas a for loop will stop as soon as it finds a match.
    
    2. Cache the tags to avoid calling hasTag() multiple times:
    This is because calling hasTag() repeatedly can be expensive.

--- src/penrose/playerspawnevent/ban/globalbanlist.ts
+++ src/penrose/playerspawnevent/ban/globalbanlist.ts
@@ -1,29 +1,31 @@
-import { world, PlayerSpawnEvent, Player } from "@minecraft/server";
+import { world, PlayerSpawnEvent } from "@minecraft/server";
 import { banplayer } from "../../../data/globalban.js";
 import { banMessage } from "../../../util.js";
 
-async function banHammerTime(object: PlayerSpawnEvent) {
+function banHammerTime(object: PlayerSpawnEvent) {
     /**
      * We only want to execute this when it's a players initial spawn
      */
-    let player: Player;
-    if (object.initialSpawn === true) {
-        player = object.player;
-    } else {
+    if (!object.initialSpawn) {
         return;
     }
 
+    const player = object.player;
+
     // Check global ban list and if the player who is joining is on the server then kick them out
-    if (banplayer.some((code) => JSON.stringify(code) === JSON.stringify({ name: player.nameTag }))) {
-        if (!player.hasTag("By:Paradox Anticheat")) {
-            // if they are not tagged then we do that here before we ban
-            player.addTag("By:Paradox Anticheat");
+    for (let i = 0; i < banplayer.length; i++) {
+        const code = banplayer[i];
+        if (code.name === player.nameTag) {
+            const playerTags = player.getTags();
+            if (!playerTags.includes("By:Paradox Anticheat")) {
+                player.addTag("By:Paradox Anticheat");
+            }
+            if (!playerTags.includes("Reason:You are globally banned from Paradox!")) {
+                player.addTag("Reason:You are globally banned from Paradox!");
+            }
+            banMessage(player);
+            break;
         }
-        if (!player.hasTag("Reason:You are globally banned from Paradox!")) {
-            // if they are not tagged then we do that here before we ban
-            player.addTag("Reason:You are globally banned from Paradox!");
-        }
-        banMessage(player);
     }
 }
 
