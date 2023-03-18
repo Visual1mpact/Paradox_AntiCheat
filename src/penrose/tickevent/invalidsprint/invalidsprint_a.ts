0eb743bd6eaa9f318e8a0c0b17382b1a7d781678
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-03-18 12:54:54 -0400

    [invalidSprintA] Check for gliding and riding
    We don't want to flag players for legitimate gameplay riding or gliding such as using a boat or an elytra.

--- src/penrose/tickevent/invalidsprint/invalidsprint_a.ts
+++ src/penrose/tickevent/invalidsprint/invalidsprint_a.ts
@@ -56,6 +56,17 @@ function invalidsprinta(id: number) {
         if (uniqueId === player.name) {
             continue;
         }
+
+        const glideCheck = player.hasTag("gliding");
+        if (glideCheck) {
+            return;
+        }
+
+        const rideCheck = player.hasTag("riding");
+        if (rideCheck) {
+            return;
+        }
+
         const { x, y, z } = player.location;
         highestBps = calculateMovementBPS([x, y, z]);
         // We compare with a 20% buffer to minimize false flags
