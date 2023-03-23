29ea0d36151c82a4b110374df699f3869a8a075a
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-03-23 13:46:36 -0400

    [lockdown] Enable command in config
    This was disabled in e3aa0816aa8efb80b73057e0a033d0f1500408ec. This was due to Dynamic Properties being implemented into Paradox but shortly later discovering that DP had issues with server and realms upon being released with the API. This issue has been fixed for a very long time now and is no longer a concern so we can turn the command back on.

--- src/data/config.ts
+++ src/data/config.ts
@@ -63,7 +63,7 @@ export default {
         chatranks: true,
         antishulker: true,
         stackban: true,
-        lockdown: false,
+        lockdown: true,
         punish: true,
         sethome: true,
         gohome: true,
