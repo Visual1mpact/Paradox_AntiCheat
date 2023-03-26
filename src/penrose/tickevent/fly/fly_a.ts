d0605f8ee64ee19fb4d1e12314eaf820fd54026a
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-03-26 17:50:40 -0400

    [flya] Update logic
    1. Added a new map called playersAirTimeStart to keep track of when each player started being in the air.
    2. Updated the condition for checking if the player has jumped to also check if they are already in the playersAirTimeStart map.
    3. Added a new condition to check if the player is on the ground, and if so, remove their entry from the playersAirTimeStart map.
    4. Added a new condition to check if the player is gliding, and if so, remove their entry from the playersAirTimeStart map.
    5. Added a new condition to check if the player is falling, and if so, remove their entry from the playersAirTimeStart map.
    6. Updated the condition for checking if the player has been in the air for at least 2 seconds to also check if they are in the playersAirTimeStart map.
    7. Moved the calculation of the horizontal velocity and its components inside the condition for when the player has been in the air for at least 2 seconds.
    8. Updated the condition for checking if the player is surrounded by air to only be checked if the player has been in the air for at least 2 seconds.

--- src/penrose/tickevent/fly/fly_a.ts
+++ src/penrose/tickevent/fly/fly_a.ts
@@ -3,18 +3,17 @@ import { flag, startTimer } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../worldinitializeevent/registry.js";
 
 const playersOldCoordinates = new Map<string, Vector>();
+const playersAirTimeStart = new Map<string, number>();
 
 function flya(id: number) {
     // Get Dynamic Property
     const flyABoolean = dynamicPropertyRegistry.get("flya_b");
-
     // Unsubscribe if disabled in-game
     if (flyABoolean === false) {
         playersOldCoordinates.clear();
         system.clearRun(id);
         return;
     }
-
     // Exclude creative gamemode
     const gm = new Object() as EntityQueryOptions;
     gm.excludeGameModes = [GameMode.creative, GameMode.spectator];
@@ -22,76 +21,78 @@ function flya(id: number) {
     for (const player of world.getPlayers(gm)) {
         // Get unique ID
         const uniqueId = dynamicPropertyRegistry.get(player?.scoreboard?.id);
-
         // Skip if they have permission
         if (uniqueId === player.name) {
             continue;
         }
-
         const jumpCheck = player.hasTag("jump");
-        if (jumpCheck) {
+        if (jumpCheck && !playersAirTimeStart.has(player.name)) {
+            playersAirTimeStart.set(player.name, Date.now());
+        }
+        const groundCheck = player.hasTag("ground");
+        if (groundCheck) {
+            playersAirTimeStart.delete(player.name);
             continue;
         }
-
         const glideCheck = player.hasTag("gliding");
         if (glideCheck) {
+            playersAirTimeStart.delete(player.name);
             continue;
         }
-
         const velocity = player.getVelocity();
         if (velocity.y < 0) {
             // Player is falling, ignore them
+            playersAirTimeStart.delete(player.name);
             continue;
         }
-        const horizontalVelocity = new Vector(velocity.x, 0, velocity.z);
-
-        const xyVelocity = Math.hypot(horizontalVelocity.x, horizontalVelocity.y).toFixed(4);
-        const zyVelocity = Math.hypot(horizontalVelocity.z, horizontalVelocity.y).toFixed(4);
-
-        if (Number(xyVelocity) > 0 || Number(zyVelocity) > 0) {
-            const oldPlayerCoords = playersOldCoordinates.get(player.name);
-
-            const playerX = Math.trunc(player.location.x);
-            const playerY = Math.trunc(player.location.y);
-            const playerZ = Math.trunc(player.location.z);
-            playersOldCoordinates.set(player.name, new Vector(playerX, playerY, playerZ));
-
-            /**
-             * startTimer will make sure the key is properly removed
-             * when the time for theVoid has expired. This will preserve
-             * the integrity of our Memory.
-             */
-            const timerExpired = startTimer("flya", player.name, Date.now());
-            if (timerExpired.namespace.indexOf("flya") !== -1 && timerExpired.expired) {
-                const deletedKey = timerExpired.key; // extract the key without the namespace prefix
-                playersOldCoordinates.delete(deletedKey);
-            }
-
-            if (oldPlayerCoords) {
-                let isSurroundedByAir = true;
-
-                for (let x = -1; x <= 1; x++) {
-                    for (let y = -1; y <= 1; y++) {
-                        for (let z = -1; z <= 1; z++) {
-                            const block = player.dimension.getBlock(new Vector(player.location.x + x, player.location.y + y, player.location.z + z));
-
-                            if (block.typeId !== "minecraft:air") {
-                                isSurroundedByAir = false;
-                                break;
+        if (jumpCheck && playersAirTimeStart.has(player.name)) {
+            const airTime = Date.now() - playersAirTimeStart.get(player.name);
+            if (airTime >= 2000) {
+                const horizontalVelocity = new Vector(velocity.x, 0, velocity.z);
+                const xyVelocity = Math.hypot(horizontalVelocity.x, horizontalVelocity.y).toFixed(4);
+                const zyVelocity = Math.hypot(horizontalVelocity.z, horizontalVelocity.y).toFixed(4);
+                if (Number(xyVelocity) > 0 || Number(zyVelocity) > 0) {
+                    const oldPlayerCoords = playersOldCoordinates.get(player.name);
+                    const playerX = Math.trunc(player.location.x);
+                    const playerY = Math.trunc(player.location.y);
+                    const playerZ = Math.trunc(player.location.z);
+                    playersOldCoordinates.set(player.name, new Vector(playerX, playerY, playerZ));
+                    /**
+                     * startTimer will make sure the key is properly removed
+                     * when the time for theVoid has expired. This will preserve
+                     * the integrity of our Memory.
+                     */
+                    const timerExpired = startTimer("flya", player.name, Date.now());
+                    if (timerExpired.namespace.indexOf("flya") !== -1 && timerExpired.expired) {
+                        const deletedKey = timerExpired.key; // extract the key without the namespace prefix
+                        playersOldCoordinates.delete(deletedKey);
+                        playersAirTimeStart.delete(deletedKey);
+                    }
+                    if (oldPlayerCoords) {
+                        let isSurroundedByAir = true;
+                        for (let x = -1; x <= 1; x++) {
+                            for (let y = -1; y <= 1; y++) {
+                                for (let z = -1; z <= 1; z++) {
+                                    const block = player.dimension.getBlock(new Vector(player.location.x + x, player.location.y + y, player.location.z + z));
+                                    if (block.typeId !== "minecraft:air") {
+                                        isSurroundedByAir = false;
+                                        break;
+                                    }
+                                }
                             }
+                        }
+                        if (isSurroundedByAir) {
+                            try {
+                                // Use try/catch since variables for cords could return undefined if player is loading in
+                                // and they meet the conditions. An example is them flagging this, logging off, then logging
+                                // back on again.
+                                player.teleport(new Vector(oldPlayerCoords.x, oldPlayerCoords.y, oldPlayerCoords.z), player.dimension, 0, 0);
+                            } catch (error) {}
+                            flag(player, "Fly", "A", "Exploit", null, null, null, null, false, null);
                         }
                     }
                 }
-
-                if (isSurroundedByAir) {
-                    try {
-                        // Use try/catch since variables for cords could return undefined if player is loading in
-                        // and they meet the conditions. An example is them flagging this, logging off, then logging
-                        // back on again.
-                        player.teleport(new Vector(oldPlayerCoords.x, oldPlayerCoords.y, oldPlayerCoords.z), player.dimension, 0, 0);
-                    } catch (error) {}
-                    flag(player, "Fly", "A", "Exploit", null, null, null, null, false, null);
-                }
+                playersAirTimeStart.delete(player.name);
             }
         }
     }
