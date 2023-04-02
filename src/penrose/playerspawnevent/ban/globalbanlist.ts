b36ac0ffbf3cc1bea9520add957884aa5edf52df
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-04-02 14:28:25 -0400

    [globalban] Performance improvement
    This change represents the new implementation that uses a Set to store the globally banned players and checks for membership in the set using the has method. This implementation is more efficient and faster than the original code, which uses a for loop to iterate through the global ban list and check if the player is banned. Additionally, the use of a Set eliminates the need for duplicate entries, ensuring that banned players are not duplicated.
    
    This change represents the old implementation that iterates through the global ban list using a for loop and checks if the player is banned. This implementation is slower and less efficient than the original code since it has to iterate through the entire global ban list every time a new player joins the server. Additionally, this implementation is prone to duplicate entries since it does not have a way to ensure that banned players are not duplicated.

--- src/penrose/playerspawnevent/ban/globalbanlist.ts
+++ src/penrose/playerspawnevent/ban/globalbanlist.ts
@@ -2,9 +2,12 @@ import { world, PlayerSpawnEvent } from "@minecraft/server";
 import { banplayer } from "../../../data/globalban.js";
 import { banMessage } from "../../../util.js";
 
+// Create a Set to store the names of globally banned players
+const bannedPlayers = new Set(banplayer.map((player) => player.name));
+
 function banHammerTime(object: PlayerSpawnEvent) {
     /**
-     * We only want to execute this when it's a players initial spawn
+     * We only want to execute this when it's a player's initial spawn
      */
     if (!object.initialSpawn) {
         return;
@@ -12,20 +15,16 @@ function banHammerTime(object: PlayerSpawnEvent) {
 
     const player = object.player;
 
-    // Check global ban list and if the player who is joining is on the server then kick them out
-    for (let i = 0; i < banplayer.length; i++) {
-        const code = banplayer[i];
-        if (code.name === player.nameTag) {
-            const playerTags = player.getTags();
-            if (!playerTags.includes("By:Paradox Anticheat")) {
-                player.addTag("By:Paradox Anticheat");
-            }
-            if (!playerTags.includes("Reason:You are globally banned from Paradox!")) {
-                player.addTag("Reason:You are globally banned from Paradox!");
-            }
-            banMessage(player);
-            break;
+    // Check if the player who is joining is on the global ban list, and kick them out if they are
+    if (bannedPlayers.has(player.nameTag)) {
+        const playerTags = player.getTags();
+        if (!playerTags.includes("By:Paradox Anticheat")) {
+            player.addTag("By:Paradox Anticheat");
         }
+        if (!playerTags.includes("Reason:You are globally banned from Paradox!")) {
+            player.addTag("Reason:You are globally banned from Paradox!");
+        }
+        banMessage(player);
     }
 }
 
