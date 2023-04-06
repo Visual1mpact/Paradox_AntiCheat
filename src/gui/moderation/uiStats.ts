9b18241d5aa50984bff7c6d6b4f67350439315bf
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-04-05 21:52:38 -0400

    [uiStats] Refactor code and improve performance
    1.    Import statements for necessary modules and functions.
    2.    The uiSTATS function takes in three parameters: statsResult of type ModalFormResponse which is the response received from a modal form, onlineList of type string[] which is a list of online players' names, and player of type Player which is the player who triggered the modal form.
    3.    Extract the selected value from the statsResult object.
    4.    Search for the player object whose name matches the selected name from the onlineList array. If no matching player is found, return an error message to the player.
    5.    Check if the player executing the command is registered as a Paradox operator. If not, return an error message.
    6.    Create an array of score names.
    7.    Create an array reportBody that contains the name of the player being reported, their current gamemode, and their current location.
    8.    Iterate over each score in the scores array and attempt to retrieve the corresponding objective from the scoreboard. If the score exists, add the score's name and value to the reportBody array.
    9.    Create a new ActionFormData object for displaying the report UI, set the title to include the player's name, set the body to the reportBody array, and add a close button.
    10.    Show the UI to the player who triggered the command, and then return the player object. After the UI is closed, call the paradoxui function to show the main UI again.o

--- src/gui/moderation/uiStats.ts
+++ src/gui/moderation/uiStats.ts
@@ -7,6 +7,7 @@ import { paradoxui } from "../paradoxui.js";
 
 export function uiSTATS(statsResult: ModalFormResponse, onlineList: string[], player: Player) {
     const [value] = statsResult.formValues;
+
     let member: Player = undefined;
     for (let pl of world.getPlayers()) {
         if (pl.nameTag.toLowerCase().includes(onlineList[value].toLowerCase().replace(/"|\\|@/g, ""))) {
@@ -14,35 +15,17 @@ export function uiSTATS(statsResult: ModalFormResponse, onlineList: string[], pl
             break;
         }
     }
-    // Get unique ID
-    const uniqueId = dynamicPropertyRegistry.get(player?.scoreboard?.id);
 
-    // Make sure the user has permissions to run the command
+    if (!member) {
+        return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r The player is not online.`);
+    }
+
+    const uniqueId = dynamicPropertyRegistry.get(player?.scoreboard?.id);
     if (uniqueId !== player.name) {
         return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You need to be Paradox-Opped.`);
     }
-    //scores
-    let getScoreAutoClicker: number = 0;
-    let getScoreBadpackets1: number = 0;
-    let getScoreKillaura: number = 0;
-    let getScoreFlya: number = 0;
-    let getScoreIllegalItemsa: number = 0;
-    let getScoreKillauraA: number = 0;
-    let getScoreNoSlowA: number = 0;
-    let getScoreCBE: number = 0;
-    let getScoreGameModeChange: number = 0;
-    let getScoreAutoTotem: number = 0;
-    let getScoreSpammer: number = 0;
-    let getScoreNameSpoof: number = 0;
-    let getScoreCrasher: number = 0;
-    let getScoreReach: number = 0;
-    let getScoreInventoryMods: number = 0;
-    let getScoreInvalidSprint: number = 0;
-    let getScoreEnchantedArmor: number = 0;
-    let getScoreAntiKnockBack: number = 0;
-    //Try getting the scores the script will stop if the target doesnt have a score for that objective.
-    let ScoreboardObjectivesValues = world.scoreboard.getObjectives();
-    const Violations: string[] = [
+
+    const scores = [
         "autoclickervl",
         "badpacketsvl",
         "killauravl",
@@ -62,124 +45,24 @@ export function uiSTATS(statsResult: ModalFormResponse, onlineList: string[], pl
         "armorvl",
         "antikbvl",
     ];
-    // build the report body for the ui.
-    let reportBody = ["§6All Stats for " + member.name + "§r" + "\n" + "\n"];
-    // get the target players gamemode
-    let MembersGamemode = getGamemode(member);
-    reportBody.push("§6" + member.name + "§r is in Gamemode: " + MembersGamemode + "\n");
-    //get the target players coords
-    reportBody.push("§6" + member.name + "§r is currently at X= " + member.location.x.toFixed(0) + " Y= " + member.location.y.toFixed(0) + " Z= " + member.location.z.toFixed(0) + "\n");
-    for (let i = 0; i < ScoreboardObjectivesValues.length; i++) {
-        if (ScoreboardObjectivesValues[i].getParticipants().find((pT) => pT.displayName == member.name) && Violations.includes(ScoreboardObjectivesValues[i].displayName)) {
-            let tempscore = member.scoreboard.getScore(world.scoreboard.getObjective(ScoreboardObjectivesValues[i].displayName));
-            if (ScoreboardObjectivesValues[i].displayName.includes("autoclickervl")) {
-                getScoreAutoClicker = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6AutoClicker§4]§r number of Violations: " + getScoreAutoClicker + "\n");
-                }
+
+    const reportBody = [
+        `§6All Stats for ${member.name}§r\n\n`,
+        `§6${member.name}§r is in Gamemode: ${getGamemode(member)}\n`,
+        `§6${member.name}§r is currently at X= ${member.location.x.toFixed(0)} Y= ${member.location.y.toFixed(0)} Z= ${member.location.z.toFixed(0)}\n`,
+    ];
+
+    scores.forEach((score) => {
+        try {
+            const objective = world.scoreboard.getObjective(score);
+            const playerScore = member.scoreboard.getScore(objective);
+            if (playerScore > 0) {
+                reportBody.push(`§r§4[§6${score.replace("vl", "").toUpperCase()}§4]§r number of Violations: ${playerScore}\n`);
             }
-            if (ScoreboardObjectivesValues[i].displayName.includes("badpacketsvl")) {
-                getScoreBadpackets1 = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Badpackets§4]§r number of Violations: " + getScoreBadpackets1 + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("killauravl")) {
-                getScoreKillaura = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Killaura§4]§r number of Violations: " + getScoreKillaura + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("flyvl")) {
-                getScoreFlya = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6FlyaA§4]§r number of Violations: " + getScoreFlya + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("illegalitemsvl")) {
-                getScoreIllegalItemsa = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6IllegalItemsA§4]§r number of Violations: " + getScoreIllegalItemsa + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("interactusevl")) {
-                getScoreKillauraA = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6KillauraA§4]§r number of Violations: " + getScoreKillauraA + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("noslowvl")) {
-                getScoreNoSlowA = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6NoSlowA§4]§r§r number of Violations: " + getScoreNoSlowA + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("cbevl")) {
-                getScoreCBE = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6CBE§4]§r§r number of Violations: " + getScoreCBE + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("gamemodevl")) {
-                getScoreGameModeChange = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Gamemode Change§4]§r§r number of Violations: " + getScoreGameModeChange + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("autototemvl")) {
-                getScoreAutoTotem = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6AutoTotem§4]§r§r number of Violations: " + getScoreAutoTotem + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("spammervl")) {
-                getScoreSpammer = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Spammer§4]§r§r number of Violations: " + getScoreSpammer + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("namespoofvl")) {
-                getScoreNameSpoof = tempscore;
-                reportBody.push("§r§4[§6Namespoof§4]§r§r number of Violations: " + getScoreNameSpoof + "\n");
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("crashervl")) {
-                getScoreCrasher = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Crasher§4]§r§r number of Violations: " + getScoreCrasher + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("reachvl")) {
-                getScoreReach = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6Reach§4]§r§r number of Violations: " + getScoreReach + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("invmovevl")) {
-                getScoreInventoryMods = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6InventoryMods§4]§r§r number of Violations: " + getScoreInventoryMods + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("invalidsprintvl")) {
-                getScoreInvalidSprint = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6InvalidSprint§4]§r§r number of Violations: " + getScoreInvalidSprint + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("armorvl")) {
-                getScoreEnchantedArmor = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6EnchantedArmor§4]§r§r number of Violations: " + getScoreEnchantedArmor + "\n");
-                }
-            }
-            if (ScoreboardObjectivesValues[i].displayName.includes("antikbvl")) {
-                getScoreAntiKnockBack = tempscore;
-                if (tempscore > 0) {
-                    reportBody.push("§r§4[§6AntiKnockback§4]§r§r number of Violations: " + getScoreAntiKnockBack + "\n");
-                }
-            }
+        } catch {
+            // Ignore since this score doesn't exist for this player yet.
         }
-    }
+    });
 
     const ResultsUI = new ActionFormData();
     ResultsUI.title("§4Paradox - Report for §4" + member.name);
