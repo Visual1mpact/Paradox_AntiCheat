24f752ebd62a8d475fce1e88d003bedb8eedd0f1
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-05-02 19:07:11 -0400

    [showrules] Update logic using promises
    The code updates the behavior of the showrules function, making it asynchronous and using the await keyword to wait for the form.show(player) promise to resolve before proceeding with the code execution. This ensures that the user sees the message form before any other actions are taken. The code also uses Promise.all() to wait for all the promises returned by the map() function to resolve before continuing execution. This makes the code more efficient and less prone to errors.
    
    We have incorporated the waitForTwentySeconds function that returns a promise which resolves after 20 seconds, in addition to waiting for promises. By utilizing Promise.race, the function will now wait for either all promises in the promises array to resolve or for the 20-second timeout to occur. This approach guarantees that the function won't hang indefinitely, even if a player is AFK and fails to respond to the form.

--- src/gui/showrules/showrules.ts
+++ src/gui/showrules/showrules.ts
@@ -4,12 +4,18 @@ import { sendMsgToPlayer } from "../../util.js";
 import { MessageFormData } from "@minecraft/server-ui";
 import { dynamicPropertyRegistry } from "../../penrose/worldinitializeevent/registry.js";
 
+function waitForTwentySeconds(): Promise<void> {
+    return new Promise((resolve) => {
+        system.runTimeout(() => {
+            resolve();
+        }, 20 * 20); // 20 ticks per second, so 20 * 20 ticks = 20 seconds
+    });
+}
+
 async function showrules(id: number) {
-    //Get Dynamic Property
     const showrulesBoolean = dynamicPropertyRegistry.get("showrules_b");
     const KickOnDeclineBoolean = dynamicPropertyRegistry.get("kickondecline_b");
 
-    // Unsubscribe if disabled in-game
     if (showrulesBoolean === false) {
         system.clearRun(id);
         return;
@@ -17,62 +23,40 @@ async function showrules(id: number) {
 
     const filter = new Object() as EntityQueryOptions;
     filter.tags = ["ShowRulesOnJoin"];
-    // run as each player
-    for (const player of world.getPlayers(filter)) {
-        // pull the rules from the config file.
-        const cfgrule1 = config.modules.showrules.rule1;
-        const cfgrule2 = config.modules.showrules.rule2;
-        const cfgrule3 = config.modules.showrules.rule3;
-        const cfgrule4 = config.modules.showrules.rule4;
-        const cfgrule5 = config.modules.showrules.rule5;
-        //Join the strings to make a single string and add the newlines
-        const CompleteRules = cfgrule1 + "\n" + cfgrule2 + "\n" + cfgrule3 + "\n" + cfgrule4 + "\n" + cfgrule5;
+
+    const [cfgrule1, cfgrule2, cfgrule3, cfgrule4, cfgrule5] = [config.modules.showrules.rule1, config.modules.showrules.rule2, config.modules.showrules.rule3, config.modules.showrules.rule4, config.modules.showrules.rule5];
+
+    const CompleteRules = `${cfgrule1}\n${cfgrule2}\n${cfgrule3}\n${cfgrule4}\n${cfgrule5}`;
+
+    const players = world.getPlayers(filter);
+    const promises = players.map(async (player) => {
         const form = new MessageFormData();
         form.title("Server Rules");
         form.body(CompleteRules);
         form.button1("I Agree");
         form.button2("Decline");
-        form.show(player)
-            .then((r) => {
-                // Button 1 (Agreement)
-                if (r.selection === 1) {
-                    // The code when the player responds to the form
-                    player.removeTag("ShowRulesOnJoin");
-                    // We appreciate you for agreeing to the rules
-                    sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r Thank you for accepting the rules ${player.name}.`);
-                    // This needs to be monitored with multiplayer
-                    // It may cause disruptions with other players who get this form?
-                    return system.clearRun(id);
+        const r = await form.show(player);
+        if (r.selection === 1) {
+            player.removeTag("ShowRulesOnJoin");
+            sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r Thank you for accepting the rules ${player.name}.`);
+            return;
+        }
+        if (r.selection === 0) {
+            if (KickOnDeclineBoolean === true) {
+                const reason = "You must agree to the rules to join.";
+                try {
+                    player.runCommandAsync(`kick ${JSON.stringify(player.name)} ${reason}`);
+                } catch (error) {
+                    player.triggerEvent("paradox:kick");
                 }
+            }
+            return;
+        }
+    });
 
-                // Button 2 (Declined)
-                if (r.selection === 0) {
-                    const reason = "You must agree to the rules to join.";
-                    if (KickOnDeclineBoolean === true) {
-                        // Goodbye lol
-                        try {
-                            player.runCommandAsync(`kick ${JSON.stringify(player.name)} ${reason}`);
-                        } catch (error) {
-                            player.triggerEvent("paradox:kick");
-                        }
-                    }
-                    //if kick is not enabled then dont kick
-
-                    // This needs to be monitored with multiplayer
-                    // It may cause disruptions with other players who get this form?
-                    return system.clearRun(id);
-                }
-
-                if (r.canceled) {
-                    // This needs to be monitored with multiplayer
-                    // It may cause disruptions with other players who get this form?
-                    return system.clearRun(id);
-                }
-            })
-            .catch((e) => {
-                console.error(e, e.stack);
-            });
-    }
+    const allPromises = Promise.all(promises);
+    const twentySecondsPromise = waitForTwentySeconds();
+    await Promise.race([allPromises, twentySecondsPromise]);
 }
 
 export function ShowRules() {
