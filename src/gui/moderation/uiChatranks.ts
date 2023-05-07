41aabb8d37b97d246c2eba69c840cce79d0a941d
Pete9xi <peter95rose@msn.com>
2023-05-07 22:50:10 +0100

    [uiChatranks] Add a check to prevent setting being set if not required.

--- src/gui/moderation/uiChatranks.ts
+++ src/gui/moderation/uiChatranks.ts
@@ -16,7 +16,7 @@ export function uiCHATRANKS(notifyResult: ModalFormResponse, onlineList: string[
     }
     // Get unique ID
     const uniqueId = dynamicPropertyRegistry.get(player?.id);
-
+    const chatRanksBoolean = dynamicPropertyRegistry.get("chatranks_b");
     // Make sure the user has permissions to run the command
     if (uniqueId !== player.name) {
         return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You need to be Paradox-Opped to enable Notifications.`);
@@ -59,13 +59,13 @@ export function uiCHATRANKS(notifyResult: ModalFormResponse, onlineList: string[
         }
         member.addTag("Rank:" + customrank);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has updated ${member.nameTag} Rank.`);
-        if (ChatRanksToggle === true) {
+        if (ChatRanksToggle === true && chatRanksBoolean === false) {
             // Allow
             dynamicPropertyRegistry.set("chatranks_b", true);
             world.setDynamicProperty("chatranks_b", true);
             sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has enabled §6ChatRanks§r!`);
         }
-        if (ChatRanksToggle === false) {
+        if (ChatRanksToggle === false && chatRanksBoolean === true) {
             // Deny
             dynamicPropertyRegistry.set("chatranks_b", false);
             world.setDynamicProperty("chatranks_b", false);
