6766f977de8e6899293db5612989d8d7ccabec9a
Pete9xi <peter95rose@msn.com>
2023-05-08 09:42:19 +0100

    [uiBadpackets] add checks to prevent the settings be set when not required

--- src/gui/modules/uiBadpackets.ts
+++ src/gui/modules/uiBadpackets.ts
@@ -12,32 +12,33 @@ export function uiBADPACKETS(badpacketsResult: ModalFormResponse, player: Player
     const uniqueId = dynamicPropertyRegistry.get(player?.id);
 
     // Get Dynamic Property Boolean
-
+    const badPackets1Boolean = dynamicPropertyRegistry.get("badpackets1_b");
+    const badPackets2Boolean = dynamicPropertyRegistry.get("badpackets2_b");
     // Make sure the user has permissions to run the command
     if (uniqueId !== player.name) {
         return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You need to be Paradox-Opped to configure Badpackets`);
     }
-    if (BadPackets1Toggle === true) {
+    if (BadPackets1Toggle === true && badPackets1Boolean === false) {
         // Allow
         dynamicPropertyRegistry.set("badpackets1_b", true);
         world.setDynamicProperty("badpackets1_b", true);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has enabled §6Badpackets1§r!`);
         BadPackets1();
     }
-    if (BadPackets1Toggle === false) {
+    if (BadPackets1Toggle === false && badPackets1Boolean === true) {
         // Deny
         dynamicPropertyRegistry.set("badpackets1_b", false);
         world.setDynamicProperty("badpackets1_b", false);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has disabled §4Badpackets1§r!`);
     }
-    if (BadPackets2Toggle === true) {
+    if (BadPackets2Toggle === true && badPackets2Boolean === false) {
         // Allow
         dynamicPropertyRegistry.set("badpackets2_b", true);
         world.setDynamicProperty("badpackets2_b", true);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has enabled §6Badpackets2§r!`);
         BadPackets2();
     }
-    if (BadPackets2Toggle === false) {
+    if (BadPackets2Toggle === false && badPackets2Boolean === true) {
         // Deny
         dynamicPropertyRegistry.set("badpackets2_b", false);
         world.setDynamicProperty("badpackets2_b", false);
