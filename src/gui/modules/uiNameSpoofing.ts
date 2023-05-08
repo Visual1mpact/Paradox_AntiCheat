037003d350fae6082ecdd3d2f9a561f1e772e7a9
Pete9xi <peter95rose@msn.com>
2023-05-08 09:44:43 +0100

    [uiNamespoofing] Add checks to prevent settings being set when not required.

--- src/gui/modules/uiNameSpoofing.ts
+++ src/gui/modules/uiNameSpoofing.ts
@@ -12,32 +12,33 @@ export function uiNAMESPOOFING(namespoofingResult: ModalFormResponse, player: Pl
     const uniqueId = dynamicPropertyRegistry.get(player?.id);
 
     // Get Dynamic Property Boolean
-
+    const nameSpoofABoolean = dynamicPropertyRegistry.get("namespoofa_b");
+    const nameSpoofBBoolean = dynamicPropertyRegistry.get("namespoofb_b");
     // Make sure the user has permissions to run the command
     if (uniqueId !== player.name) {
         return sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You need to be Paradox-Opped to configure Name Spoofing`);
     }
-    if (NameSpoofAToggle === true) {
+    if (NameSpoofAToggle === true && nameSpoofABoolean === false) {
         // Allow
         dynamicPropertyRegistry.set("namespoofa_b", true);
         world.setDynamicProperty("namespoofa_b", true);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has enabled §6NamespoofA§r!`);
         NamespoofA();
     }
-    if (NameSpoofAToggle === false) {
+    if (NameSpoofAToggle === false && nameSpoofABoolean === true) {
         // Deny
         dynamicPropertyRegistry.set("namespoofa_b", false);
         world.setDynamicProperty("namespoofa_b", false);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has disabled §4NamespoofA§r!`);
     }
-    if (NameSpoofBToggle === true) {
+    if (NameSpoofBToggle === true && nameSpoofBBoolean === false) {
         // Allow
         dynamicPropertyRegistry.set("namespoofb_b", true);
         world.setDynamicProperty("namespoofb_b", true);
         sendMsg("@a[tag=paradoxOpped]", `§r§4[§6Paradox§4]§r ${player.nameTag}§r has enabled §6NamespoofB§r!`);
         NamespoofB;
     }
-    if (NameSpoofBToggle === false) {
+    if (NameSpoofBToggle === false && nameSpoofBBoolean === true) {
         // Deny
         dynamicPropertyRegistry.set("namespoofb_b", false);
         world.setDynamicProperty("namespoofb_b", false);
