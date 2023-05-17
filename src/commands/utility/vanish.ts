1b3eea50e2b53e5336cb0f90264a2e5d9340e5f6
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-05-17 19:12:56 -0400

    [vanish] Simplify logic by removing redundant code

--- src/commands/utility/vanish.ts
+++ src/commands/utility/vanish.ts
@@ -54,32 +54,18 @@ export async function vanish(message: BeforeChatEvent, args: string[]) {
         return vanishHelp(player, prefix);
     }
 
-    if (player.hasTag("vanish")) {
-        player.addTag("novanish");
-    }
+    const vanishBoolean = player.hasTag("vanish");
 
-    if (player.hasTag("novanish")) {
+    if (vanishBoolean) {
         player.removeTag("vanish");
-    }
-
-    if (player.hasTag("novanish")) {
         player.triggerEvent("unvanish");
         await player.runCommandAsync(`effect @s clear`);
         sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You are no longer vanished.`);
-        sendMsg(`@a[tag=paradoxOpped]`, `${player.nameTag}§r is no longer in vanish.`);
-    }
-
-    if (!player.hasTag("novanish")) {
+        sendMsg(`@a[tag=paradoxOpped]`, `§r§4[§6Paradox§4]§r ${player.nameTag}§r is no longer in vanish.`);
+    } else {
         player.addTag("vanish");
-    }
-
-    if (player.hasTag("vanish") && !player.hasTag("novanish")) {
         player.triggerEvent("vanish");
         sendMsgToPlayer(player, `§r§4[§6Paradox§4]§r You are now vanished!`);
-        sendMsg(`@a[tag=paradoxOpped]`, `${player.nameTag}§r is now vanished!`);
-    }
-
-    if (player.hasTag("novanish")) {
-        player.removeTag("novanish");
+        sendMsg(`@a[tag=paradoxOpped]`, `§r§4[§6Paradox§4]§r ${player.nameTag}§r is now vanished!`);
     }
 }
