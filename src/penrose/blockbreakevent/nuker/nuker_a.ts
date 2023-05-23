3855fa7cd467cff6bad810a88b6b3338376f0ccc
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-05-23 16:57:39 -0400

    [nukerA] Remove blockCounter Map

--- src/penrose/blockbreakevent/nuker/nuker_a.ts
+++ src/penrose/blockbreakevent/nuker/nuker_a.ts
@@ -4,12 +4,10 @@ import { dynamicPropertyRegistry } from "../../worldinitializeevent/registry.js"
 import { kickablePlayers } from "../../../kickcheck.js";
 
 const lastBreakTime = new Map<string, number>();
-const blockCounter = new Map<string, number>();
 
 async function nukera(object: BlockBreakEvent): Promise<void> {
     const antiNukerABoolean = dynamicPropertyRegistry.get("antinukera_b");
     if (antiNukerABoolean === false) {
-        blockCounter.clear();
         lastBreakTime.clear();
         world.events.blockBreak.unsubscribe(nukera);
         return;
@@ -31,47 +29,35 @@ async function nukera(object: BlockBreakEvent): Promise<void> {
     const timerExpired = startTimer("nukera", player.id, Date.now());
     if (timerExpired.namespace.indexOf("nukera") !== -1 && timerExpired.expired) {
         const deletedKey = timerExpired.key; // extract the key without the namespace prefix
-        blockCounter.delete(deletedKey);
         lastBreakTime.delete(deletedKey);
     }
-
-    let counter = blockCounter.get(player.id) || 0;
 
     const now = Date.now();
     const lastBreak = lastBreakTime.get(player.id);
     if (lastBreak && now - lastBreak < 50) {
-        counter++;
-        if (counter >= 2) {
-            const blockLoc = dimension.getBlock(new Vector(x, y, z));
-            const blockID = brokenBlockPermutation.clone();
+        const blockLoc = dimension.getBlock(new Vector(x, y, z));
+        const blockID = brokenBlockPermutation.clone();
 
-            flag(player, "Nuker", "A", "Break", null, null, null, null, false, null);
-            blockLoc.setPermutation(blockID);
-            blockCounter.delete(player.id);
-            lastBreakTime.delete(player.id);
+        flag(player, "Nuker", "A", "Break", null, null, null, null, false, null);
+        blockLoc.setPermutation(blockID);
+        lastBreakTime.delete(player.id);
 
-            try {
-                await player.runCommandAsync(`kill @e[x=${x},y=${y},z=${z},r=10,c=1,type=item]`);
-            } catch (error) {}
+        try {
+            await player.runCommandAsync(`kill @e[x=${x},y=${y},z=${z},r=10,c=1,type=item]`);
+        } catch (error) {}
 
-            try {
-                player.addTag("Reason:Anti-NukerA");
-                player.addTag("By:Paradox");
-                player.addTag("isBanned");
-            } catch (error) {
-                kickablePlayers.add(player);
-                player.triggerEvent("paradox:kick");
-            }
-            return;
+        try {
+            player.addTag("Reason:Anti-NukerA");
+            player.addTag("By:Paradox");
+            player.addTag("isBanned");
+        } catch (error) {
+            kickablePlayers.add(player);
+            player.triggerEvent("paradox:kick");
         }
-        // Reset the counter if a certain condition is not met
-        counter = 0;
+        return;
     } else {
         lastBreakTime.set(player.id, now);
     }
-
-    // Update the counter value in the map
-    blockCounter.set(player.id, counter);
 }
 
 const NukerA = () => {
