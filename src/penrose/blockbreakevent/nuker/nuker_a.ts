a28f636004dc884a9cc71a44d780c9dfb01446ef
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-05-25 21:36:33 -0400

    [nukerA] Ignore vegetation

--- src/penrose/blockbreakevent/nuker/nuker_a.ts
+++ src/penrose/blockbreakevent/nuker/nuker_a.ts
@@ -32,9 +32,67 @@ async function nukera(object: BlockBreakEvent): Promise<void> {
         lastBreakTime.delete(deletedKey);
     }
 
+    // Ignore vegetation
+    const vegetation = [
+        "minecraft:yellow_flower",
+        "minecraft:red_flower",
+        "minecraft:double_plant",
+        "minecraft:wither_rose",
+        "minecraft:tallgrass",
+        "minecraft:hanging_roots",
+        "minecraft:leaves",
+        "minecraft:leaves2",
+        "minecraft:azalea_leaves",
+        "minecraft:azalea_leaves_flowered",
+        "minecraft:deadbush",
+        "minecraft:cocoa",
+        "minecraft:chorus_plant",
+        "minecraft:chorus_flower",
+        "minecraft:cave_vines",
+        "minecraft:cave_vines_body_with_berries",
+        "minecraft:cave_vines_head_with_berries",
+        "minecraft:glow_berries",
+        "minecraft:carrots",
+        "minecraft:cactus",
+        "minecraft:big_dripleaf",
+        "minecraft:beetroot",
+        "minecraft:bamboo",
+        "minecraft:bamboo_sapling",
+        "minecraft:azalea",
+        "minecraft:flowering_azalea",
+        "minecraft:waterlily",
+        "minecraft:melon_block",
+        "minecraft:melon_stem",
+        "minecraft:potatoes",
+        "minecraft:pumpkin",
+        "minecraft:carved_pumpkin",
+        "minecraft:pumpkin_stem",
+        "minecraft:sapling",
+        "minecraft:seagrass",
+        "minecraft:small_dripleaf_block",
+        "minecraft:spore_blossom",
+        "minecraft:reeds",
+        "minecraft:sweet_berry_bush",
+        "minecraft:sweet_berries",
+        "minecraft:vine",
+        "minecraft:wheat",
+        "minecraft:kelp",
+        "minecraft:crimson_fungus",
+        "minecraft:warped_fungus",
+        "minecraft:glow_lichen",
+        "minecraft:brown_mushroom",
+        "minecraft:red_mushroom",
+        "minecraft:nether_wart",
+        "minecraft:nether_sprouts",
+        "minecraft:crimson_roots",
+        "minecraft:warped_roots",
+        "minecraft:twisting_vines",
+        "minecraft:weeping_vines",
+    ];
+
     const now = Date.now();
     const lastBreak = lastBreakTime.get(player.id);
-    if (lastBreak && now - lastBreak < 15) {
+    if (vegetation.indexOf(brokenBlockPermutation.type.id) === -1 && lastBreak && now - lastBreak < 15) {
         const blockLoc = dimension.getBlock(new Vector(x, y, z));
         const blockID = brokenBlockPermutation.clone();
 
