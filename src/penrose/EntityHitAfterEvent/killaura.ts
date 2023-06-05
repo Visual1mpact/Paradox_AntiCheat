b44fbf6db1c505a311eafb87585e9f49356c525a
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-06-05 19:57:36 -0400

    [killaura] Refactor killaura function and introduce isAttackingFromOutsideView
    In this commit, the killaura function has been refactored to include a new helper function called isAttackingFromOutsideView. This helper function calculates the distance between two players and verifies that it is equal to or greater than 2 blocks, acting as an artificial collision in Minecraft Bedrock.
    
    By introducing the isAttackingFromOutsideView function, we ensure that the angle calculation accurately represents the attacker's view direction relative to the hit entity. This modification addresses the issue of players being able to face the hit entity from an angle greater than 90 degrees when there is no collision detection in Minecraft Bedrock.
    
    These changes improve the accuracy of the angle comparison and enhance the functionality of the killaura feature in the game.

--- src/penrose/EntityHitAfterEvent/killaura.ts
+++ src/penrose/EntityHitAfterEvent/killaura.ts
@@ -3,6 +3,53 @@ import { dynamicPropertyRegistry } from "../WorldInitializeAfterEvent/registry";
 import { flag } from "../../util";
 import { MinecraftEffectTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 
+function isAttackingFromOutsideView(player1: Player, player2: Player) {
+    // Calculate the distance between the two players
+    const distance = Math.sqrt(Math.pow(player2.location.x - player1.location.x, 2) + Math.pow(player2.location.y - player1.location.y, 2) + Math.pow(player2.location.z - player1.location.z, 2));
+
+    // Check if the distance is greater than or equal to 1.5 blocks
+    if (distance >= 2) {
+        // Get the view direction vectors of both players
+        const player1ViewDir = player1.getViewDirection();
+        const player2ViewDir = player2.getViewDirection();
+
+        // Calculate the vector from player1 to player2
+        const player1ToPlayer2 = {
+            x: player2.location.x - player1.location.x,
+            y: player2.location.y - player1.location.y,
+            z: player2.location.z - player1.location.z,
+        };
+
+        // Calculate the vector from player2 to player1
+        const player2ToPlayer1 = {
+            x: player1.location.x - player2.location.x,
+            y: player1.location.y - player2.location.y,
+            z: player1.location.z - player2.location.z,
+        };
+
+        // Calculate the dot products
+        const dotProduct1 = player1ViewDir.x * player1ToPlayer2.x + player1ViewDir.y * player1ToPlayer2.y + player1ViewDir.z * player1ToPlayer2.z;
+        const dotProduct2 = player2ViewDir.x * player2ToPlayer1.x + player2ViewDir.y * player2ToPlayer1.y + player2ViewDir.z * player2ToPlayer1.z;
+
+        // Calculate the magnitudes of the vectors
+        const player1ToPlayer2Magnitude = Math.sqrt(player1ToPlayer2.x * player1ToPlayer2.x + player1ToPlayer2.y * player1ToPlayer2.y + player1ToPlayer2.z * player1ToPlayer2.z);
+        const player2ToPlayer1Magnitude = Math.sqrt(player2ToPlayer1.x * player2ToPlayer1.x + player2ToPlayer1.y * player2ToPlayer1.y + player2ToPlayer1.z * player2ToPlayer1.z);
+
+        // Normalize the dot products
+        const normalizedDotProduct1 = dotProduct1 / (player1ToPlayer2Magnitude * Math.sqrt(player1ViewDir.x * player1ViewDir.x + player1ViewDir.y * player1ViewDir.y + player1ViewDir.z * player1ViewDir.z));
+        const normalizedDotProduct2 = dotProduct2 / (player2ToPlayer1Magnitude * Math.sqrt(player2ViewDir.x * player2ViewDir.x + player2ViewDir.y * player2ViewDir.y + player2ViewDir.z * player2ViewDir.z));
+
+        // Convert dot products to angles in degrees
+        const angle1 = Math.acos(normalizedDotProduct1) * (180 / Math.PI);
+        const angle2 = Math.acos(normalizedDotProduct2) * (180 / Math.PI);
+
+        // Compare angles with the threshold of 90 degrees
+        return angle1 > 90 && angle2 > 90;
+    }
+
+    return false;
+}
+
 function killaura(obj: EntityHitAfterEvent) {
     // Get Dynamic Property
     const antiKillAuraBoolean = dynamicPropertyRegistry.get("antikillaura_b");
@@ -29,28 +76,10 @@ function killaura(obj: EntityHitAfterEvent) {
         return;
     }
 
-    // Get the view direction of the attacking entity
-    const { x: entityX, y: entityY, z: entityZ } = entity.getViewDirection();
+    const angleBoolean = isAttackingFromOutsideView(entity, hitEntity);
 
-    // Get the location of the hit entity
-    const { x: hitX, y: hitY, z: hitZ } = hitEntity.location;
-
-    // Calculate the vector from the attacking entity to the hit entity
-    const dx = hitX - entity.location.x;
-    const dy = hitY - entity.location.y;
-    const dz = hitZ - entity.location.z;
-
-    // Calculate the dot product and magnitudes
-    const dotProduct = entityX * dx + entityY * dy + entityZ * dz;
-    const entityMagnitude = Math.sqrt(entityX * entityX + entityY * entityY + entityZ * entityZ);
-    const vectorMagnitude = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-    // Calculate the angle in radians
-    const cosAngle = dotProduct / (entityMagnitude * vectorMagnitude);
-    const angle = Math.acos(cosAngle) * (180 / Math.PI);
-
-    if (angle > 160) {
-        // Entity is facing hitEntity at an angle greater than 160 degrees
+    if (angleBoolean) {
+        // Entity is facing hitEntity at an angle greater than 90 degrees
         flag(entity, "KillAura", "A", "Combat", null, null, null, null, false);
         // Blindness
         entity.addEffect(MinecraftEffectTypes.Blindness, 1000000, { amplifier: 255, showParticles: true });
