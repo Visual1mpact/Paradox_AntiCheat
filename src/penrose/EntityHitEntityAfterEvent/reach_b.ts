b5ea5b42cc21b8af04d1a02114210b74872a829a
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-07-26 11:26:54 -0400

    [reachB] Improve Calculation
    The commit introduces enhancements to the ReachB calculation. Key modifications include:
    
    1. Recording player locations and velocities: The script now stores the previous locations and velocities of both hitEntity and damagingEntity using a Map. This ensures that player movements are taken into account during reach distance calculations.
    
    2. Reach Distance calculation with velocities: The calculateReachDistanceWithVelocity function now incorporates player velocities to provide a more accurate reach distance calculation.
    
    3. Rounding down Reach Distance: The reachDistance value is now rounded down to the nearest integer using Math.floor() before being compared with the configured reachB.reach value.
    
    4. Refactoring and organization: The code has been refactored and organized for better readability and maintainability.
    
    The changes aim to minimize false reach flags and improve the accuracy of reach distance checks during player attacks. Testing and monitoring reveal better reach distance results while considering player movements. The improvements should enhance the gameplay experience on the server.

--- src/penrose/EntityHitEntityAfterEvent/reach_b.ts
+++ src/penrose/EntityHitEntityAfterEvent/reach_b.ts
@@ -1,22 +1,40 @@
-import { world, Player, EntityHitEntityAfterEvent } from "@minecraft/server";
+import { world, Player, EntityHitEntityAfterEvent, system } from "@minecraft/server";
 import config from "../../data/config.js";
 import { flag } from "../../util.js";
 import { dynamicPropertyRegistry } from "../WorldInitializeAfterEvent/registry.js";
+
+// Store the previous locations and velocities of hitEntity and damagingEntity
+const previousData: Map<
+    Player,
+    {
+        location: { x: number; y: number; z: number };
+        velocity: { x: number; y: number; z: number };
+    }
+> = new Map();
+
+function recordPlayerData(player: Player) {
+    // Store the current location and velocity of the player
+    previousData.set(player, {
+        location: { ...player.location },
+        velocity: { ...player.getVelocity() },
+    });
+}
 
 function reachb(object: EntityHitEntityAfterEvent) {
     // Get Dynamic Property
     const reachBBoolean = dynamicPropertyRegistry.get("reachb_b");
 
-    // Unsubscribe if disabled in-game
+    // Unsubscribe if disabled in-game and stop the interval
     if (reachBBoolean === false) {
         world.afterEvents.entityHitEntity.unsubscribe(reachb);
+        stopLocationRecordingInterval();
         return;
     }
 
     // Properties from class
     const { hitEntity, damagingEntity } = object;
 
-    // If a block or not a player entity then ignore
+    // If not a player entity then ignore
     if (!(hitEntity instanceof Player) || !(damagingEntity instanceof Player)) {
         return;
     }
@@ -29,22 +47,109 @@ function reachb(object: EntityHitEntityAfterEvent) {
         return;
     }
 
-    // Entity coordinates
-    const { x, y, z } = hitEntity.location;
-    // Player coordinates
-    const { x: x1, y: y1, z: z1 } = damagingEntity.location;
+    // Get the previous recorded data of hitEntity and damagingEntity
+    const previousHitEntityData = previousData.get(hitEntity);
+    const previousDamagingEntityData = previousData.get(damagingEntity);
 
-    const dx = x - x1;
-    const dy = y - y1;
-    const dz = z - z1;
-    const distanceSquared = Math.floor(dx * dx + dy * dy + dz * dz);
+    if (previousHitEntityData && previousDamagingEntityData && isWithinReach(previousHitEntityData, previousDamagingEntityData, hitEntity.location, damagingEntity.location)) {
+        // Update the recorded data for hitEntity and damagingEntity
+        recordPlayerData(hitEntity);
+        recordPlayerData(damagingEntity);
+        return;
+    }
 
-    if (distanceSquared > config.modules.reachB.reach * config.modules.reachB.reach) {
-        flag(damagingEntity, "Reach", "B", "Attack", null, null, "reach", Math.sqrt(distanceSquared).toFixed(3), false);
+    // Reset the reach distance after each hit
+    resetReachDistance(damagingEntity);
+
+    // Calculate the actual reach distance with velocities
+    const reachDistance = calculateReachDistanceWithVelocity(previousDamagingEntityData, previousHitEntityData, damagingEntity.location, hitEntity.location);
+
+    // Round down the reachDistance to the nearest integer
+    const roundedReachDistance = Math.floor(reachDistance);
+    if (roundedReachDistance > config.modules.reachB.reach) {
+        flag(damagingEntity, "Reach", "B", "Attack", null, null, "reach", reachDistance.toString(), false);
     }
 }
 
+function isWithinReach(
+    previousHitEntityData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    previousDamagingEntityData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    currentHitEntityLocation: { x: number; y: number; z: number },
+    currentDamagingEntityLocation: { x: number; y: number; z: number }
+): boolean {
+    if (!previousHitEntityData || !previousDamagingEntityData) {
+        return false;
+    }
+
+    // Calculate the distance squared between the previous and current locations of hitEntity and damagingEntity
+    const hitEntityDistanceSquared = calculateDistanceSquared(previousHitEntityData.location, currentHitEntityLocation);
+    const damagingEntityDistanceSquared = calculateDistanceSquared(previousDamagingEntityData.location, currentDamagingEntityLocation);
+
+    // Compare the distances with the allowed distance squared
+    const allowedDistanceSquared = config.modules.reachB.reach;
+    return hitEntityDistanceSquared <= allowedDistanceSquared && damagingEntityDistanceSquared <= allowedDistanceSquared;
+}
+
+function calculateDistanceSquared(position1: { x: number; y: number; z: number }, position2: { x: number; y: number; z: number }): number {
+    const dx = position2.x - position1.x;
+    const dy = position2.y - position1.y;
+    const dz = position2.z - position1.z;
+    return dx * dx + dy * dy + dz * dz;
+}
+
+function calculateReachDistanceWithVelocity(
+    previousDamagingEntityData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    previousHitEntityData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    currentDamagingEntityLocation: { x: number; y: number; z: number },
+    currentHitEntityLocation: { x: number; y: number; z: number }
+): number {
+    if (!previousDamagingEntityData) {
+        // Use the current damaging entity location as the previous location if not available
+        previousDamagingEntityData = {
+            location: { ...currentDamagingEntityLocation },
+            velocity: { x: 0, y: 0, z: 0 },
+        };
+    }
+    if (!previousHitEntityData) {
+        // Use the current hit entity location as the previous location if not available
+        previousHitEntityData = {
+            location: { ...currentHitEntityLocation },
+            velocity: { x: 0, y: 0, z: 0 },
+        };
+    }
+
+    // Incorporate velocities into the reach distance calculation
+    const dx = currentHitEntityLocation.x + previousHitEntityData.velocity.x - previousDamagingEntityData.location.x - previousDamagingEntityData.velocity.x;
+    const dy = currentHitEntityLocation.y + previousHitEntityData.velocity.y - previousDamagingEntityData.location.y - previousDamagingEntityData.velocity.y;
+    const dz = currentHitEntityLocation.z + previousHitEntityData.velocity.z - previousDamagingEntityData.location.z - previousDamagingEntityData.velocity.z;
+
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+function resetReachDistance(player: Player) {
+    // Reset the previous location and velocity of the player to the current location and velocity
+    recordPlayerData(player);
+}
+
+// Interval ID to stop the recording
+let locationRecordingInterval: number;
+
+function startLocationRecordingInterval() {
+    locationRecordingInterval = system.runInterval(() => {
+        for (const player of world.getAllPlayers()) {
+            recordPlayerData(player);
+        }
+    }, 20);
+}
+
+function stopLocationRecordingInterval() {
+    system.clearRun(locationRecordingInterval);
+}
+
 const ReachB = () => {
+    // Subscribe to the tick event to record player locations and velocities
+    startLocationRecordingInterval();
+
     world.afterEvents.entityHitEntity.subscribe(reachb);
 };
 
