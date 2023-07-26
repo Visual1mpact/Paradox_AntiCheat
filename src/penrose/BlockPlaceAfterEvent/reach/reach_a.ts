3112a63f329321d31041c89aaaae79ab941aed80
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-07-26 11:31:01 -0400

    [reachA] Enhance Calculation and Block Placement
    The commit introduces improvements to the ReachA calculation and block placement logic. The key changes include:
    
    1. Recording player locations and velocities: The script now maintains a Map to store the previous locations and velocities of both the block and player. This enhancement enables the consideration of player movements during reach distance calculations for block placements.
    
    2. Reach Distance calculation with velocities: The calculateReachDistanceWithVelocity function now takes into account the player's velocity to provide a more accurate reach distance calculation when placing blocks.
    
    3. Rounding down Reach Distance: The reachDistance value is now rounded down to the nearest integer using Math.floor() before being compared with the configured reachA.reach value. This ensures consistent and precise reach distance comparisons.
    
    4. Refactoring and organization: The code has been refactored and organized for better readability and maintainability.
    
    The changes aim to enhance the accuracy of reach distance checks during block placements and provide a more reliable gameplay experience on the server. The modifications also account for player movements and interactions with blocks, contributing to improved reach distance calculations.

--- src/penrose/BlockPlaceAfterEvent/reach/reach_a.ts
+++ src/penrose/BlockPlaceAfterEvent/reach/reach_a.ts
@@ -1,20 +1,43 @@
-import { world, MinecraftBlockTypes, BlockPlaceAfterEvent } from "@minecraft/server";
+import { world, Player, system, BlockPlaceAfterEvent, MinecraftBlockTypes, Block } from "@minecraft/server";
 import config from "../../../data/config.js";
-import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
 import { flag } from "../../../util.js";
+import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
+
+// Store the previous locations and velocities of block and player
+const previousData: Map<
+    Player,
+    {
+        location: { x: number; y: number; z: number };
+        velocity: { x: number; y: number; z: number };
+    }
+> = new Map();
+
+function recordPlayerData(player: Player) {
+    // Store the current location and velocity of the player
+    previousData.set(player, {
+        location: { ...player.location },
+        velocity: { ...player.getVelocity() },
+    });
+}
 
 function reacha(object: BlockPlaceAfterEvent) {
     // Get Dynamic Property
     const reachABoolean = dynamicPropertyRegistry.get("reacha_b");
 
-    // Unsubscribe if disabled in-game
+    // Unsubscribe if disabled in-game and stop the interval
     if (reachABoolean === false) {
         world.afterEvents.blockPlace.unsubscribe(reacha);
+        stopLocationRecordingInterval();
         return;
     }
 
     // Properties from class
     const { block, player, dimension } = object;
+
+    // If not a player entity or a block then ignore
+    if (!(player instanceof Player) || !(block instanceof Block)) {
+        return;
+    }
 
     // Get unique ID
     const uniqueId = dynamicPropertyRegistry.get(player?.id);
@@ -24,24 +47,110 @@ function reacha(object: BlockPlaceAfterEvent) {
         return;
     }
 
-    // Block coordinates
-    const { x, y, z } = block.location;
-    // Player coordinates
-    const { x: x1, y: y1, z: z1 } = player.location;
+    // Get the previous recorded data of block and player
+    const previousblockData = previousData.get(block);
+    const previousplayerData = previousData.get(player);
 
-    // Calculate the distance squared between the player and the block being placed
-    const dx = x - x1;
-    const dy = y - y1;
-    const dz = z - z1;
-    const distanceSquared = Math.floor(dx * dx + dy * dy + dz * dz);
+    if (previousblockData && previousplayerData && isWithinReach(previousblockData, previousplayerData, block.location, player.location)) {
+        // Update the recorded data for block and player
+        recordPlayerData(block);
+        recordPlayerData(player);
+        return;
+    }
 
-    if (distanceSquared > config.modules.reachA.reach * config.modules.reachA.reach) {
-        dimension.getBlock({ x: x, y: y, z: z }).setType(MinecraftBlockTypes.air);
-        flag(player, "Reach", "A", "Placement", null, null, "reach", Math.sqrt(distanceSquared).toFixed(3), false);
+    // Reset the reach distance after each hit
+    resetReachDistance(player);
+
+    // Calculate the actual reach distance with velocities
+    const reachDistance = calculateReachDistanceWithVelocity(previousplayerData, previousblockData, player.location, block.location);
+
+    // Round down the reachDistance to the nearest integer
+    const roundedReachDistance = Math.floor(reachDistance);
+    if (roundedReachDistance > config.modules.reachA.reach) {
+        dimension.getBlock(block.location).setType(MinecraftBlockTypes.air);
+        flag(player, "Reach", "A", "Placement", null, null, "reach", roundedReachDistance.toString(), false);
     }
 }
 
+function isWithinReach(
+    previousblockData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    previousplayerData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    currentblockLocation: { x: number; y: number; z: number },
+    currentplayerLocation: { x: number; y: number; z: number }
+): boolean {
+    if (!previousblockData || !previousplayerData) {
+        return false;
+    }
+
+    // Calculate the distance squared between the previous and current locations of block and player
+    const blockDistanceSquared = calculateDistanceSquared(previousblockData.location, currentblockLocation);
+    const playerDistanceSquared = calculateDistanceSquared(previousplayerData.location, currentplayerLocation);
+
+    // Compare the distances with the allowed distance squared
+    const allowedDistanceSquared = config.modules.reachA.reach;
+    return blockDistanceSquared <= allowedDistanceSquared && playerDistanceSquared <= allowedDistanceSquared;
+}
+
+function calculateDistanceSquared(position1: { x: number; y: number; z: number }, position2: { x: number; y: number; z: number }): number {
+    const dx = position2.x - position1.x;
+    const dy = position2.y - position1.y;
+    const dz = position2.z - position1.z;
+    return dx * dx + dy * dy + dz * dz;
+}
+
+function calculateReachDistanceWithVelocity(
+    previousplayerData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    previousblockData: { location: { x: number; y: number; z: number }; velocity: { x: number; y: number; z: number } },
+    currentplayerLocation: { x: number; y: number; z: number },
+    currentblockLocation: { x: number; y: number; z: number }
+): number {
+    if (!previousplayerData) {
+        // Use the current damaging entity location as the previous location if not available
+        previousplayerData = {
+            location: { ...currentplayerLocation },
+            velocity: { x: 0, y: 0, z: 0 },
+        };
+    }
+    if (!previousblockData) {
+        // Use the current hit entity location as the previous location if not available
+        previousblockData = {
+            location: { ...currentblockLocation },
+            velocity: { x: 0, y: 0, z: 0 },
+        };
+    }
+
+    // Incorporate velocities into the reach distance calculation
+    const dx = currentblockLocation.x + previousblockData.velocity.x - previousplayerData.location.x - previousplayerData.velocity.x;
+    const dy = currentblockLocation.y + previousblockData.velocity.y - previousplayerData.location.y - previousplayerData.velocity.y;
+    const dz = currentblockLocation.z + previousblockData.velocity.z - previousplayerData.location.z - previousplayerData.velocity.z;
+
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+function resetReachDistance(player: Player) {
+    // Reset the previous location and velocity of the player to the current location and velocity
+    recordPlayerData(player);
+}
+
+// Interval ID to stop the recording
+let locationRecordingInterval: number;
+
+function startLocationRecordingInterval() {
+    locationRecordingInterval = system.runInterval(() => {
+        for (const player of world.getAllPlayers()) {
+            recordPlayerData(player);
+        }
+    }, 20);
+}
+
+function stopLocationRecordingInterval() {
+    system.clearRun(locationRecordingInterval);
+}
+
 const ReachA = () => {
+    // Subscribe to the tick event to record player locations and velocities
+    startLocationRecordingInterval();
+
     world.afterEvents.blockPlace.subscribe(reacha);
 };
 
