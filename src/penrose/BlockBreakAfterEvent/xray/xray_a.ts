f1dac0cebe2bccf4681b25800b64adaf35265856
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-07-26 15:43:55 -0400

    [xrayA] Enhanced X-ray behavior detection
    This patch introduces significant changes to the X-ray detection system. The previous code solely focused on notifying players when they broke specific blocks. I felt this was barbaric and it badly spammed chat making it inefficient, and aggravating. The new code enhances this functionality by detecting suspicious behavior and notifying other players when a certain threshold of blocks is broken within a minute. Additionally, the new code also handles player logout events to prevent memory leaks and ensure proper data management.
    
    Changes Made:
    1. Added a threshold variable (XRAY_THRESHOLD) to adjust the number of X-ray blocks broken required to trigger a notification.
    2. Created an interface XrayData to store the lastNotifyTime for each player's X-ray data.
    3. Utilized global variables to store xrayData and blocksBrokenCount maps, making them accessible across different functions.
    4. Implemented the isXraySuspicious function to check for suspicious X-ray behavior based on the number of blocks broken and the time since the last notification.
    5. Added the onPlayerLogout function to handle player logout events and remove the player's data from the maps upon logout.
    6. Modified the xraya function to increment the count of broken blocks for each player and reset the timer whenever new ore data is added.
    7. Implemented the notification system to alert other players when a suspicious X-ray activity is detected and reset the block broken count after notifying.

--- src/penrose/BlockBreakAfterEvent/xray/xray_a.ts
+++ src/penrose/BlockBreakAfterEvent/xray/xray_a.ts
@@ -1,7 +1,34 @@
-import { BlockBreakAfterEvent, world } from "@minecraft/server";
+import { BlockBreakAfterEvent, PlayerLeaveAfterEvent, world } from "@minecraft/server";
 import { xrayblocks } from "../../../data/xray.js";
 import { sendMsg } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
+
+const XRAY_THRESHOLD = 5; // Adjust this threshold based on your observations
+
+interface XrayData {
+    lastNotifyTime: number;
+}
+
+// Use a global variable to store xrayData map
+let xrayData: Map<string, XrayData> = new Map();
+
+// Use a global variable to store the number of blocks broken within the last minute
+let blocksBrokenCount: Map<string, number> = new Map();
+
+function isXraySuspicious(playerName: string): boolean {
+    const data = xrayData.get(playerName);
+    if (!data) return false;
+    const currentTime = Date.now();
+    const timeSinceLastNotify = currentTime - data.lastNotifyTime;
+    return blocksBrokenCount.get(playerName) >= XRAY_THRESHOLD && timeSinceLastNotify <= 60000;
+}
+
+function onPlayerLogout(event: PlayerLeaveAfterEvent): void {
+    // Remove the player's data from the map when they log off
+    const playerName = event.playerName;
+    xrayData.delete(playerName);
+    blocksBrokenCount.delete(playerName);
+}
 
 function xraya(object: BlockBreakAfterEvent) {
     // Get Dynamic Property
@@ -9,6 +36,8 @@ function xraya(object: BlockBreakAfterEvent) {
 
     // Unsubscribe if disabled in-game
     if (xrayBoolean === false) {
+        xrayData.clear();
+        blocksBrokenCount.clear();
         world.afterEvents.blockBreak.unsubscribe(xraya);
         return;
     }
@@ -28,12 +57,35 @@ function xraya(object: BlockBreakAfterEvent) {
     const { x, y, z } = player.location;
 
     if (brokenBlockPermutation.type.id in xrayblocks) {
-        sendMsg(`@a[tag=notify]`, `§r§4[§6Paradox§4]§r §4[Xray]§r ${player.name}§r§6 has found §r1x ${brokenBlockPermutation.type.id.replace("minecraft:", "")}§6 at X=§r${x.toFixed(0)}§6 Y=§r${y.toFixed(0)}§6 Z=§r${z.toFixed(0)}.`);
+        const playerName = player.name;
+        if (!xrayData.has(playerName)) {
+            xrayData.set(playerName, { lastNotifyTime: 0 });
+        }
+        if (!blocksBrokenCount.has(playerName)) {
+            blocksBrokenCount.set(playerName, 0);
+        }
+        const playerData = xrayData.get(playerName)!;
+
+        // Increment the count for the player
+        blocksBrokenCount.set(playerName, (blocksBrokenCount.get(playerName) || 0) + 1);
+
+        // Reset the timer whenever we add new ore data
+        playerData.lastNotifyTime = Date.now();
+
+        if (isXraySuspicious(playerName)) {
+            sendMsg(
+                `@a[tag=notify]`,
+                `§r§4[§6Paradox§4]§r §4[Xray]§r ${playerName}§r§6 has found §r${blocksBrokenCount.get(playerName)}x ${brokenBlockPermutation.type.id.replace("minecraft:", "")}§6 at X=§r${x.toFixed(0)}§6 Y=§r${y.toFixed(0)}§6 Z=§r${z.toFixed(0)}.`
+            );
+            // Reset the count after notifying
+            blocksBrokenCount.set(playerName, 0);
+        }
     }
 }
 
 const XrayA = () => {
     world.afterEvents.blockBreak.subscribe(xraya);
+    world.afterEvents.playerLeave.subscribe(onPlayerLogout); // Subscribe to player logout events
 };
 
 export { XrayA };
