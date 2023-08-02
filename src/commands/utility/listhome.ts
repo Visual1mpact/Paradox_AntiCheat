054da8111076c453e0adcaf04def4f0a507d05f1
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-02 17:35:25 -0400

    Update saved locations to support AES encryption
    Currently set up to be backward compatible with the old method only for a short time and then such code will be removed.

--- src/commands/utility/listhome.ts
+++ src/commands/utility/listhome.ts
@@ -52,20 +52,18 @@ export function listhome(message: ChatSendAfterEvent, args: string[]) {
     const tags = player.getTags();
     let counter = 0;
     for (let i = 0; i < tags.length; i++) {
-        /**
-         * This first if statement is to verify if they have old coordinates
-         * not encrypted. If so then we encrypt it now. This is only a temporary
-         * patch to minimize players having to manually record and remove the old
-         * tags. Eventually this will be removed.
-         */
-        if (tags[i].startsWith("LocationHome:")) {
-            player.removeTag(tags[i]);
-            tags[i] = encryptString(tags[i], String(salt));
-            player.addTag(tags[i]);
-        }
+        // 6f78 is temporary and will be removed
         if (tags[i].startsWith("6f78")) {
+            // Remove old encryption
+            player.removeTag(tags[i])
+            // Change to AES Encryption so we can abandon the old method
+            tags[i] = decryptString(tags[i], salt as string);
+            tags[i] = encryptString(tags[i], salt as string)
+            player.addTag(tags[i])
+        }
+        if (tags[i].startsWith("1337")) {
             // Decode it so we can verify it
-            tags[i] = decryptString(tags[i], String(salt));
+            tags[i] = decryptString(tags[i], salt as string);
             // If invalid then skip it
             if (tags[i].startsWith("LocationHome:") === false) {
                 continue;
