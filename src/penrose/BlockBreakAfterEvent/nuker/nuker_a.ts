33bea93f8679f82e1ba084776eb14e85852ea87c
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-05 17:02:28 -0400

    [nukerA] Update list of vegetation

--- src/penrose/BlockBreakAfterEvent/nuker/nuker_a.ts
+++ src/penrose/BlockBreakAfterEvent/nuker/nuker_a.ts
@@ -1,7 +1,7 @@
 import { world, BlockBreakAfterEvent, system, EntityQueryOptions, PlayerLeaveAfterEvent } from "@minecraft/server";
 import { flag } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
-import { MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index.js";
+import { MinecraftBlockTypes, MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index.js";
 
 const lastBreakTime = new Map<string, number>();
 const breakCounter = new Map<string, number>();
@@ -32,60 +32,140 @@ async function nukera(object: BlockBreakAfterEvent): Promise<void> {
 
     // Ignore vegetation
     const vegetation = [
-        "minecraft:yellow_flower",
-        "minecraft:red_flower",
-        "minecraft:double_plant",
-        "minecraft:wither_rose",
-        "minecraft:tallgrass",
-        "minecraft:hanging_roots",
+        /**
+         * Leaves
+         *
+         * Oak, Spruce, Birch, Jungle, Acacia, Dark Oak,
+         * Azalea, Flowering Azalea, Mangrove, Cherry.
+         */
         "minecraft:leaves",
         "minecraft:leaves2",
         "minecraft:azalea_leaves",
         "minecraft:azalea_leaves_flowered",
-        "minecraft:deadbush",
-        "minecraft:cocoa",
-        "minecraft:chorus_plant",
-        "minecraft:chorus_flower",
-        "minecraft:cave_vines",
-        "minecraft:cave_vines_body_with_berries",
-        "minecraft:cave_vines_head_with_berries",
-        "minecraft:glow_berries",
-        "minecraft:carrots",
-        "minecraft:cactus",
-        "minecraft:big_dripleaf",
-        "minecraft:beetroot",
-        "minecraft:bamboo",
+        "minecraft:cherry_leaves",
+        "minecraft:mangrove_leaves",
+
+        /**
+         * Saplings
+         *
+         * Oak, Sapling, Birch, Jungle, Acacia, Dark Oak,
+         * Azalea, Flowering Azalea, Mangove Propagule, Cherry,
+         * Bamboo.
+         */
         "minecraft:bamboo_sapling",
-        "minecraft:azalea",
+        "minecraft:sapling",
+        "minecraft:cherry_sapling",
+
+        /**
+         * Flowers
+         *
+         * Allium, Azure Bluet, Blue Orchid, Cornflower, Dandelion,
+         * Lilac, Lily of the Valley, Orange Tulip, Oxeye Daisy,
+         * Peony, Pink Tulip, Poppy, Red Tulip, Rose Bush, Sunflower,
+         * White Tulip, Wither Rose, Chorus.
+         */
+        "minecraft:yellow_flower",
+        "minecraft:red_flower",
+        "minecraft:chorus_flower",
         "minecraft:flowering_azalea",
-        "minecraft:waterlily",
+        "minecraft:azalea_leaves_flowered",
+        "minecraft:wither_rose",
+
+        /**
+         * Mushrooms
+         *
+         * Brown Mushroom, Brown Mushroom Block, Mushroom Stem,
+         * Red Mushroom, Red Mushroom Block.
+         */
+        "minecraft:brown_mushroom",
+        "minecraft:red_mushroom",
+        "minecraft:brown_mushroom_block",
+        "minecraft:red_mushroom_block",
+
+        /**
+         * Crops
+         *
+         * Bamboo, Cactus, Carved Pumpkin, Hay Bale,
+         * Melon, Pumpkin, Sugar Cane, Potatoes, Carrots
+         * Beetroot, Wheat.
+         */
         "minecraft:melon_block",
         "minecraft:melon_stem",
         "minecraft:potatoes",
         "minecraft:pumpkin",
         "minecraft:carved_pumpkin",
         "minecraft:pumpkin_stem",
-        "minecraft:sapling",
-        "minecraft:seagrass",
-        "minecraft:small_dripleaf_block",
-        "minecraft:spore_blossom",
-        "minecraft:reeds",
-        "minecraft:sweet_berry_bush",
-        "minecraft:sweet_berries",
-        "minecraft:vine",
+        "minecraft:beetroot",
+        "minecraft:bamboo",
         "minecraft:wheat",
-        "minecraft:kelp",
+        "minecraft:carrots",
+        "minecraft:reeds",
+
+        /**
+         * Cave Plants
+         *
+         * Big Dripleaf, Glow Lichen, Hanging Roots,
+         * Moss Block, Moss Carpet, Small Dripleaf,
+         * Spore Blossom, Cave Vines.
+         */
+        "minecraft:glow_lichen",
+        "minecraft:small_dripleaf_block",
+        "minecraft:big_dripleaf",
+        "minecraft:cave_vines",
+        "minecraft:cave_vines_body_with_berries",
+        "minecraft:cave_vines_head_with_berries",
+        "minecraft:moss_block",
+        "minecraft:moss_carpet",
+        "minecraft:hanging_roots",
+        "minecraft:spore_blossom",
+        "minecraft:glow_berries",
+
+        /**
+         * Shrubbery
+         *
+         * Dead Bush, Fern, Grass, Large Fern,
+         * Lily Pad, Tall Grass, Vines
+         */
+        "minecraft:double_plant",
+        "minecraft:tallgrass",
+        "minecraft:deadbush",
+        "minecraft:vine",
+        "minecraft:twisting_vines",
+        "minecraft:weeping_vines",
+        "minecraft:chorus_plant",
+
+        /**
+         * Nether
+         *
+         * Crimson Fungus, Warped Fungus, Nether Wart,
+         * Nether Sprouts, Crimson Roots, Warped Roots.
+         */
         "minecraft:crimson_fungus",
         "minecraft:warped_fungus",
-        "minecraft:glow_lichen",
-        "minecraft:brown_mushroom",
-        "minecraft:red_mushroom",
         "minecraft:nether_wart",
         "minecraft:nether_sprouts",
         "minecraft:crimson_roots",
         "minecraft:warped_roots",
-        "minecraft:twisting_vines",
-        "minecraft:weeping_vines",
+
+        /**
+         * Water Plants
+         *
+         * Water Lily, Sea Grass, Kelp
+         */
+        "minecraft:waterlily",
+        "minecraft:seagrass",
+        "minecraft:kelp",
+
+        /**
+         * Miscellaneous
+         *
+         * Blocks that I am too lazy to sort out right now
+         */
+        "minecraft:cocoa",
+        "minecraft:cactus",
+        "minecraft:azalea",
+        "minecraft:sweet_berry_bush",
+        "minecraft:sweet_berries",
     ];
 
     const now = Date.now();
