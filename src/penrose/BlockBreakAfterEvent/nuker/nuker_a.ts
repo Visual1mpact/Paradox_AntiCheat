de7348e10604b36dc449d578125869f265135eda
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-07 19:49:37 -0400

    [nukerA] Remove unused module

--- src/penrose/BlockBreakAfterEvent/nuker/nuker_a.ts
+++ src/penrose/BlockBreakAfterEvent/nuker/nuker_a.ts
@@ -1,7 +1,7 @@
 import { world, BlockBreakAfterEvent, system, EntityQueryOptions, PlayerLeaveAfterEvent } from "@minecraft/server";
 import { flag } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
-import { MinecraftBlockTypes, MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index.js";
+import { MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index.js";
 
 const lastBreakTime = new Map<string, number>();
 const breakCounter = new Map<string, number>();
