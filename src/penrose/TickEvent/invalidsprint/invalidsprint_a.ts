2673a843a970d01e7ce0f299311a84621d5b0c63
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-08 20:11:15 -0400

    [invalidsprintA] Improve accuracy with entityHurt and playerSpawn events
    This commit enhances accuracy by incorporating the entityHurt and playerSpawn events. The entityHurt event prevents false flagging caused by knockback effects, ensuring fair movement analysis. Additionally, the playerSpawn event prevents false flagging due to sudden movements during respawns. These additions enhance cheat detection precision while maintaining gameplay integrity.

--- src/penrose/TickEvent/invalidsprint/invalidsprint_a.ts
+++ src/penrose/TickEvent/invalidsprint/invalidsprint_a.ts
@@ -1,16 +1,46 @@
-import { world, system, PlayerLeaveAfterEvent } from "@minecraft/server";
+import { world, system, PlayerLeaveAfterEvent, EntityHurtAfterEvent, PlayerSpawnAfterEvent } from "@minecraft/server";
 import { MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index";
 import { flag, isTimerExpired } from "../../../util.js";
 import config from "../../../data/config.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
 
 // Create a Map to store each player's last known position, timestamp, and highest speed
-const playerData = new Map<string, { lastPosition: number[]; lastTimestamp: number; highestBps: number }>();
+const playerData = new Map<string, { lastPosition: number[]; lastTimestamp: number; highestBps: number; lastHitTimestamp: number }>();
 
 function onPlayerLogout(event: PlayerLeaveAfterEvent): void {
     // Remove the player's data from the map when they log off
     const playerName = event.playerId;
     playerData.delete(playerName);
+}
+
+function onPlayerSpawn(event: PlayerSpawnAfterEvent): void {
+    if (event.initialSpawn === true) {
+        return;
+    }
+
+    const playerName = event.player.id;
+
+    // Initialize player data when they spawn
+    playerData.set(playerName, {
+        lastPosition: [event.player.location.x, event.player.location.y, event.player.location.z],
+        lastTimestamp: Date.now(),
+        highestBps: 0,
+        lastHitTimestamp: 0,
+    });
+}
+
+function onEntityHurt(event: EntityHurtAfterEvent): void {
+    if (!event.hurtEntity.isValid()) {
+        return;
+    }
+
+    const playerName = event.hurtEntity.id;
+
+    // Update the last hit timestamp for the player
+    if (playerData.has(playerName)) {
+        const playerInfo = playerData.get(playerName);
+        playerInfo.lastHitTimestamp = Date.now();
+    }
 }
 
 function calculateMovementBPS(currentPosition: number[], lastPosition: number[], playerTimestamp: number, lastTimestamp: number): number {
@@ -44,6 +74,8 @@ function invalidsprinta(id: number) {
     // Unsubscribe if disabled in-game
     if (invalidSprintABoolean === false) {
         playerData.clear();
+        world.afterEvents.playerSpawn.unsubscribe(onPlayerSpawn);
+        world.afterEvents.entityHurt.unsubscribe(onEntityHurt);
         world.afterEvents.playerLeave.unsubscribe(onPlayerLogout);
         system.clearRun(id);
         return;
@@ -75,11 +107,26 @@ function invalidsprinta(id: number) {
 
         // If playerData Map doesn't have a key for the player's name, add it with initial values
         if (!playerData.has(playerName)) {
-            playerData.set(playerName, { lastPosition: playerPosition, lastTimestamp: playerTimestamp, highestBps: 0 });
+            playerData.set(playerName, {
+                lastPosition: playerPosition,
+                lastTimestamp: playerTimestamp,
+                highestBps: 0,
+                lastHitTimestamp: 0,
+            });
         }
 
         const playerInfo = playerData.get(playerName);
-        const { lastPosition, lastTimestamp, highestBps } = playerInfo;
+        const { lastPosition, lastTimestamp, highestBps, lastHitTimestamp } = playerInfo;
+
+        // Check if player was hit recently (within the last X milliseconds)
+        const timeSinceLastHit = Date.now() - lastHitTimestamp;
+        const recentlyHit = timeSinceLastHit <= 1000; // Adjust the time threshold as needed
+
+        // Skip processing for players who were recently hit
+        if (recentlyHit) {
+            continue;
+        }
+
         const bps = calculateMovementBPS(playerPosition, lastPosition, playerTimestamp, lastTimestamp);
         playerInfo.lastPosition = playerPosition;
         playerInfo.lastTimestamp = playerTimestamp;
@@ -101,6 +148,8 @@ function invalidsprinta(id: number) {
  * if needed to do so.
  */
 export function InvalidSprintA() {
+    world.afterEvents.playerSpawn.subscribe(onPlayerSpawn);
+    world.afterEvents.entityHurt.subscribe(onEntityHurt);
     world.afterEvents.playerLeave.subscribe(onPlayerLogout);
     const invalidSprintAId = system.runInterval(() => {
         invalidsprinta(invalidSprintAId);
