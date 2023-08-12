ba15ab0bdf5aaf7403819e73942c6b150f3a5108
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-12 19:01:12 -0400

    [help] Add missing tag command

--- src/commands/moderation/help.ts
+++ src/commands/moderation/help.ts
@@ -122,6 +122,7 @@ export function help(message: ChatSendAfterEvent) {
         `§6${prefix}paradoxui§f - ${config.customcommands.paradoxiu ? `Shows GUI for main menu.` : textDisabled}`,
         `§6${prefix}tpr§f - ${config.customcommands.tpr ? `Will send requests to tp to players.` : textDisabled}`,
         `§6${prefix}biome§f - ${config.customcommands.biome ? `Sends the current biome and direction the player is facing.` : textDisabled}`,
+        `§6${prefix}tag§f - ${config.customcommands.tag ? `Gives one or more ranks to a specified player or resets it.` : textDisabled}`,
         `§6${prefix}version§f - Will print to chat the currently installed version of paradox.`,
         ` `,
         `§l§o§6[§4Debugging Utilites§6]§r§o`,
