25a11b1af6b70d04656292b901978854f2ef88b7
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-13 17:42:29 -0400

    feat: Add player ban notification for staff
    This commit adds a notification system to inform staff members about a player being banned. When a player is banned, a message is sent to all staff members with the relevant details, including the player's name, the banning staff member (if available), and the reason for the ban.
    
    This notification system enhances communication and awareness among staff members regarding player bans.

--- src/util.ts
+++ src/util.ts
@@ -86,6 +86,9 @@ export async function banMessage(player: Player) {
             player.triggerEvent("paradox:kick");
         });
     }
+
+    // Notify staff that a player was banned
+    sendMsg("@a[tag=paradoxOpped]", [`§f§4[§6Paradox§4]§f ${player.name} has been banned!`, `§4[§6Banned By§4]§f: ${by || "§7N/A"}`, `§4[§6Reason§4]§f: ${reason || "§7N/A"}`]);
 }
 
 /**
