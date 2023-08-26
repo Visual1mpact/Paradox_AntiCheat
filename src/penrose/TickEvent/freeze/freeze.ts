e373416ad194be850d90256a1f612325dccde05c
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-26 14:16:10 -0400

    refactor: Update freeze script to use encrypted tags for freeze data storage
    Revamped the freeze script to enhance security and efficiency by replacing the previous use of a map for freeze data storage with encrypted tags.

--- src/penrose/TickEvent/freeze/freeze.ts
+++ src/penrose/TickEvent/freeze/freeze.ts
@@ -1,20 +1,11 @@
-import { world, Player, system, EntityQueryOptions, Vector, Vector3, Dimension } from "@minecraft/server";
-import { sendMsg, setTimer } from "../../../util";
+import { world, Player, system, EntityQueryOptions, Vector } from "@minecraft/server";
+import { decryptString, encryptString, sendMsg, setTimer } from "../../../util";
 import { MinecraftEffectTypes } from "../../../node_modules/@minecraft/vanilla-data/lib/index";
-
-const freezeDataMap: Map<string, FreezeData> = new Map();
-
-type FreezeData = {
-    player: Player;
-    freezeId: number;
-    originalLocation: Vector3;
-    originalDimension: Dimension;
-};
 
 function freezePlayer(player: Player) {
     // Record the player's original location
     const originalLocation = player.location;
-    const originalDimension = player.dimension;
+    const originalDimension = player.dimension.id;
 
     setTimer(player.id);
     // Teleport the player to the freezing location
@@ -29,14 +20,10 @@ function freezePlayer(player: Player) {
     // Create prison around the player
     player.runCommand(`fill ${originalLocation.x + 2} ${245 + 2} ${originalLocation.z + 2} ${originalLocation.x - 2} ${245 - 1} ${originalLocation.z - 2} barrier [] hollow`);
 
-    // Save the player's freeze data
-    const freezeData: FreezeData = {
-        player,
-        freezeId: -1, // Placeholder value, will be updated later
-        originalLocation: originalLocation,
-        originalDimension: originalDimension,
-    };
-    freezeDataMap.set(player.id, freezeData);
+    // Encrypt the data
+    const encryptData = encryptString(`${originalLocation.x},${originalLocation.y},${originalLocation.z},${originalDimension.replace("minecraft:", "")}`, player.id);
+    // Store original location and dimension in a tag
+    player.addTag(`paradoxFreezeData:${encryptData}`);
 }
 
 function unfreezePlayer(player: Player) {
@@ -44,43 +31,39 @@ function unfreezePlayer(player: Player) {
         return; // Player object is undefined or null
     }
 
-    const freezeData = freezeDataMap.get(player.id);
-    if (!freezeData) {
-        return; // Player not frozen
-    }
+    // Retrieve the tag
+    const freezeTag = player.getTags().find((tag) => tag.startsWith("paradoxFreezeData:"));
 
-    const { originalLocation, originalDimension } = freezeData;
+    if (freezeTag) {
+        // Decrypt data
+        const decryptData = decryptString(freezeTag.replace("paradoxFreezeData:", ""), player.id);
+        const freezeTagDecrypt = `paradoxFreezeData:${decryptData}`;
+        // Parse the tag to extract location and dimension information
+        const tagParts = freezeTagDecrypt.split(":");
+        if (tagParts.length === 2) {
+            const locationAndDimension = tagParts[1].split(",");
+            if (locationAndDimension.length === 4) {
+                const originalX = parseFloat(locationAndDimension[0]);
+                const originalY = parseFloat(locationAndDimension[1]);
+                const originalZ = parseFloat(locationAndDimension[2]);
+                const originalDimensionName = locationAndDimension[3];
 
-    // Remove the prison blocks
-    player.runCommand(`fill ${originalLocation.x + 2} ${245 + 2} ${originalLocation.z + 2} ${originalLocation.x - 2} ${245 - 1} ${originalLocation.z - 2} air [] hollow`);
+                player.removeTag(freezeTag);
 
-    setTimer(player.id);
-    // Teleport the player back to their original location
-    player.teleport(originalLocation, {
-        dimension: originalDimension,
-        rotation: player.getRotation(),
-        facingLocation: player.getViewDirection(),
-        checkForBlocks: false,
-        keepVelocity: false,
-    });
+                // Remove the prison blocks
+                player.runCommand(`fill ${originalX + 2} ${245 + 2} ${originalZ + 2} ${originalX - 2} ${245 - 1} ${originalZ - 2} air [] hollow`);
 
-    // Remove the freeze data
-    freezeDataMap.delete(player.id);
-}
-
-// Function to unfreeze a player by their ID
-function unfreezePlayerById(id: string) {
-    const player = world.getPlayers().find((check) => {
-        return check.id === id;
-    });
-    if (!player) {
-        return; // Player not found
-    }
-
-    try {
-        unfreezePlayer(player);
-    } catch (error) {
-        console.error(`Error unfreezing player: ${error}`);
+                setTimer(player.id);
+                // Teleport the player back to their original location
+                player.teleport(new Vector(originalX, originalY, originalZ), {
+                    dimension: world.getDimension(originalDimensionName),
+                    rotation: player.getRotation(),
+                    facingLocation: player.getViewDirection(),
+                    checkForBlocks: false,
+                    keepVelocity: false,
+                });
+            }
+        }
     }
 }
 
@@ -91,86 +74,94 @@ const freezePlayers = () => {
     };
     const players = world.getPlayers(filter);
     for (const player of players) {
-        const freezeData = freezeDataMap.get(player.id);
         const hasFreezeTag = player.hasTag("paradoxFreeze");
         const hasAuraTag = player.hasTag("freezeAura");
         const hasNukerTag = player.hasTag("freezeNukerA");
 
-        if (!freezeData && hasFreezeTag) {
-            freezePlayer(player);
-        } else if (freezeData && !hasFreezeTag) {
+        if (!hasFreezeTag) {
+            // Player doesn't have the freeze tag, unfreeze them
             unfreezePlayer(player);
-        } else if (freezeData && hasFreezeTag) {
-            const originalLocation = freezeData.originalLocation;
-            if (!originalLocation) {
-                continue; // Skip this player if freeze data is missing
-            }
-            const effects = [MinecraftEffectTypes.Blindness, MinecraftEffectTypes.MiningFatigue, MinecraftEffectTypes.Weakness, MinecraftEffectTypes.Slowness];
-            for (const typeEffect of effects) {
-                if (!player.getEffect(typeEffect)) {
-                    player.addEffect(typeEffect, 1000000, { amplifier: 255, showParticles: true });
+        } else {
+            // Player has the freeze tag, freeze or update them
+            let freezeDataTag = player.getTags().find((tag) => tag.startsWith("paradoxFreezeData:"));
+            if (freezeDataTag) {
+                // Decrypt data
+                const decryptData = decryptString(freezeDataTag.replace("paradoxFreezeData:", ""), player.id);
+                freezeDataTag = `paradoxFreezeData:${decryptData}`;
+                // Process data
+                const freezeData = freezeDataTag.split(":")[1];
+                const [originalX, originalY, originalZ, originalDimension] = freezeData.split(",").map((value, index) => (index === 3 ? value : parseFloat(value)));
+
+                // Check if the player has moved and teleport if necessary
+                const { x, y, z } = player.location;
+                if (Math.floor(x) !== Math.floor(originalX as number) || Math.floor(y) !== Math.floor(originalY as number) || Math.floor(z) !== Math.floor(originalZ as number) || player.dimension.id !== originalDimension) {
+                    player.teleport(new Vector(originalX as number, originalY as number, originalZ as number), {
+                        dimension: world.getDimension(originalDimension as string),
+                        rotation: player.getRotation(),
+                        facingLocation: player.getViewDirection(),
+                        checkForBlocks: false,
+                        keepVelocity: false,
+                    });
                 }
-            }
-            const { x: originalX, z: originalZ } = originalLocation;
-            const { x: currentX, y: currentY, z: currentZ } = player.location;
-            if (Math.floor(currentX) !== Math.floor(originalX) || Math.floor(currentY) !== 245 || Math.floor(currentZ) !== Math.floor(originalZ)) {
-                setTimer(player.id);
-                // Teleport the player to the freezing location
-                player.teleport(new Vector(originalX, 245, originalZ), {
-                    dimension: world.getDimension("overworld"),
-                    rotation: { x: 0, y: 0 },
-                    facingLocation: { x: 0, y: 0, z: 0 },
-                    checkForBlocks: false,
-                    keepVelocity: false,
-                });
-                // Create prison around the player again
-                player.runCommand(`fill ${originalLocation.x + 2} ${245 + 2} ${originalLocation.z + 2} ${originalLocation.x - 2} ${245 - 1} ${originalLocation.z - 2} barrier [] hollow`);
+
+                // Check and apply effects if not already present
+                const effects = [MinecraftEffectTypes.Blindness, MinecraftEffectTypes.MiningFatigue, MinecraftEffectTypes.Weakness, MinecraftEffectTypes.Slowness];
+                for (const typeEffect of effects) {
+                    if (!player.getEffect(typeEffect)) {
+                        player.addEffect(typeEffect, 1000000, { amplifier: 255, showParticles: true });
+                    }
+                }
+            } else {
+                // Player has the freeze tag but no freeze data tag, freeze them
+                freezePlayer(player);
             }
 
-            // If both tags exist display custom message
-            if (hasAuraTag && hasNukerTag) {
-                player.onScreenDisplay.setTitle("§f§4[§6Paradox§4]§f Frozen!", { subtitle: "§fContact Staff §4[§6KA§4]§f§4[§6NA§4]§f", fadeInDuration: 0, fadeOutDuration: 0, stayDuration: 60 });
-            } else {
-                player.onScreenDisplay.setTitle("§f§4[§6Paradox§4]§f Frozen!", { subtitle: "§fContact Staff §o§4[§6Command§4]§f", fadeInDuration: 0, fadeOutDuration: 0, stayDuration: 60 });
-            }
+            // Display custom message if both aura and nuker tags exist
+            const title = hasAuraTag && hasNukerTag ? { subtitle: "§fContact Staff §4[§6KA§4]§f§4[§6NA§4]§f" } : { subtitle: "§fContact Staff §o§4[§6Command§4]§f" };
+
+            player.onScreenDisplay.setTitle("§f§4[§6Paradox§4]§f Frozen!", {
+                ...title,
+                fadeInDuration: 0,
+                fadeOutDuration: 0,
+                stayDuration: 60,
+            });
         }
     }
 
     // Unfreeze players who no longer have the "paradoxFreeze" tag
-    for (const id of freezeDataMap.keys()) {
-        const player = world.getPlayers().find((check) => {
-            return check.id === id;
-        });
-        if (!player) {
-            continue; // Skip this iteration if player is not available
-        }
-        try {
-            const hasTag = player?.hasTag && player.hasTag("paradoxFreeze");
-            if (!hasTag) {
-                unfreezePlayerById(id);
-            }
-        } catch (error) {
-            console.error(`Error unfreezing player with ID ${id}: ${error}`);
+    for (const player of world.getPlayers()) {
+        if (!player.hasTag("paradoxFreeze")) {
+            unfreezePlayer(player);
         }
     }
 };
 
 // Subscribe to the playerLeave event to handle frozen players leaving
 export const freezeLeave = (): void => {
-    world.afterEvents.playerLeave.subscribe((player) => {
-        const boolean = freezeDataMap.has(player.playerId);
-        if (boolean) {
-            sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f ${player.playerName}§f was frozen and left the server.`);
+    world.afterEvents.playerLeave.subscribe((event) => {
+        const playerId = event.playerId;
+        const hasFreezeTag = world
+            .getPlayers()
+            .find((player) => player.id === playerId)
+            ?.hasTag("paradoxFreeze");
+
+        if (hasFreezeTag) {
+            sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f ${event.playerName}§f was frozen and left the server.`);
         }
     });
 };
 
 // Subscribe to the playerJoin event to handle frozen players returning
 export const freezeJoin = (): void => {
-    world.afterEvents.playerJoin.subscribe((player) => {
-        const boolean = freezeDataMap.has(player.playerId);
-        if (boolean) {
-            sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f ${player.playerName}§f was frozen and returned to the server.`);
+    world.afterEvents.playerJoin.subscribe((event) => {
+        const playerId = event.playerId;
+        const hasFreezeTag = world
+            .getPlayers()
+            .find((player) => player.id === playerId)
+            ?.hasTag("paradoxFreeze");
+
+        if (hasFreezeTag) {
+            sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f ${event.playerName}§f was frozen and returned to the server.`);
         }
     });
 };
