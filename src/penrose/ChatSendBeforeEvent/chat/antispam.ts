04438e40b758d15486f0f02c6486adb4aab99a09
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-08-29 21:01:28 -0400

    Enhance Anti-Spam System with Warning Messages
    This commit introduces enhancements to the anti-spam system to provide better user feedback and handling of spam behavior. The code has been improved as follows:
    
    1. Added Warning Messages:
       - A set of randomized warning messages has been included to notify players about excessive messaging behavior.
       - Players will receive different warning messages when they approach the spam limit threshold, improving communication with players.
    
    2. Last Chance Warning:
       - A "lastChanceWarning" message has been introduced to notify players when they are close to reaching the offense limit.
       - This message informs players that continued rapid messaging will result in a ban.
    
    3. Improved ChatRecord Handling:
       - The logic for managing player chat records has been refined.
       - The code now initializes a new chat record for players who do not have existing records.
       - Players' offense counts and times are updated more accurately based on their messaging behavior.

--- src/penrose/ChatSendBeforeEvent/chat/antispam.ts
+++ src/penrose/ChatSendBeforeEvent/chat/antispam.ts
@@ -2,10 +2,23 @@ import { ChatSendBeforeEvent, PlayerLeaveAfterEvent, world } from "@minecraft/se
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
 import { sendMsgToPlayer } from "../../../util.js";
 
-const chatSpamLimit = 3; // The maximum number of messages a player can send in the spamTime frame.
 const spamTime = 2 * 1000; // The time frame during which the player's messages will be counted.
-const offenseCount = 3; // Total strikes until you are kicked out.
+const offenseCount = 5; // Total strikes until you are kicked out.
 const strikeReset = 30 * 1000; // The time frame until the strike is reduced
+const lastChanceWarning = "Final warning: Continued rapid messaging will result in a ban.";
+
+const warningMessages = [
+    "Please refrain from sending messages too quickly.",
+    "Messaging too frequently may result in disruptions.",
+    "Slow down your messaging pace to maintain chat quality.",
+    "Sending messages rapidly can lead to temporary restrictions.",
+    "Avoid spamming the chat with quick messages.",
+    "Remember to allow some time between messages.",
+    "Excessive messaging speed can lead to chat limitations.",
+    "Maintain a reasonable pace while using the chat.",
+    "Help keep the chat readable by sending messages thoughtfully.",
+    "Keep in mind that messaging too quickly might be limited.",
+];
 
 interface ChatRecord {
     count: number;
@@ -20,6 +33,11 @@ function onPlayerLogout(event: PlayerLeaveAfterEvent): void {
     // Remove the player's data from the map when they log off
     const playerName = event.playerId;
     chatRecords.delete(playerName);
+}
+
+function getRandomWarningMessage() {
+    const randomIndex = Math.floor(Math.random() * warningMessages.length);
+    return warningMessages[randomIndex];
 }
 
 function beforeantispam(msg: ChatSendBeforeEvent) {
@@ -43,11 +61,29 @@ function beforeantispam(msg: ChatSendBeforeEvent) {
     // Ignore those with permissions
     if (uniqueId !== player.name) {
         const now = Date.now();
-        const chatRecord = chatRecords.get(player.id) ?? { count: 0, lastTime: now, offense: 0, lastOffenseTime: now };
+        let chatRecord = chatRecords.get(player.id);
+
+        if (!chatRecord) {
+            // Initialize a new chat record for the player
+            chatRecord = { count: 0, lastTime: now, offense: 0, lastOffenseTime: now };
+            chatRecords.set(player.id, chatRecord);
+            chatRecord = chatRecords.get(player.id);
+        }
 
         if (now - chatRecord.lastTime > spamTime) {
             // Reset count if time frame has expired
             chatRecord.count = 0;
+        } else if (now !== chatRecord.lastTime) {
+            // Send a random warning message
+            if (chatRecord.offense === offenseCount - 2) {
+                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f ${lastChanceWarning}`);
+            } else {
+                const randomWarningMessage = getRandomWarningMessage();
+                sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f ${randomWarningMessage}`);
+            }
+            msg.sendToTargets = true;
+            chatRecord.offense++;
+            chatRecord.lastOffenseTime = now;
         }
 
         chatRecord.count++;
@@ -55,16 +91,11 @@ function beforeantispam(msg: ChatSendBeforeEvent) {
 
         chatRecords.set(player.id, chatRecord);
 
-        if (chatRecord.count > chatSpamLimit) {
-            msg.sendToTargets = true;
-            sendMsgToPlayer(player, `§f§4[§6Paradox§4]§f You are sending too many messages in a short time!`);
-            chatRecord.offense++;
-            chatRecord.lastOffenseTime = now;
-        }
         if (chatRecord.offense >= offenseCount) {
             msg.sendToTargets = true;
             // Add tag information to the message
             msg.message = `;tag:${player.name},Reason:Spamming,By:Paradox,isBanned`;
+            chatRecords.delete(player.id);
         } else if (chatRecord.offense > 0 && now - chatRecord.lastOffenseTime >= strikeReset) {
             chatRecord.offense--;
             chatRecord.lastOffenseTime = now;
