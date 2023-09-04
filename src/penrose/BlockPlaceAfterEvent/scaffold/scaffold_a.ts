cb84f3672d0c4338ae4c5c6d479a6ea78b063df1
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-09-04 10:56:33 -0400

    Refactor scaffolda logic for improved block placement detection
    In this commit, the scaffolda function has been refactored to enhance the detection of illegal scaffolding behavior. The key changes include:
    
        1. Introduction of the isBlockInFrontAndBelowPlayer function for more accurate block positioning checks.
        2. Improved usage of player and block locations for efficiency and clarity.
        3. Enhanced criteria for considering a block as 'in front and below' the player.
        4. Streamlined block checking logic with more precise conditions.
    
    These changes aim to provide a more reliable and fine-tuned mechanism for identifying and handling illegal scaffolding behavior during block placement events.

--- src/penrose/BlockPlaceAfterEvent/scaffold/scaffold_a.ts
+++ src/penrose/BlockPlaceAfterEvent/scaffold/scaffold_a.ts
@@ -1,11 +1,26 @@
-import { world, MinecraftBlockTypes, BlockPlaceAfterEvent } from "@minecraft/server";
+import { world, MinecraftBlockTypes, Vector3, BlockPlaceAfterEvent } from "@minecraft/server";
 import { flag } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
+
+function isBlockInFrontAndBelowPlayer(blockLocation: Vector3, playerLocation: Vector3) {
+    // Calculate the difference in coordinates between the block and player
+    const dx = blockLocation.x - playerLocation.x;
+    const dy = blockLocation.y - playerLocation.y;
+    const dz = blockLocation.z - playerLocation.z;
+
+    // You can adjust these thresholds based on your requirements
+    // For example, you might want to consider a block in front and below if it's within a certain range.
+    const xThreshold = 1.0;
+    const yThresholdFront = 0.5; // Consider the block in front if it's at a similar height
+    const yThresholdBelow = -0.5; // Consider the block below if it's a certain distance below the player
+    const zThreshold = 1.0;
+
+    return dx < xThreshold && dy < yThresholdFront && dy < yThresholdBelow && dz < zThreshold;
+}
 
 function scaffolda(object: BlockPlaceAfterEvent) {
     // Get Dynamic Property
     const antiScaffoldABoolean = dynamicPropertyRegistry.get("antiscaffolda_b");
-
     // Unsubscribe if disabled in-game
     if (antiScaffoldABoolean === false) {
         world.afterEvents.blockPlace.unsubscribe(scaffolda);
@@ -23,21 +38,24 @@ function scaffolda(object: BlockPlaceAfterEvent) {
         return;
     }
 
-    // Block coordinates
-    const { x, y, z } = block.location;
+    // Cache player location
+    const playerLocation = player.location;
 
-    // Block below placement
-    const belowBlockLocation = { x, y: y - 1, z };
+    // Cache block location
+    const blockLocation = block.location;
 
-    // Is it air
-    const blockType = dimension.getBlock(belowBlockLocation).isAir();
+    // Check if the block is in front and below the player
+    if (isBlockInFrontAndBelowPlayer(blockLocation, playerLocation)) {
+        // Check if the block below is air
+        const blockType = dimension.getBlock({ x: blockLocation.x, y: blockLocation.y - 1, z: blockLocation.z }).isAir();
 
-    // Are they sprinting
-    const isSprinting = player.isSprinting;
+        // Are they sprinting
+        const isSprinting = player.isSprinting;
 
-    if (blockType && isSprinting) {
-        dimension.getBlock({ x: x, y: y, z: z }).setType(MinecraftBlockTypes.air);
-        flag(player, "Scaffold", "A", "Placement", null, null, null, null, false);
+        if (blockType && isSprinting) {
+            dimension.getBlock(blockLocation).setType(MinecraftBlockTypes.air);
+            flag(player, "Scaffold", "A", "Placement", null, null, null, null, false);
+        }
     }
 }
 
