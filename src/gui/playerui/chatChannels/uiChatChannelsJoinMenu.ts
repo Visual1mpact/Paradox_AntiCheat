5d82ddda7f99047c5609ecc810e6dcde8c22ec9f
Pete9xi <peter95rose@msn.com>
2023-09-04 12:34:40 +0100

    [utils] Function to return current channels and if they require a password [modulesui] Typo fix [uiChatChannelsJoinMenu] implement chat channel joining via the UI need to add error handling.

--- src/gui/playerui/chatChannels/uiChatChannelsJoinMenu.ts
+++ src/gui/playerui/chatChannels/uiChatChannelsJoinMenu.ts
@@ -1,9 +1,22 @@
 import { Player } from "@minecraft/server";
 import { ModalFormData } from "@minecraft/server-ui";
+import { listChatChannels } from "../../../util";
+import { uiChatChannelJoin } from "./uiChatChannels";
 
-export function uiChatChannelsJoinMenu(player: Player) {
+export function ChatChannelsJoinMenuUI(player: Player) {
     const menu = new ModalFormData();
     menu.title("§4Paradox - Join A Channel§4");
-    menu.textField("Channel Name: ", "");
+    const channelsList = listChatChannels();
+    const channelDropdownData = channelsList.map((channel) => ({
+        text: `${channel.channelName}, §fPassword: ${channel.hasPassword === "Yes" ? "§aYes" : "§cNo"}`,
+        value: channel.channelName,
+    }));
+    if (channelDropdownData.length === 0) {
+        channelDropdownData.push({ text: "§6There are no existing channels", value: "" });
+    }
+    menu.dropdown(`\n§fSelect a channel:\n\n`, channelDropdownData);
     menu.textField("Channel Password: ", "");
+    menu.show(player).then((chatChannelsJoinResult) => {
+        uiChatChannelJoin(chatChannelsJoinResult, player, channelDropdownData);
+    });
 }
