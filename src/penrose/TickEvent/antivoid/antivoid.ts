de68469f7db5e6ebb3655830f1c107262979e100
Pete9xi <peter95rose@msn.com>
2023-09-05 17:29:23 +0100

    [antiVoid] WIP basic detection in place still needs work.

--- src/penrose/TickEvent/antivoid/antivoid.ts
+++ src/penrose/TickEvent/antivoid/antivoid.ts
@@ -1,7 +1,8 @@
 import { world, EntityQueryOptions, GameMode, system } from "@minecraft/server";
 //import { flag } from "../../../util.js";
 import { dynamicPropertyRegistry } from "../../WorldInitializeAfterEvent/registry.js";
-
+var savedValue: number;
+var flagPlayer: boolean = false;
 function antiVoid(id: number) {
     //exclude players who are in creative.
     const gm: EntityQueryOptions = {
@@ -9,49 +10,45 @@ function antiVoid(id: number) {
     };
     const filteredPlayers = world.getPlayers(gm);
 
-    /*const airBlocksToCheck = new Set<{ dx: number; dy: number; dz: number }>([
-        //check for a half block that the player maybe standing on if its a lower slab
-        { dx: 0, dy: -0.5, dz: 0 },
-        { dx: 0, dy: -1, dz: 0 },
-        { dx: 1, dy: -1, dz: 0 },
-        { dx: -1, dy: -1, dz: 0 },
-        { dx: 0, dy: -1, dz: 1 },
-        { dx: 0, dy: -0.5, dz: 1 },
-        { dx: 0, dy: -1, dz: -1 },
-        { dx: 0, dy: -0.5, dz: -1 },
-        { dx: 1, dy: -1, dz: 1 },
-        { dx: 1, dy: -0.5, dz: 1 },
-        { dx: 1, dy: -1, dz: -1 },
-        { dx: 1, dy: -0.5, dz: -1 },
-        { dx: -1, dy: -1, dz: 1 },
-        { dx: -1, dy: -0.5, dz: 1 },
-        { dx: -1, dy: -1, dz: -1 },
-        { dx: -1, dy: -0.5, dz: -1 },
-    ]); */
-
     for (const player of filteredPlayers) {
         // Get unique ID
         const uniqueId = dynamicPropertyRegistry.get(player?.id);
-
         // Skip if they have permission
         if (uniqueId === player.name) {
             continue;
         }
 
-        let lastStandingBlock;
-        let checkAfterFall;
         let lastFallingCord;
+        let lastSavedCord;
         if (player.isFalling == true) {
             lastFallingCord = player.location.y;
-        }
-        if (player.isOnGround == true && checkAfterFall == false) {
-            lastStandingBlock = player.location.y;
-        }
-        if (player.isOnGround == true && checkAfterFall == true) {
-            console.log(lastStandingBlock);
+            saveOrGetValue("save", lastFallingCord);
+            flagPlayer = false;
             console.log(lastFallingCord);
         }
-        //console.log(`1: ${y1}\n2: ${y2}\n3: ${y3}\n4: ${y4}\n5: ${y5}\n6: ${y6}\n7: ${y7}`);
+        if (player.isOnGround == true) {
+            // @ts-ignore
+            lastSavedCord = saveOrGetValue("get");
+            if (player.location.y - lastSavedCord >= 3) {
+                if (flagPlayer == false) {
+                    console.log("Player has been flagged for AntiVoid Expliot");
+                    flagPlayer = true;
+                }
+            }
+        }
+    }
+    // @ts-ignore
+    function saveOrGetValue(action: string, value: number) {
+        if (action === "save") {
+            // Save the value
+            savedValue = value;
+        } else if (action === "get") {
+            // Retrieve the saved value
+            return savedValue;
+        } else {
+            // Handle invalid action
+            console.error('Invalid action. Use "save" or "get".');
+        }
     }
 }
 
@@ -63,5 +60,5 @@ function antiVoid(id: number) {
 export function antiVoida() {
     const antiVoidId = system.runInterval(() => {
         antiVoid(antiVoidId);
-    }, 20);
+    }, 5);
 }
