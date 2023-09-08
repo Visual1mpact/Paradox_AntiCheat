e5af71bc2fff95a7df4d0e7d7da9bc2f5a1c91ba
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-09-07 21:16:44 -0400

    Add error handling for chatChannelsMenu

--- src/gui/guiHandler/results/chatChannelsMenu.ts
+++ src/gui/guiHandler/results/chatChannelsMenu.ts
@@ -13,26 +13,38 @@ export function chatChannelMainMenu(player: Player) {
     menu.button("Invite A Player");
     menu.button("Leave A Channel");
     menu.button("Delete A Channel");
-    menu.show(player).then((chatChannelsMenuUIResult) => {
-        switch (chatChannelsMenuUIResult.selection) {
-            case 0:
-                chatChannelsCreateMenuUI(player);
-                break;
-            case 1:
-                ChatChannelsJoinMenuUI(player);
-                break;
-            case 2:
-                ChatChannelsInviteMenuUI(player);
-                break;
-            case 3:
-                chatChannelsLeaveMenuUI(player);
-                break;
-            case 4:
-                ChatChannelsDeleteMenuUI(player);
-                break;
+    menu.show(player)
+        .then((chatChannelsMenuUIResult) => {
+            switch (chatChannelsMenuUIResult.selection) {
+                case 0:
+                    chatChannelsCreateMenuUI(player);
+                    break;
+                case 1:
+                    ChatChannelsJoinMenuUI(player);
+                    break;
+                case 2:
+                    ChatChannelsInviteMenuUI(player);
+                    break;
+                case 3:
+                    chatChannelsLeaveMenuUI(player);
+                    break;
+                case 4:
+                    ChatChannelsDeleteMenuUI(player);
+                    break;
 
-            default:
-                break;
-        }
-    });
+                default:
+                    break;
+            }
+        })
+        .catch((error) => {
+            console.error("Paradox Unhandled Rejection: ", error);
+            // Extract stack trace information
+            if (error instanceof Error) {
+                const stackLines = error.stack.split("\n");
+                if (stackLines.length > 1) {
+                    const sourceInfo = stackLines;
+                    console.error("Error originated from:", sourceInfo[0]);
+                }
+            }
+        });
 }
