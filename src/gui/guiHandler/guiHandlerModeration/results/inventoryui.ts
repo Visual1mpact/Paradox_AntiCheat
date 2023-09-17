7f9374cd0430a00651a17bdcb4042ae738c6e349
Pete9xi <peter95rose@msn.com>
2023-09-17 11:54:07 +0100

    [uiInventory] Remove from Debug

--- src/gui/guiHandler/guiHandlerModeration/results/inventoryui.ts
+++ src/gui/guiHandler/guiHandlerModeration/results/inventoryui.ts
@@ -1,30 +1,26 @@
 import { Player, world } from "@minecraft/server";
-import config from "../../../../data/config";
 import { ModalFormData } from "@minecraft/server-ui";
 import { uiINVENTORY } from "../../../moderation/uiInventory";
-
 export function inventoryHandler(player: Player) {
-    if (config.debug === true) {
-        const inventoryUI = new ModalFormData();
-        inventoryUI.title("§4Paradox - Inventory Managment§4");
-        let onlineList: string[] = [];
-        onlineList = Array.from(world.getPlayers(), (player) => player.name);
-        inventoryUI.dropdown(`\n§fSelect a player:§f\n\nPlayer's Online\n`, onlineList);
-        inventoryUI
-            .show(player)
-            .then((inventoryUIResult) => {
-                uiINVENTORY(inventoryUIResult, onlineList, player);
-            })
-            .catch((error) => {
-                console.error("Paradox Unhandled Rejection: ", error);
-                // Extract stack trace information
-                if (error instanceof Error) {
-                    const stackLines = error.stack.split("\n");
-                    if (stackLines.length > 1) {
-                        const sourceInfo = stackLines;
-                        console.error("Error originated from:", sourceInfo[0]);
-                    }
+    const inventoryUI = new ModalFormData();
+    inventoryUI.title("§4Paradox - Inventory Management§4");
+    let onlineList: string[] = [];
+    onlineList = Array.from(world.getPlayers(), (player) => player.name);
+    inventoryUI.dropdown(`\n§fSelect a player:§f\n\nPlayer's Online\n`, onlineList);
+    inventoryUI
+        .show(player)
+        .then((inventoryUIResult) => {
+            uiINVENTORY(inventoryUIResult, onlineList, player);
+        })
+        .catch((error) => {
+            console.error("Paradox Unhandled Rejection: ", error);
+            // Extract stack trace information
+            if (error instanceof Error) {
+                const stackLines = error.stack.split("\n");
+                if (stackLines.length > 1) {
+                    const sourceInfo = stackLines;
+                    console.error("Error originated from:", sourceInfo[0]);
                 }
-            });
-    }
+            }
+        });
 }
