26099f1687d933a1d14ec0baaf61c2d99697d6a3
Pete9xi <peter95rose@msn.com>
2023-09-23 22:43:32 +0100

    [pvpManager.ts] Check entity if the name is the same add a counter if the counter reaches 10 alert paradox opped staff of the event, else warn the attacker.

--- src/penrose/EntityHitEntityAfterEvent/pvpManager.ts
+++ src/penrose/EntityHitEntityAfterEvent/pvpManager.ts
@@ -2,6 +2,9 @@ import { world, Player, EntityHitEntityAfterEvent } from "@minecraft/server";
 import { sendMsg, sendMsgToPlayer } from "../../util.js";
 import { MinecraftEffectTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 
+let counter = 0;
+let lastAttackedName: string;
+
 function pvp(obj: EntityHitEntityAfterEvent) {
     // Properties from class
     const { damagingEntity, hitEntity } = obj;
@@ -16,7 +19,16 @@ function pvp(obj: EntityHitEntityAfterEvent) {
         for (const effectType of effectsToAdd) {
             hitEntity.addEffect(effectType, 5, { amplifier: 255, showParticles: false });
         }
-        sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${damagingEntity.name}§f has attacked §7${hitEntity.name}§f while §7${hitEntity.name}§f has PVP disabled.`);
+        if (hitEntity.name === lastAttackedName) {
+            counter++;
+        } else {
+            lastAttackedName = hitEntity.name;
+            counter = 0;
+        }
+        if (counter == 10) {
+            sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f §7${damagingEntity.name}§f has attacked §7${hitEntity.name}§f while §7${hitEntity.name}§f has PVP disabled.`);
+            counter = 0;
+        }
     }
 }
 export const PVP = () => {
