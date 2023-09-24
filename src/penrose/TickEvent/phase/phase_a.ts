7ddce229c44a4e50247855b83d8d571da77dfb20
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2023-09-24 09:12:29 -0400

    [phaseA] Adjust block collision check for crawling players
    Incorporated a fix to handle crawling animations where the player's upper body coordinates remain the same, incorrectly causing false flags for phasing through blocks. This adjustment ensures accurate collision checks for players, accounting for crawling animations. The issue likely stems from a Mojang bug that misrepresents upper body coordinates during crawling, leading to false detections of phasing. This fix prevents false positive flags and enhances the reliability of the anti-phase mechanism.

--- src/penrose/TickEvent/phase/phase_a.ts
+++ src/penrose/TickEvent/phase/phase_a.ts
@@ -44,27 +44,17 @@ function antiphasea(id: number) {
 
         const { x, y, z } = player.location;
 
-        // Calculate block locations for head, waist, and feet
-        const upperBody = { x, y: y + 1, z };
+        // Calculate block locations
         const lowerBody = { x, y, z };
 
-        const blocksToCheck = [lowerBody, upperBody];
+        let allSafe = false;
 
-        const allSafe = blocksToCheck.every((block) => {
-            let blockType: Block;
-            try {
-                blockType = player.dimension.getBlock(block);
-            } catch {}
-            // Check if the block is soul sand and the player's y-coordinate difference is within -0.125
-            if (block === lowerBody && blockType && blockType.typeId === "minecraft:soul_sand" && y - block.y <= 0.125) {
-                return true;
-            }
-            // Check if the block is passable or if the player is riding
-            if (!blockType || player.hasTag("riding") || passableSolids.has(blockType.typeId.replace("minecraft:", ""))) {
-                return true;
-            }
-            return !blockType.isSolid;
-        });
+        try {
+            const blockType = player.dimension.getBlock(lowerBody);
+            allSafe = blockType && ((blockType.typeId === "minecraft:soul_sand" && y - lowerBody.y <= 0.125) || player.hasTag("riding") || passableSolids.has(blockType.typeId.replace("minecraft:", "")));
+        } catch {
+            allSafe = true; // Handle any errors in getting the block type
+        }
 
         if (allSafe) {
             // Update last safe location
